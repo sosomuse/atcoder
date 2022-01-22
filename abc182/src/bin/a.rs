6cd13ae485bc4ba05ff98d8f19a579a880654ece
So Nishimura <s.nishimura@gemcook.com>
2022-01-22 13:37:52 +0900

    feat: abc182-a

--- abc182/src/bin/a.rs
+++ abc182/src/bin/a.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+// use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: i32,
+        b: i32,
+    }
+    let x = 2 * a + 100 - b;
+    println!("{}", x);
 }
---
+++ templtes.rs
@@ -0,0 +1,11 @@
+// use proconio::input;
+// use std::collections::HashMap;
+
+fn main() {
+  input! {
+      a: i32,
+      b: i32,
+  }
+
+  println!("hello world");
+}

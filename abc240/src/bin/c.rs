1311497fabb7b6b5393f97560622aff630c9ae1e
So Nishimura <s.nishimura@gemcook.com>
2022-05-01 22:05:44 +0900

    feat: abc241

--- abc240/src/bin/c.rs
+++ abc240/src/bin/c.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        a: [u32; n],
+    }
+
+    let mut p = 0;
+    let mut v: Vec<u32> = vec![];
+
+    for t in a {
+        let x = p + t;
+        if x >= 360 {
+            p = x - 360;
+            v.push(p);
+        } else {
+            p += t;
+            v.push(p);
+        }
+    }
+
+    v.sort();
+    v.reverse();
+
+    let mut prev = 360;
+    let mut ans = 0;
+
+    for t in v {
+        ans = ans.max(prev - t);
+        prev = t;
+    }
+
+    println!("{}", ans);
 }

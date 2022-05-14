2733c00edab6e8e588f50813ecca517530b2ebc2
So Nishimura <s.nishimura@gemcook.com>
2022-05-14 21:41:36 +0900

    feat: abc242 b

--- abc242/src/bin/b.rs
+++ abc242/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    }
+
+    let mut vec: Vec<char> = s.chars().collect();
+
+    vec.sort();
+
+    let ans: String = vec.into_iter().collect();
+
+    println!("{}", ans);
 }

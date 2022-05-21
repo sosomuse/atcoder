f82d308289137fb71bba1b153e1dc8cd220dccbb
So Nishimura <s.nishimura@gemcook.com>
2022-05-21 22:46:30 +0900

    feat: abc252 b

--- abc252/src/bin/b.rs
+++ abc252/src/bin/b.rs
@@ -1,3 +1,47 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input! {
+        (n, k): (u32, u32),
+        a: [u32; n],
+        b: [u32; k],
+    }
+
+    println!("{}", func((n, k), a, b));
+}
+
+fn func((n, k): (u32, u32), a: Vec<u32>, b: Vec<u32>) -> String {
+    let mut maximums = HashMap::new();
+    let max = a.iter().max().unwrap();
+
+    a.iter().enumerate().for_each(|(i, x)| {
+        if x == max {
+            maximums.insert(i as u32 + 1, i);
+        }
+    });
+
+    let mut ans = "No";
+
+    dbg!("{:?}", &maximums);
+
+    for v in b {
+        if maximums.get(&v).is_some() {
+            ans = "Yes";
+            break;
+        }
+    }
+
+    ans.to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(func((3, 2), vec![1, 2, 3], vec![1, 2]), "Yes");
+        assert_eq!(func((3, 2), vec![1, 2, 3], vec![1, 3]), "No");
+    }
 }

8907648398258822dca28417a348f7bf912fc612
So Nishimura <s.nishimura@gemcook.com>
2022-05-25 18:38:35 +0900

    feat: abc251 c

--- abc251/src/bin/c.rs
+++ abc251/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        a: [(String, u32); n],
+    }
+
+    let mut map: HashMap<String, u32> = HashMap::new();
+    let c = a.clone();
+
+    for (s, v) in a {
+        map.entry(s).or_insert(v);
+    }
+
+    let max = map.values().max().unwrap();
+
+    for (i, (s, v)) in c.into_iter().enumerate() {
+        if v == *max && v == *map.get(&s).unwrap() {
+            println!("{}", i + 1);
+            break;
+        }
+    }
 }
--- utils/src/bin/vector.rs
+++ utils/src/bin/vector.rs
@@ -4,6 +4,10 @@ fn main() {
     let mut a = vec![1, 2, 3];
     let n = a.len();
 
+    // Clone
+    let clone = a.clone();
+    println!("{:?}", clone);
+
     // HashMap
     let map: HashMap<u32, u32> = HashMap::new();
     let set = HashSet::<i32>::new();

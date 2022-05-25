a125e7866586ddea4b97562115d662f3cab4e48a
So Nishimura <s.nishimura@gemcook.com>
2022-05-25 17:29:40 +0900

    feat: abc251 b

--- abc251/src/bin/b.rs
+++ abc251/src/bin/b.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input! {
+        (n, w): (u32, u32),
+        a: [u32; n],
+    }
+
+    let mut map: HashMap<usize, u32> = HashMap::new();
+    let mut ans: HashMap<u32, u32> = HashMap::new();
+
+    for i in 0..a.len() {
+        map.entry(i).or_insert(a[i]);
+    }
+
+    for i in 0..a.len() {
+        for j in i + 1..a.len() {
+            for t in j + 1..a.len() {
+                let sum = map[&i] + map[&j] + map[&t];
+                if sum <= w {
+                    ans.entry(sum).or_insert(0);
+                }
+            }
+
+            let sum = map[&i] + map[&j];
+            if sum <= w {
+                ans.entry(sum).or_insert(0);
+            }
+        }
+
+        let sum = map[&i];
+        if sum <= w {
+            ans.entry(sum).or_insert(0);
+        }
+    }
+
+    // dbg!("{}", &map);
+    println!("{}", ans.len());
 }
--- utils/src/bin/vector.rs
+++ utils/src/bin/vector.rs
@@ -1,11 +1,15 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 fn main() {
     let mut a = vec![1, 2, 3];
     let n = a.len();
 
     // HashMap
     let map: HashMap<u32, u32> = HashMap::new();
+    let set = HashSet::<i32>::new();
+
     println!("{:?}", map);
+    println!("{:?}", set);
 
     // for ループ
     for _ in 0..n {

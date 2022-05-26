3c4368c44a860d6eae42f96469be9801aabe805f
So Nishimura <s.nishimura@gemcook.com>
2022-05-26 23:18:24 +0900

    feat: abc250 b

--- abc250/src/bin/b.rs
+++ abc250/src/bin/b.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        (n, a, b): (u8, u8, u8),
+    }
+
+    for i in 0..n {
+        for _ in 0..a {
+            for k in 0..n {
+                for _ in 0..b {
+                    if i % 2 == 1 {
+                        if k % 2 == 0 {
+                            print!("#");
+                            continue;
+                        } else {
+                            print!(".");
+                            continue;
+                        }
+                    }
+
+                    if k % 2 == 0 {
+                        print!(".");
+                    } else {
+                        print!("#");
+                    }
+                }
+            }
+
+            println!();
+        }
+    }
 }

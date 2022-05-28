e59f61907737b9efedb8ac507e734ca4981a5759
So Nishimura <s.nishimura@gemcook.com>
2022-05-28 20:03:27 +0900

    feat: abc085b

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        mut d: [u32; n],
+    }
+
+    d.sort_by(|a, b| b.cmp(a));
+
+    let mut ans = 1;
+    let mut current = d[0];
+
+    // dbg!(&d);
+
+    for v in d {
+        if current > v {
+            ans += 1;
+            current = v;
+        }
+    }
+
+    println!("{}", ans);
 }
--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        mut d: [u32; n],
+    }
+
+    d.sort_by(|a, b| b.cmp(a));
+
+    let mut ans = 0;
+    let mut current = 0;
+
+    for v in d {
+        if v > current {
+            ans += 1;
+            current = v;
+        }
+    }
+
+    println!("{}", ans);
 }

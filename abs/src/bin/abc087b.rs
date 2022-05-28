14af3053f5549edc0ca6e809854abaf001ae71be
So Nishimura <s.nishimura@gemcook.com>
2022-05-28 19:04:56 +0900

    feat: abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        x: u32,
+    }
+
+    let mut ans = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if i * 500 + j * 100 + k * 50 == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    print!("{}", ans);
 }

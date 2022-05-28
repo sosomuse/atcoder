9c3ad206d3a28f5236e2d4e201d6f3e412b89226
So Nishimura <s.nishimura@gemcook.com>
2022-05-28 19:56:52 +0900

    feat: abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        mut a: [u32; n],
+    }
+
+    let mut ans = 0;
+    a.sort_by(|s, t| t.cmp(s));
+
+    dbg!(&a);
+    for i in 0..a.len() {
+        let v = a[i];
+
+        if i % 2 == 0 {
+            ans += v;
+        } else {
+            ans -= v;
+        }
+    }
+
+    print!("{}", ans);
 }

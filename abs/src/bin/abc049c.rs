00d9964c6b1153fba332cf07902ee7ac9a9d207c
So Nishimura <s.nishimura@gemcook.com>
2022-05-29 15:03:02 +0900

    feat: abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(s: String);
+
+    let mut end_idx: usize = s.len();
+
+    loop {
+        for i in 5..=7 {
+            if end_idx < i {
+                println!("NO");
+                return;
+            }
+            if &s[(end_idx - i)..end_idx] == "dream"
+                || &s[(end_idx - i)..end_idx] == "dreamer"
+                || &s[(end_idx - i)..end_idx] == "erase"
+                || &s[(end_idx - i)..end_idx] == "eraser"
+            {
+                end_idx -= i;
+                break;
+            }
+            if i == 7 {
+                println!("NO");
+                return;
+            }
+        }
+        if end_idx == 0 {
+            println!("YES");
+            break;
+        }
+    }
 }

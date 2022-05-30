a0f6df2026bef08a9aacf195c1d55785332c8f66
So Nishimura <s.nishimura@gemcook.com>
2022-05-31 00:44:18 +0900

    feat: 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut ans;
+    for i in 0..(1 << n) {
+        let mut candidate = "".to_string();
+        for j in 1..=n {
+            let t = n - j;
+            if i & (1 << t) == 0 {
+                candidate = format!("{}{}", candidate, '(');
+            } else {
+                candidate = format!("{}{}", candidate, ')');
+            }
+        }
+
+        ans = candidate;
+        if solve(&ans) {
+            println!("{}", &ans);
+        }
+    }
+}
+
+fn solve(s: &String) -> bool {
+    let mut dep: isize = 0;
+    for i in 0..s.len() {
+        if s.chars().nth(i).unwrap() == '(' {
+            dep += 1;
+        }
+        if s.chars().nth(i).unwrap() == ')' {
+            dep -= 1;
+        }
+        if dep < 0 {
+            return false;
+        }
+    }
+    if dep == 0 {
+        true
+    } else {
+        false
+    }
 }

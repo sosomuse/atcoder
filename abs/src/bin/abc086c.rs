9b862c3addfd2703b7b2b4ee54092beea1d133c1
So Nishimura <s.nishimura@gemcook.com>
2022-05-30 12:37:24 +0900

    feat: abc086c

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(n: u32, v: [(i32, i32, i32); n]);
+
+    let mut current_t = 0;
+    let mut current_x = 0;
+    let mut current_y = 0;
+
+    for &(t, x, y) in &v {
+        let use_time = t - current_t;
+
+        let diff = (x - current_x).abs() + (y - current_y).abs();
+
+        if (diff + use_time) % 2 == 1 {
+            println!("No");
+            return;
+        }
+
+        if diff > use_time {
+            println!("No");
+            return;
+        }
+
+        current_x = x;
+        current_y = y;
+        current_t = t;
+    }
+
+    println!("Yes");
 }

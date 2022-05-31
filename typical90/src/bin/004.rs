44e1f592938ec9e869a3eb21cbf0192711fbed54
So Nishimura <s.nishimura@gemcook.com>
2022-05-31 19:30:49 +0900

    feat: typical 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        (h, w): (usize, usize),
+    }
+
+    let mut vertical_map = std::collections::HashMap::<usize, Vec<usize>>::new();
+    let mut horizontal_map = std::collections::HashMap::<usize, Vec<usize>>::new();
+
+    for i in 0..h {
+        input! {
+            a: [usize; w],
+        }
+
+        for j in 0..a.len() {
+            let vertical_v = a[i];
+            vertical_map.entry(i).or_insert(Vec::new()).push(vertical_v);
+
+            let horizontal_v = a[j];
+            horizontal_map
+                .entry(j)
+                .or_insert(Vec::new())
+                .push(horizontal_v)
+        }
+    }
+
+    dbg!(&vertical_map);
+    dbg!(&horizontal_map);
+
+    for i in 0..h {
+        for j in 0..w {
+            let vertical_sum = vertical_map.get(&i).unwrap();
+            let horizontal_sum = horizontal_map.get(&j).unwrap();
+
+            print!("{}", vertical_sum.into_iter().sum().unwrap());
+
+            if j != w {
+                print!(" ");
+            }
+        }
+
+        println!();
+    }
 }

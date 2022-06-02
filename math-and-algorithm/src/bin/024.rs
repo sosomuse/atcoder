282099d339201f014d6a785299d7ee9e31e9c52d
So Nishimura <s.nishimura@gemcook.com>
2022-06-03 01:37:03 +0900

    feat: math-algo 024~026

--- math-and-algorithm/src/bin/024.rs
+++ math-and-algorithm/src/bin/024.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut ans: f32 = 0.;
+
+    for _ in 0..n {
+        input! {
+            (p, q): (f32, f32),
+        }
+
+        let pt = q / p;
+
+        ans += pt;
+    }
+
+    println!("{}", ans)
 }
--- math-and-algorithm/src/bin/025.rs
+++ math-and-algorithm/src/bin/025.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [f32; n],
+        b: [f32; n],
+    }
+
+    let a_sum = a.iter().sum::<f32>();
+    let b_sum = b.iter().sum::<f32>();
+
+    let ans = a_sum / 3. + b_sum * 2. / 3.;
+
+    println!("{}", ans)
 }

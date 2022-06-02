dfdf4d33446210cb024f9bf89c246c892e8de9b0
So Nishimura <s.nishimura@gemcook.com>
2022-06-03 00:59:01 +0900

    feat: 023

--- math-and-algorithm/src/bin/023.rs
+++ math-and-algorithm/src/bin/023.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        b: [f32; n],
+        r: [f32; n],
+    }
+
+    let b_ave = b.iter().sum::<f32>() / b.len() as f32;
+    let r_ave = r.iter().sum::<f32>() / r.len() as f32;
+
+    println!("{}", r_ave + b_ave)
 }

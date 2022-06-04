5349b434ad66c6cb1575979a67fc9acbb02771b8
So Nishimura <s.nishimura@gemcook.com>
2022-06-04 20:55:28 +0900

    feat: abc249

---
+++ abc249/src/bin/a.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        (a, b, c, d, e, f, x): (usize, usize,usize,usize,usize,usize,usize),
+    }
+
+    let taka = (x / (a + c)) * a * b + (x % (a + c)).min(a) * b;
+    let ao = (x / (d + f)) * d * e + (x % (d + f)).min(d) * e;
+
+    if taka == ao {
+        println!("Draw");
+    }
+
+    if taka > ao {
+        println!("Takahashi");
+    }
+
+    if taka < ao {
+        println!("Aoki");
+    }
+}

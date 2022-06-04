bd738a4e580344bd2d930b5298193253556a6a35
So Nishimura <s.nishimura@gemcook.com>
2022-06-04 20:31:02 +0900

    feat: malgo

--- math-and-algorithm/src/bin/029.rs
+++ math-and-algorithm/src/bin/029.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut dp = vec![0; n + 1];
+
+    for i in 0..=n {
+        if i <= 1 {
+            dp[i] = 1;
+        } else {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+    }
+
+    dbg!(&dp);
+
+    println!("{}", dp[n]);
 }
--- math-and-algorithm/src/bin/030.rs
+++ math-and-algorithm/src/bin/030.rs
@@ -1,3 +1,26 @@
+use proconio::*;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
+    }
+
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+
+    // dbg!(&dp);
+    // dbg!(&wv);
+
+    for i in 0..n {
+        for j in 0..=w {
+            if j >= wv[i].0 {
+                dp[i + 1][j] = (dp[i][j - wv[i].0] + wv[i].1).max(dp[i][j]);
+            } else {
+                dp[i + 1][j] = dp[i][j];
+            }
+        }
+    }
+
+    println!("{}", dp[n][w - 1]);
 }

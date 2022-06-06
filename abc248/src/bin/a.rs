dfa473478538eb7619c3bc30faa2b2373389b167
So Nishimura <s.nishimura@gemcook.com>
2022-06-06 13:51:21 +0900

    feat: abc248

--- abc248/src/bin/a.rs
+++ abc248/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    }
+
+    let a = "1234567890";
+
+    for v in a.chars() {
+        if !s.contains(v) {
+            println!("{}", v)
+        }
+    }
 }
--- abc248/src/bin/b.rs
+++ abc248/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut a: u64,
+        b: u64,
+        k: u64,
+    }
+
+    let mut ans = 0;
+
+    while a < b {
+        a *= k;
+        ans += 1;
+    }
+
+    println!("{}", ans)
 }

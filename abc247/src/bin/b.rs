989dfa01a5a5605797b4d5306b64e9deab8e18a6
So Nishimura <s.nishimura@gemcook.com>
2022-06-10 00:01:55 +0900

    feat: abc

--- abc247/src/bin/b.rs
+++ abc247/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [(String, String); n],
+    }
+
+    for i in 0..n {
+        let mut can_give = true;
+        for (j, st) in a.iter().enumerate() {
+            if i == j {
+                continue;
+            }
+
+            let (s1, t1) = &a[i];
+            let (s2, t2) = st;
+
+            if (s1 == s2 || s1 == t2) && (t1 == s2 || t1 == t2) {
+                can_give = false;
+                break;
+            }
+        }
+
+        if !can_give {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }

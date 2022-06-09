6f8978543a2268b9921e5d603f12cff79e4f1c61
So Nishimura <s.nishimura@gemcook.com>
2022-06-09 13:35:14 +0900

    feat: abc246 a

--- abc246/src/bin/a.rs
+++ abc246/src/bin/a.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        (x1, y1): (isize, isize),
+        (x2, y2): (isize, isize),
+        (x3, y3): (isize, isize),
+    }
+
+    let mut x = 0;
+    let mut y = 0;
+
+    if x1 == x2 {
+        x = x3;
+    }
+
+    if x1 == x3 {
+        x = x2;
+    }
+
+    if x2 == x3 {
+        x = x1;
+    }
+
+    if y1 == y2 {
+        y = y3;
+    }
+
+    if y1 == y3 {
+        y = y2;
+    }
+
+    if y2 == y3 {
+        y = y1;
+    }
+
+    println!("{} {}", x, y)
 }

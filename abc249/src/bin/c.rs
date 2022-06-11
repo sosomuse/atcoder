804baf517a790ff47dc5146a844fa95813e405cc
So Nishimura <s.nishimura@gemcook.com>
2022-06-11 19:21:28 +0900

    feat abc249 c

--- abc249/src/bin/c.rs
+++ abc249/src/bin/c.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        s: [String; n],
+    }
+
+    let mut ans = 0;
+
+    for bit in 0..1 << n {
+        let mut count = 0;
+
+        let mut map = HashMap::<char, usize>::new();
+
+        for i in 0..n {
+            if bit & (1 << i) == 0 {
+                continue;
+            }
+
+            for x in s[i].chars() {
+                *map.entry(x).or_insert(0) += 1;
+            }
+        }
+
+        for (_, v) in map.iter() {
+            if *v == k {
+                count += 1;
+            }
+        }
+
+        ans = ans.max(count);
+    }
+
+    println!("{}", ans);
 }

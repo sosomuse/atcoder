8218412301228fbbd198edf3debdbf2035af4f9f
So Nishimura <s.nishimura@gemcook.com>
2022-06-11 18:45:55 +0900

    feat: abc253

--- abc252/src/bin/c.rs
+++ abc252/src/bin/c.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u8,
+        r: [String; n],
+    }
+
+    let mut cnt = vec![vec![0; 10]; 10];
+
+    for s in r.iter() {
+        for (j, c) in s.chars().enumerate() {
+            let c = c.to_digit(10).unwrap() as usize;
+            cnt[c][j] += 1;
+        }
+    }
+
+    let mut ans = 100000;
+
+    // dbg!(&cnt);
+
+    for i in 0..10 {
+        let mut max = 0;
+        for j in 0..10 {
+            max = max.max(10 * (cnt[i][j] as isize - 1) + j as isize);
+        }
+        ans = ans.min(max);
+    }
+
+    print!("{}", ans);
 }

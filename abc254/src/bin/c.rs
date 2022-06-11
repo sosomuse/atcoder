c79dda52a784dbf2c7e2c6462287a77e7bf90d50
So Nishimura <s.nishimura@gemcook.com>
2022-06-11 13:49:03 +0900

    feat: abc254c

--- abc254/src/bin/c.rs
+++ abc254/src/bin/c.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut vec = vec![vec![]; k];
+
+    for (i, v) in a.iter().enumerate() {
+        let p = i % k;
+        vec[p].push(*v);
+    }
+
+    for v in vec.iter_mut() {
+        v.sort();
+    }
+
+    let mut c = 0;
+
+    for i in 0..n {
+        let p = i % k;
+        let v = vec[p][i / k];
+        if c > v {
+            println!("No");
+            return;
+        }
+
+        c = v;
+    }
+
+    println!("Yes");
 }

1ef2e1a9fc03bd506a58663d6264e5456402a8e5
So Nishimura <s.nishimura@gemcook.com>
2022-06-12 23:09:52 +0900

    feat: abc255 d

--- abc255/src/bin/d.rs
+++ abc255/src/bin/d.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+        q: u64,
+        mut a: [u64; n],
+    }
+
+    a.sort();
+
+    for _ in 0..q {
+        input! {
+            x: u64,
+        }
+
+        let b = a.binary_search(&x).unwrap_or_else(|i| i);
+        dbg!(b);
+        let mins = a[..b].iter().sum::<u64>() / a[..b].len() as u64;
+        let maxs = a[b..].iter().sum::<u64>() / a[..b].len() as u64;
+
+        let ans = mins + maxs;
+
+        println!("{}", ans);
+    }
 }

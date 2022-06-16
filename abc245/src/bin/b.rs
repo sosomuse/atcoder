44f3cb3874972a295940644c5d80b93c356b96fa
So Nishimura <s.nishimura@gemcook.com>
2022-06-16 13:14:32 +0900

    feat: abc245

--- abc245/src/bin/b.rs
+++ abc245/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [isize; n],
+    }
+
+    a.sort();
+
+    let mut ans: isize = -1;
+
+    for v in a {
+        if v == ans || v == ans + 1 {
+            ans = v;
+        } else {
+            println!("{}", ans + 1);
+            return;
+        }
+    }
+
+    println!("{}", ans + 1)
 }

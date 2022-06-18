66e714708b593b13484c51e3b60ea9b4f22eb338
So Nishimura <s.nishimura@gemcook.com>
2022-06-18 17:00:06 +0900

    feat: abc247

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut k: usize,
+        x: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort();
+    a.reverse();
+
+    for v in a.iter_mut() {
+        if k == 0 {
+            break;
+        }
+
+        if *v >= x {
+            let count = (*v / x).min(k);
+            k -= count;
+            *v -= x * count;
+        }
+    }
+
+    a.sort();
+    a.reverse();
+
+    for v in a.iter_mut() {
+        if k == 0 {
+            break;
+        }
+
+        k -= 1;
+        *v = 0;
+    }
+
+    println!("{}", a.iter().sum::<usize>());
 }
--- abc247/src/bin/c.rs
+++ abc247/src/bin/c.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let vec = Vec::new();
+
+    let ans = calc(0, n, vec);
+
+    for v in ans {
+        print!("{} ", v);
+    }
+}
+
+fn calc(mut c: usize, n: usize, mut vec: Vec<usize>) -> Vec<usize> {
+    if c == n {
+        return vec;
+    }
+
+    let mut vec2 = vec.clone();
+    c += 1;
+    vec.push(c);
+
+    if c > 1 {
+        vec.append(&mut vec2);
+    }
+
+    calc(c, n, vec)
 }

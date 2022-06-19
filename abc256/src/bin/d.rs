bc23174ec7020343070f8736b9e7f5e3c05ba7b0
So Nishimura <s.nishimura@gemcook.com>
2022-06-19 23:28:49 +0900

    feat: abc256 d

--- abc256/src/bin/d.rs
+++ abc256/src/bin/d.rs
@@ -3,28 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        lr: [(usize, usize); n],
+        mut lr: [(usize, usize); n],
     }
+
+    lr.sort_by(|a, b| a.0.cmp(&b.0));
 
     let mut vec: Vec<(usize, usize)> = vec![];
 
     vec.push(lr[0]);
 
     for (l, r) in lr {
-        for i in 0..vec.len() {
-            let (l2, r2) = vec.get_mut(i).unwrap();
-            if *r2 >= l && *r2 < r {
-                *r2 = r;
-            }
-
-            if *l2 > l && *l2 <= r {
-                *r2 = r;
-            }
-
-            if l > *l2 && r > *r2 {
-                vec.push((l, r));
-                continue;
-            }
+        let (_, r2) = vec.last_mut().unwrap();
+        if *r2 >= l {
+            *r2 = r.max(*r2);
+        } else {
+            vec.push((l, r));
         }
     }
 

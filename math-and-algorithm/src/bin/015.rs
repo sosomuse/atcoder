58288f0d2fbd1f25780d01bf379ce1db815bfcb6
So Nishimura <s.nishimura@gemcook.com>
2022-06-22 21:14:02 +0900

    feat: math-algo 015

--- math-and-algorithm/src/bin/015.rs
+++ math-and-algorithm/src/bin/015.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    println!("{}", gcd(a, b));
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    let mut r = a % b;
+    while r != 0 {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+
+    return b;
 }

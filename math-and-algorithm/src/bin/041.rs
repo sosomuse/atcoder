45737a7a8607d00cd6ef65c3bf2519052d754eca
So Nishimura <s.nishimura@gemcook.com>
2022-06-24 07:38:16 +0900

    feat: math-algo 041

--- math-and-algorithm/src/bin/041.rs
+++ math-and-algorithm/src/bin/041.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    }
+
+    let mut vec = vec![0; t + 1];
+    let mut vec2 = vec![0; t + 1];
+
+    for (l, r) in lr {
+        vec[l] += 1;
+        vec[r] -= 1;
+    }
+
+    for i in 0..t {
+        if i == 0 {
+            vec2[i] = vec[i];
+        } else {
+            vec2[i] = vec2[i - 1] + vec[i];
+        }
+    }
+
+    for i in 0..t {
+        let v = vec2[i];
+        println!("{}", v);
+    }
 }

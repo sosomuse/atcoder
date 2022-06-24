56657d785b8749c3a3ee6c5b16f744aca92af94b
So Nishimura <s.nishimura@gemcook.com>
2022-06-24 22:10:41 +0900

    feat: math algo 043

--- math-and-algorithm/src/bin/043.rs
+++ math-and-algorithm/src/bin/043.rs
@@ -4,33 +4,34 @@ fn main() {
     input! {
         n: usize,
         m: usize,
-        ab: [(usize, usize); n],
+        ab: [(usize, usize); m],
     }
 
     let mut graph: Vec<Vec<usize>> = vec![vec![]; n + 1];
     let mut visited: Vec<bool> = vec![false; graph.len()];
-    let mut ans = vec![];
+    let mut ans: Vec<usize> = vec![];
 
     for (a, b) in ab {
         graph[a].push(b);
+        graph[b].push(a);
     }
 
     dfs(1, &graph, &mut visited, &mut ans);
 
     if ans.len() == n {
-        println!("The graph is connected.");
+        println!("The graph is connected");
     } else {
-        println!(" The graph is not connected.");
+        println!("The graph is not connected");
     }
 }
 
-fn dfs(v: usize, g: &Vec<Vec<usize>>, visited: &mut Vec<bool>, ans: &mut Vec<usize>) {
+fn dfs(v: usize, graph: &Vec<Vec<usize>>, visited: &mut Vec<bool>, ans: &mut Vec<usize>) {
     visited[v] = true;
     ans.push(v);
 
-    for &next in g[v].iter() {
-        if !visited[next] {
-            dfs(next, g, visited, ans);
+    for &w in graph[v].iter() {
+        if !visited[w] {
+            dfs(w, graph, visited, ans);
         }
     }
 }

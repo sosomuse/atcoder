a2a03e1fd46fcfab6a9affd9707d493123bcd5fa
So Nishimura <s.nishimura@gemcook.com>
2022-06-25 15:47:41 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/053.rs
+++ math-and-algorithm/src/bin/053.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let _mod = 1000000007;
+
+    println!("{}", modpow(4, n, _mod))
+}
+
+fn modpow(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut ans = 1;
+
+    for i in 0..30 {
+        if b & (1 << i) != 0 {
+            ans = ans * p % m;
+        }
+        p = p * p % m;
+    }
+
+    ans
 }

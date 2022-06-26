a48f9b5dae32651017f053185312864b2ba8ee34
So Nishimura <s.nishimura@gemcook.com>
2022-06-26 20:11:52 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/062.rs
+++ math-and-algorithm/src/bin/062.rs
@@ -1,3 +1,32 @@
+use proconio::{marker::Usize1, *};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut k: usize,
+        a: [Usize1 ;n]
+    }
+
+    let mut c = vec![0; n];
+    let mut v = 0;
+    let mut count = 0;
+
+    while c[v] == 0 {
+        if count == k {
+            println!("{}", v + 1);
+            return;
+        }
+        c[v] = count;
+        v = a[v];
+        count += 1;
+    }
+
+    let len = count - c[v];
+    k = (k - c[v]) % len;
+
+    for _ in 0..k {
+        v = a[v];
+    }
+
+    println!("{}", v + 1);
 }

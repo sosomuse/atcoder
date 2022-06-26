4a450bbc6f44c938879aab774372e2383bd25838
So Nishimura <s.nishimura@gemcook.com>
2022-06-26 20:27:04 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/064.rs
+++ math-and-algorithm/src/bin/064.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let sum = a.iter().sum::<usize>();
+
+    if sum % 2 != k % 2 {
+        println!("No");
+    } else if sum > k {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }

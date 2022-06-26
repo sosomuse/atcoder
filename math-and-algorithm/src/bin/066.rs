0e404cb4784748c67ce8ea28765e1276712cc60e
So Nishimura <s.nishimura@gemcook.com>
2022-06-26 20:47:50 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/066.rs
+++ math-and-algorithm/src/bin/066.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: isize,
+        k: isize,
+    };
+
+    let mut v = 0;
+
+    for a in 1..=n {
+        for b in 1.max(a - (k - 1))..=n.min(a + (k - 1)) {
+            for c in 1.max(a - (k - 1))..=n.min(a + (k - 1)) {
+                if (b - c).abs() <= k - 1 {
+                    v += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", n.pow(3) - v);
 }

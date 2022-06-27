1856ed3d7a6946f848e4625de0fdc26e9fa35480
So Nishimura <s.nishimura@gemcook.com>
2022-06-27 13:59:34 +0900

    feat: abc244 c

--- abc244/src/bin/c.rs
+++ abc244/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut vec: Vec<usize> = vec![];
+
+    for i in 1..=n {
+        vec.push(i);
+    }
+
+    while vec.len() > 0 {
+        println!("{}", vec[0]);
+
+        input! {
+            o: usize,
+        };
+
+        vec.remove(o);
+    }
 }

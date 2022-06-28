e35fae1aa3724299e7e54f9e0efcf0fa394916b5
So Nishimura <s.nishimura@gemcook.com>
2022-06-28 13:59:49 +0900

    feat: math

--- math-and-algorithm/src/bin/072.rs
+++ math-and-algorithm/src/bin/072.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: isize,
+        b: isize,
+    };
+
+    let solve = |t: isize| {
+        let cl = (a + t - 1) / t;
+        let cr = b / t;
+        if cr - cl >= 1 {
+            true
+        } else {
+            false
+        }
+    };
+
+    let mut ans = 0;
+
+    for i in 1..=b {
+        if solve(i) {
+            ans = i;
+        }
+    }
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/073.rs
+++ math-and-algorithm/src/bin/073.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    // mod取ること
+    // 自分だけを選ぶパターン（aのsum）
+    // 自分が最大のパターン 1.2.3.4 1.4 2.4 3,4 1,2,4 1,3,4 2,3,4 1,2,3,4
 }

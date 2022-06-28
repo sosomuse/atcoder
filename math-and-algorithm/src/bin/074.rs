d4935e5bfbd5ef636ac9e98ec55cdd5c4171c9a7
So Nishimura <s.nishimura@gemcook.com>
2022-06-29 01:03:10 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/074.rs
+++ math-and-algorithm/src/bin/074.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: isize,
+        a: [isize; n],
+    };
+
+    let mut ans = 0;
+
+    for i in 1..=n {
+        ans += a[(i as usize) - 1] * (-n + 2 * i - 1);
+    }
+
+    println!("{}", ans);
 }

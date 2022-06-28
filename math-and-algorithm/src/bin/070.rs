81e82031e8064684174c4a3094ad5ecaba85d437
So Nishimura <s.nishimura@gemcook.com>
2022-06-28 09:29:00 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/070.rs
+++ math-and-algorithm/src/bin/070.rs
@@ -1,3 +1,42 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        xy: [(isize, isize); n],
+    };
+
+    let is_includes = |lx: isize, rx: isize, ly: isize, ry: isize| {
+        let mut count = 0;
+        for i in 0..n {
+            let (x, y) = xy[i];
+            if lx <= x && x <= rx && ly <= y && y <= ry {
+                count += 1;
+            }
+        }
+
+        count
+    };
+
+    let mut ans: usize = 10000000000000000000;
+
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                for l in 0..n {
+                    let (cl, _) = xy[i];
+                    let (cr, _) = xy[j];
+                    let (_, dl) = xy[k];
+                    let (_, dr) = xy[l];
+                    if is_includes(cl, cr, dl, dr) >= m {
+                        let area = 1 * (cr - cl) * (dr - dl);
+                        ans = ans.min(area as usize);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }

88614a21e2940527506f61e8d6f60a05a14eca12
So Nishimura <s.nishimura@gemcook.com>
2022-06-30 02:03:28 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/084.rs
+++ math-and-algorithm/src/bin/084.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: isize,
+        b: isize,
+        c: isize,
+    };
+
+    if c - a - b < 0 {
+        println!("No");
+        return;
+    }
+
+    if 4 * a * b < (c - a - b).pow(2) {
+        println!("Yes");
+        return;
+    }
+
+    println!("No");
 }

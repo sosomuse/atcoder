9a7214c8fbc7e218bbc7dab5ff2eb000dd071de7
So Nishimura <s.nishimura@gemcook.com>
2022-06-30 02:07:46 +0900

    feat: math algo

--- math-and-algorithm/src/bin/085.rs
+++ math-and-algorithm/src/bin/085.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: isize,
+        x: isize,
+        y: isize,
+    };
+
+    for i in 1..=n {
+        for j in i..=n {
+            for k in j..=n {
+                for l in k..=n {
+                    if i + j + k + l == x && i * j * k * l == y {
+                        println!("Yes");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("No");
 }

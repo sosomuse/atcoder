b53691ff0b269ffd6aaf5f8f8fbe69b6ff3e82f8
So Nishimura <s.nishimura@gemcook.com>
2022-06-30 01:54:37 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/083.rs
+++ math-and-algorithm/src/bin/083.rs
@@ -1,3 +1,23 @@
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        mut b: [usize; n],
+    };
+
+    a.sort();
+    b.sort();
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        let v = a[i];
+        let pos = b.binary_search(&v).unwrap_or_else(|i| i.max(1) - 1);
+        let target = b[pos];
+        ans += (v as isize - target as isize).abs();
+        b.remove(pos);
+    }
+
+    println!("{}", ans);
 }

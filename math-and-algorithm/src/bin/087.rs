b3538a229900168fe2b2e9447a13b4bcaa830740
So Nishimura <s.nishimura@gemcook.com>
2022-07-01 00:16:40 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/087.rs
+++ math-and-algorithm/src/bin/087.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize
+    };
+
+    let ans = ((n * (n + 1) / 2) % 1000000007).pow(2);
+
+    println!("{}", ans % 1000000007);
 }

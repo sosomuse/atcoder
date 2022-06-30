1b4ae36f08eeebad6762927dd90e31ea9c07af0f
So Nishimura <s.nishimura@gemcook.com>
2022-06-30 13:47:25 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/086.rs
+++ math-and-algorithm/src/bin/086.rs
@@ -1,3 +1,31 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+
+    let mut depth = 0;
+
+    for i in 0..n {
+        let v = s[i];
+        if v == '(' {
+            depth += 1;
+        };
+        if v == ')' {
+            depth -= 1;
+        }
+
+        if depth < 0 {
+            println!("No");
+            return;
+        }
+    }
+
+    if depth == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

912de8e24aebcef380005d2b314ed5b53ce3cd40
So Nishimura <s.nishimura@gemcook.com>
2022-07-01 00:21:08 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/088.rs
+++ math-and-algorithm/src/bin/088.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
+const MOD: usize = 998244353;
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+
+    let ans = solve(a % MOD) * solve(b % MOD) % MOD * solve(c % MOD);
+
+    println!("{}", ans % MOD);
+}
+
+fn solve(n: usize) -> usize {
+    (n * (n + 1) / 2) % MOD
 }

64b3b5250177312934a5a08d7a2a5b4b7217b04c
So Nishimura <s.nishimura@gemcook.com>
2022-07-02 20:18:44 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/094.rs
+++ math-and-algorithm/src/bin/094.rs
@@ -3,6 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        b: [usize; n],
+        b: [usize; n - 1],
     };
+
+    let mut ans = b[0] + b[n - 2];
+
+    for i in 1..n - 1 {
+        ans += b[i - 1].min(b[i]);
+    }
+
+    println!("{}", ans);
 }

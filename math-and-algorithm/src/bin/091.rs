66df4c8074cd90b7b5b24d9dc3f442be7715caa7
So Nishimura <s.nishimura@gemcook.com>
2022-07-02 19:15:37 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/091.rs
+++ math-and-algorithm/src/bin/091.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x: usize
+    };
+
+    let mut count = 0;
+
+    for a in 1..=n {
+        for b in 1..=n {
+            for c in 1..=n {
+                if a < b && b < c && c <= n {
+                    if a + b + c == x {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
 }

490ee184140f12730fe34206cf3eec9aefa0ca5b
So Nishimura <s.nishimura@gemcook.com>
2022-07-02 19:49:02 +0900

    feat: math-algo

--- math-and-algorithm/src/bin/092.rs
+++ math-and-algorithm/src/bin/092.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans: usize = 10000000000000;
+
+    for i in 1..((n as f64).sqrt().ceil() as usize) + 1 {
+        if n % i == 0 {
+            let v = n / i;
+            ans = ans.min((i + v) * 2);
+        }
+    }
+
+    println!("{}", ans);
 }

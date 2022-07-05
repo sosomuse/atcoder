fbe24f8e3203c16f8dcbedffc1a8c3d15fa045bf
So Nishimura <s.nishimura@gemcook.com>
2022-07-05 13:55:08 +0900

    feat: 096

--- math-and-algorithm/src/bin/096.rs
+++ math-and-algorithm/src/bin/096.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut t: [usize; n],
+    };
+
+    let s: usize = t.iter().sum();
+    let mut dp = vec![vec![0; s + 1]; n + 1];
+
+    let ans = 0;
+
+    println!("{}", ans);
 }

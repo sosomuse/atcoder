ee3f31904848221f107ef2f95047aa68809f92aa
So Nishimura <s.nishimura@gemcook.com>
2022-07-05 23:04:21 +0900

    feat: abc241  b

--- abc241/src/bin/b.rs
+++ abc241/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        mut b: [usize; m],
+    };
+
+    let mut ans = true;
+
+    a.sort();
+    b.sort();
+
+    for i in 0..m {
+        let x = b[i];
+        let y = a.binary_search(&x);
+
+        match y {
+            Ok(y) => {
+                a.remove(y);
+            }
+            Err(_) => {
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }

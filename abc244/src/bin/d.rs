1186e4b42d7a85936a250f36faac54b50c1e57c0
So Nishimura <s.nishimura@gemcook.com>
2022-07-06 23:42:44 +0900

    feat: d

--- abc244/src/bin/d.rs
+++ abc244/src/bin/d.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: [char; 3],
+        t: [char; 3]
+    }
+
+    let mut cnt = 0;
+
+    for i in 0..3 {
+        if s[i] != t[i] {
+            cnt += 1;
+        }
+    }
+
+    if cnt > 0 && cnt % 2 == 0 {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }

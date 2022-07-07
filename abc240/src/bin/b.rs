b26ed5053cded9abe082764e9d3844dbfce7b7f4
So Nishimura <s.nishimura@gemcook.com>
2022-07-07 13:38:09 +0900

    feat: abc240

--- abc240/src/bin/b.rs
+++ abc240/src/bin/b.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut set = HashSet::<usize>::new();
+
+    for v in a {
+        set.insert(v);
+    }
+
+    println!("{}", set.len());
 }

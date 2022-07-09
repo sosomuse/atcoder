eb3a59894dedee8fcf35e454069808381d1a3f93
So Nishimura <s.nishimura@gemcook.com>
2022-07-09 19:51:44 +0900

    feat: abc185

--- abc185/src/bin/d.rs
+++ abc185/src/bin/d.rs
@@ -1,3 +1,62 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; m],
+    };
+
+    a.sort();
+
+    let mut p = 1000000000000000000;
+
+    if m == 0 {
+        println!("{}", 1);
+        return;
+    }
+
+    for i in 0..m {
+        if i == 0 {
+            let diff = a[i] - 1;
+            if diff >= 1 {
+                p = p.min(diff).max(1);
+            }
+            continue;
+        }
+
+        if i == m - 1 {
+            let diff = n - a[i];
+            if diff >= 1 {
+                p = p.min(diff).max(1);
+            }
+        }
+
+        let diff = (a[i] - a[i - 1]) - 1;
+        p = p.min(diff).max(1);
+    }
+
+    let mut ans = 0;
+
+    for i in 0..m {
+        if i == 0 {
+            let diff = a[i] - 1;
+            if diff >= 1 {
+                ans += (diff as f64 / p as f64).ceil() as usize;
+            }
+            continue;
+        }
+
+        if i == m - 1 {
+            let diff = n - a[i];
+            if diff >= 1 {
+                ans += (diff as f64 / p as f64).ceil() as usize;
+            }
+        }
+
+        let diff = (a[i] - a[i - 1]) - 1;
+        ans += (diff as f64 / p as f64).ceil() as usize;
+    }
+
+    println!("{}", ans);
 }

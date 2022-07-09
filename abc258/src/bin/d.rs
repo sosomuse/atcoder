403fc5f091787fa676f3b7d66d9a8c09e2add754
So Nishimura <s.nishimura@gemcook.com>
2022-07-09 14:17:21 +0900

    abc258

--- abc258/src/bin/d.rs
+++ abc258/src/bin/d.rs
@@ -6,4 +6,21 @@ fn main() {
         x: usize,
         ab: [(usize, usize); n],
     };
+
+    let mut ans: usize = 10000000000000000000;
+    let mut s: usize = 0;
+    let mut l: usize = 10000000000000000000;
+
+    for i in 0..n {
+        s += ab[i].0 + ab[i].1;
+        l = l.min(ab[i].1);
+        if x < i + 1 {
+            break;
+        }
+
+        let now = s + l * (x - i - 1);
+        ans = ans.min(now);
+    }
+
+    println!("{}", ans);
 }

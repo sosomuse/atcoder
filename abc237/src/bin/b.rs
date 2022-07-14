58e0ede271423cdc17d46607bfdb2f15081622ed
So Nishimura <s.nishimura@gemcook.com>
2022-07-14 23:46:27 +0900

    feat: abc237

--- abc237/src/bin/b.rs
+++ abc237/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
+    };
+
+    for i in 0..w {
+        for j in 0..h {
+            println!("{} ", a[j][i]);
+        }
+    }
 }

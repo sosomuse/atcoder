b4ee7a772eb765a40e6910c266cb80e38232d3ff
So Nishimura <s.nishimura@gemcook.com>
2022-07-14 23:43:44 +0900

    feat: abc237 a

--- abc237/src/bin/a.rs
+++ abc237/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i128,
+    };
+
+    if n >= -2_i128.pow(31) && n < 2_i128.pow(31) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

166ffde4b8722015c3bbf5cd866fb26b8e2123dd
So Nishimura <s.nishimura@gemcook.com>
2022-07-15 13:36:43 +0900

    feat: abc 239 c

--- abc239/src/bin/c.rs
+++ abc239/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x1: isize,
+        y1: isize,
+        x2: isize,
+        y2: isize,
+    };
+
+    for i in x1 - 3..x1 + 3 {
+        for j in y1 - 3..y1 + 3 {
+            if solve(x1, y1, i, j) && solve(x2, y2, i, j) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
+}
+
+fn solve(x1: isize, y1: isize, x2: isize, y2: isize) -> bool {
+    (x1 - x2).pow(2) + (y1 - y2).pow(2) == 5
 }

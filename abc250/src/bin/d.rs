4d9905bb36c3c85ecf4d2dc0b71c4b06e6752441
So Nishimura <s.nishimura@gemcook.com>
2022-07-16 23:02:14 +0900

    feat: utils

--- abc250/src/bin/d.rs
+++ abc250/src/bin/d.rs
@@ -1,3 +1,51 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: f64,
+    };
+
+    let x = n.powf(0.333334) as usize;
+    let primes = solve(x);
+    let mut ans = 0;
+
+    for i in 0..primes.len() {
+        let v = primes[i];
+
+        for j in i + 1..primes.len() {
+            let w = primes[j];
+            if v * w * w <= n as usize / w {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn solve(n: usize) -> Vec<usize> {
+    let mut flags = vec![true; n + 1];
+    flags[0] = false;
+    flags[1] = false;
+
+    let x = (n as f64).sqrt() as usize;
+    for p in 2..=x {
+        if !flags[p] {
+            continue;
+        }
+
+        for m in ((p * p)..=n).step_by(p) {
+            flags[m] = false;
+        }
+    }
+
+    let mut ans: Vec<usize> = vec![];
+
+    for (i, v) in flags.iter().enumerate() {
+        if *v {
+            ans.push(i);
+        }
+    }
+
+    ans
 }
--- utils/src/bin/prime.rs
+++ utils/src/bin/prime.rs
@@ -4,6 +4,9 @@ fn main() {
     } else {
         println!("No");
     }
+
+    let primes = solve(100);
+    println!("{:?}", primes);
 }
 
 // O(√n)
@@ -18,4 +21,33 @@ fn is_prime(n: usize) -> bool {
     }
 
     return true;
+}
+
+// エラトステネスの篩
+// O(n log log n)
+fn solve(n: usize) -> Vec<usize> {
+    let mut flags = vec![true; n + 1];
+    flags[0] = false;
+    flags[1] = false;
+
+    let x = (n as f64).sqrt() as usize;
+    for p in 2..=x {
+        if !flags[p] {
+            continue;
+        }
+
+        for m in ((p * p)..=n).step_by(p) {
+            flags[m] = false;
+        }
+    }
+
+    let mut ans: Vec<usize> = vec![];
+
+    for (i, v) in flags.iter().enumerate() {
+        if *v {
+            ans.push(i);
+        }
+    }
+
+    ans
 }

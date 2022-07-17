d165d59e0ce8de7655bac79b291b91781dab0170
So Nishimura <s.nishimura@gemcook.com>
2022-07-17 22:29:15 +0900

    feat: abc260

--- abc260/src/bin/d.rs
+++ abc260/src/bin/d.rs
@@ -3,56 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        k: usize,
-        p: [usize; n],
+        // k: usize,
+        // p: [usize; n],
     };
 
-    if n == 1 {
-        println!("{}", 0);
-        return;
-    }
+    // let vec: Vec<isize> = vec![-1; n];
+    // let vec2: Vec<Vec<usize>> = vec![];
+    // let min = (0, 0);
 
-    let mut red = vec![0; n + 1];
-    let mut blue = vec![0; n + 1];
+    // for i in 0..n {}
 
-    red[n] = 1;
-
-    loop {
-        let mut tmp = false;
-        for i in 0..n + 1 {
-            if red[i] >= 1 {
-                if i == 1 {
-                    continue;
-                }
-
-                tmp = true;
-
-                let v = red[i];
-                red[i] = 0;
-                red[i - 1] += v;
-                blue[i] += v * x;
-            }
-        }
-
-        for i in 0..n + 1 {
-            if blue[i] > 1 {
-                if i == 1 {
-                    continue;
-                }
-
-                tmp = true;
-
-                let v = blue[i];
-                blue[i] = 0;
-                blue[i - 1] += v * y;
-                red[i - 1] += v;
-            }
-        }
-
-        if !tmp {
-            break;
-        }
-    }
-
-    println!("{}", blue[1]);
+    println!("{}", n);
 }

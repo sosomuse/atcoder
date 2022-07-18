3bf941348be7960a512e46efd772c212c3ce19e6
So Nishimura <s.nishimura@gemcook.com>
2022-07-18 22:56:07 +0900

    feat: abc236 b

--- abc236/src/bin/b.rs
+++ abc236/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n * 4 - 1],
+    }
+
+    let mut vec = vec![0; n];
+
+    for i in 0..a.len() {
+        vec[a[i] - 1] += 1;
+    }
+
+    for i in 0..n {
+        if vec[i] != 4 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }

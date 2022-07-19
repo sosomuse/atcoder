9a55fa1c66b8aa7a97dafcfebcc531b02b265ebf
So Nishimura <s.nishimura@gemcook.com>
2022-07-19 13:56:20 +0900

    feat: abc235 a

--- abc235/src/bin/a.rs
+++ abc235/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        abc: usize,
+    };
+
+    let a = abc / 100;
+    let b = abc % 100 / 10;
+    let c = abc % 10;
+
+    let bca = b * 100 + c * 10 + a;
+    let cab = c * 100 + a * 10 + b;
+    println!("{}", abc + bca + cab)
 }

3dd81adbcd41264bf1fa8f94cde43489c79a67c3
So Nishimura <s.nishimura@gemcook.com>
2022-07-20 13:12:25 +0900

    feat: abc242 c

--- abc242/src/bin/c.rs
+++ abc242/src/bin/c.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
+static MOD: u32 = 998244353;
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut dp = vec![vec![0; 10]; n + 1];
+
+    for i in 1..=9 {
+        dp[1][i] = 1;
+    }
+
+    for d in 2..=n {
+        for i in 1..=9 {
+            for j in 1.max(i - 1)..=9.min(i + 1) {
+                dp[d][j] += dp[d - 1][i];
+                dp[d][j] %= MOD;
+            }
+        }
+    }
+
+    let mut ans = 0;
+
+    for i in 1..=9 {
+        ans += dp[n][i];
+        ans %= MOD;
+    }
+
+    println!("{}", ans);
 }

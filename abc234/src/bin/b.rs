1e97151a82e17dbb0c647f3123ba1b20ae1c47cd
So Nishimura <s.nishimura@gemcook.com>
2022-07-24 22:53:34 +0900

    feat: abc234 b

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        xy: [(isize, isize); n],
+    };
+
+    let mut ans: f64 = 0.;
+
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+
+            let (x1, y1) = xy[i];
+            let (x2, y2) = xy[j];
+
+            ans = ans.max((((x1 - x2).pow(2) + (y1 - y2).pow(2)) as f64).sqrt());
+        }
+    }
+
+    println!("{}", ans);
 }

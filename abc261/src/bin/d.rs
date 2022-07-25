becca2213b6c85f22b1110c0695b2ef3935b9bf3
So Nishimura <s.nishimura@gemcook.com>
2022-07-25 22:57:39 +0900

    feat: abc261 d

--- abc261/src/bin/d.rs
+++ abc261/src/bin/d.rs
@@ -1,3 +1,41 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        x: [usize; n],
+        cy: [(usize,usize); m],
+    };
+
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    let mut vec = vec![0; n + 1];
+
+    for (c, y) in cy.iter() {
+        vec[*c] = *y;
+    }
+
+    // 何回目のコイントスか
+    for i in 1..=n {
+        let v = x[i - 1];
+
+        // 連続何回表が出たか
+        for j in 1..=n {
+            if i < j {
+                break;
+            }
+            dp[i][0] = dp[i][0].max(dp[i - 1][j]);
+            dp[i][j] = dp[i - 1][j - 1] + v + vec[j];
+        }
+    }
+
+    let mut ans = 0;
+
+    for i in 1..=n {
+        for j in 1..=n {
+            ans = ans.max(dp[i][j]);
+        }
+    }
+
+    println!("{}", ans);
 }

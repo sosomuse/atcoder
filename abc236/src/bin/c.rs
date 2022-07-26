86b9ded51c59e565c82e0bc38a6eb7e1821fd18e
So Nishimura <s.nishimura@gemcook.com>
2022-07-26 23:50:36 +0900

    feat: abc236 c

--- abc236/src/bin/c.rs
+++ abc236/src/bin/c.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [String; n],
+        t: [String; m],
+    }
+
+    let mut map = HashMap::<String, bool>::new();
+
+    for v in t.iter() {
+        map.insert(v.to_string(), true);
+    }
+
+    for v in s.iter() {
+        if map.contains_key(v) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }

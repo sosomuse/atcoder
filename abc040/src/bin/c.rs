7243b561b232bd826c58e574eba0afafb24f56b6
So Nishimura <s.nishimura@gemcook.com>
2022-07-27 20:32:46 +0900

    feat: abc040 c

--- abc040/src/bin/c.rs
+++ abc040/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [isize; n],
+    };
+
+    let mut dp = vec![0; n + 1];
+    dp[1] = 0;
+
+    for i in 2..=n {
+        if i == 2 {
+            dp[i] = dp[i - 1] + (a[i - 1] - a[i - 2]).abs();
+        } else {
+            dp[i] = (dp[i - 1] + (a[i - 2] - a[i - 1]).abs())
+                .min(dp[i - 2] + (a[i - 3] - a[i - 1]).abs());
+        }
+    }
+
+    println!("{}", dp[n]);
 }

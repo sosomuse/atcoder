54df7b710d289509620e9f0a28792933898be977
So Nishimura <s.nishimura@gemcook.com>
2022-07-27 20:51:29 +0900

    feat: dp b

--- dp/src/bin/b.rs
+++ dp/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        h: [isize; n],
+    };
+
+    let mut dp = vec![100000000000; n + 1];
+    dp[1] = 0;
+
+    for i in 2..=n {
+        if i == 2 {
+            dp[i] = (h[i - 1] - h[i - 2]).abs();
+        }
+        for j in ((1).max((i as isize) - k as isize) as usize)..i {
+            dp[i] = dp[i].min(dp[j] + (h[j - 1] - h[i - 1]).abs());
+        }
+    }
+
+    println!("{}", dp[n]);
 }

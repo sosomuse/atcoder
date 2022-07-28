0e4fb8bd83f0522c9535382a8d4d47a3c050e97e
So Nishimura <s.nishimura@gemcook.com>
2022-07-28 23:13:58 +0900

    feat: abc240 c

--- abc240/src/bin/c.rs
+++ abc240/src/bin/c.rs
@@ -7,12 +7,26 @@ fn main() {
         ab: [(usize, usize); n],
     };
 
-    let mut dp = vec![vec![false; x]; n];
+    let mut dp = vec![vec![false; x + 1]; n + 1];
     dp[0][0] = true;
 
-    for i in 0..n {
-        let (a, b) = ab[i];
+    for i in 1..=n {
+        let (a, b) = ab[i - 1];
+
+        for j in 0..=x {
+            if j >= a && dp[i - 1][j - a] {
+                dp[i][j] = true;
+            }
+
+            if j >= b && dp[i - 1][j - b] {
+                dp[i][j] = true;
+            }
+        }
     }
 
-    println!("{}", dp[0][0]);
+    if dp[n][x] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

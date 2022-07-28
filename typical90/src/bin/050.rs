80dd843650f3b4786bab151cd078172791e357e0
So Nishimura <s.nishimura@gemcook.com>
2022-07-28 22:23:02 +0900

    feat: dp 6

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        l: usize,
+    };
+
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for i in 2..=n {
+        dp[i] = dp[i - 1];
+
+        if i >= l {
+            dp[i] += dp[i - l];
+            dp[i] %= 10_u32.pow(9) + 7;
+        }
+    }
+
+    println!("{}", dp[n]);
 }

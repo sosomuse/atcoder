888e564eda62a74c2c4fcc25bf7a1d7b6c472f4f
So Nishimura <s.nishimura@gemcook.com>
2022-07-29 23:21:11 +0900

    feat: abc241

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -1,3 +1,66 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [Chars; n],
+    };
+
+    for i in 0..n {
+        for j in 0..n {
+            if i + 5 < n {
+                let mut cnt = 0;
+                for k in 0..6 {
+                    if s[i + k][j] == '#' {
+                        cnt += 1;
+                    }
+                }
+                if cnt >= 4 {
+                    println!("Yes");
+                    return;
+                }
+            }
+
+            if j + 5 < n {
+                let mut cnt = 0;
+                for k in 0..6 {
+                    if s[i][j + k] == '#' {
+                        cnt += 1;
+                    }
+                }
+                if cnt >= 4 {
+                    println!("Yes");
+                    return;
+                }
+            }
+
+            if i + 5 < n && j + 5 < n {
+                let mut cnt = 0;
+                for k in 0..6 {
+                    if s[i + k][j + k] == '#' {
+                        cnt += 1;
+                    }
+                }
+                if cnt >= 4 {
+                    println!("Yes");
+                    return;
+                }
+            }
+
+            if 0 <= (i as isize) - 5 && j + 5 < n {
+                let mut cnt = 0;
+                for k in 0..6 {
+                    if s[i - k][j + k] == '#' {
+                        cnt += 1;
+                    }
+                }
+                if cnt >= 4 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("No");
 }
--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        wu: [(usize, usize); n],
+    };
+
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+
+    for i in 1..=n {
+        let (u, v) = wu[i - 1];
+        dp[1][u] = dp[1][u].max(v);
+    }
+
+    dbg!(&dp);
+
+    // for i in 2..=n {
+    //     for j in 2..=w {
+    //         let v = wu[i - 1].0;
+    //         if j >= v {
+    //             dp[i][j] = dp[i - 1][j - v] + wu[i - 1].1;
+    //         } else {
+    //             dp[i][j] = dp[i - 1][j];
+    //         }
+    //     }
+    // }
 }

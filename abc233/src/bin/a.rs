3620d9dfd365c6bcda492e843a7dbfc09b88c97b
So Nishimura <s.nishimura@gemcook.com>
2022-07-30 23:36:40 +0900

    feat: abc233 a

--- abc233/src/bin/a.rs
+++ abc233/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+        y: usize,
+    };
+
+    if y <= x {
+        println!("0");
+        return;
+    }
+
+    let mut ans = (y - x) / 10;
+
+    if (y - x) % 10 != 0 {
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }

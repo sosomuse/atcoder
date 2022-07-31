af5febbc016b2ee8a68c2e08020f7606cc547d38
So Nishimura <s.nishimura@gemcook.com>
2022-07-31 19:59:04 +0900

    feat: abc252 d

--- abc252/src/bin/d.rs
+++ abc252/src/bin/d.rs
@@ -1,3 +1,48 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    };
+
+    a.sort();
+
+    let mut map = HashMap::<usize, usize>::new();
+    let mut ans = 0;
+    let mut cnt = 0;
+
+    for i in 0..n {
+        let v = a[i];
+        *map.entry(v).or_insert(0) += 1;
+    }
+
+    dbg!(ncr(4, 2));
+
+    for _ in 0..map.keys().len() - 2 {
+        let v = a[cnt];
+
+        let c = *map.get(&v).unwrap_or(&1);
+
+        dbg!(ans);
+
+        ans += ncr(n - cnt - c, 2) * c;
+        cnt += c;
+    }
+
+    println!("{}", ans);
+}
+
+fn ncr(n: usize, r: usize) -> usize {
+    let mut _n = 1;
+    let mut _r = 1;
+
+    for i in 0..r {
+        _n *= n - i;
+        _r *= i + 1;
+    }
+
+    _n / _r
 }
--- utils/src/bin/math.rs
+++ utils/src/bin/math.rs
@@ -1,6 +1,9 @@
 fn main() {
     let ans = rotate(2.0, 2.0, 180.0);
     println!("{} {}", ans.0, ans.1);
+
+    let ans = ncr(100, 3);
+    println!("{}", ans);
 }
 
 // 回転行列
@@ -11,4 +14,16 @@ fn rotate(x: f64, y: f64, d: f64) -> (f64, f64) {
     let y2 = q.sin() * x + q.cos() * y;
 
     (x2, y2)
+}
+
+fn ncr(n: usize, r: usize) -> usize {
+    let mut _n = 1;
+    let mut _r = 1;
+
+    for i in 0..r {
+        _n *= n - i;
+        _r *= i + 1;
+    }
+
+    _n / _r
 }

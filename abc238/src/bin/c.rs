7bbaa57ed6eebb6771a8279783a54243b5b34053
So Nishimura <s.nishimura@gemcook.com>
2022-08-01 22:29:21 +0900

    feat: abc238

--- abc238/src/bin/c.rs
+++ abc238/src/bin/c.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let c = n.to_string().len();
+    let mut ans = 0;
+
+    let _mod = 998244353;
+
+    for i in 1..=c {
+        if i != c {
+            let x = (10_usize.pow(i as u32) - 10_usize.pow(i as u32 - 1)) % _mod;
+            ans += (1 + x) * x / 2;
+            ans %= _mod;
+        } else {
+            let x = (n - (10_usize.pow(i as u32 - 1) - 1)) % _mod;
+            ans += (1 + x) * x / 2;
+            ans %= _mod;
+        }
+    }
+
+    println!("{}", ans);
 }

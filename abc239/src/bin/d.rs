c17aab703aaaec357dab7e88082d28822fdea903
So Nishimura <s.nishimura@gemcook.com>
2022-08-01 22:37:07 +0900

    feat: abc239 d

--- abc239/src/bin/d.rs
+++ abc239/src/bin/d.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+    };
+
+    for i in a..=b {
+        let mut tmp = false;
+
+        for j in c..=d {
+            if is_prime(i + j) {
+                tmp = true;
+            }
+        }
+
+        if !tmp {
+            println!("Takahashi");
+            return;
+        }
+    }
+
+    println!("Aoki");
+}
+
+fn is_prime(n: usize) -> bool {
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+
+        i += 1;
+    }
+
+    return true;
 }

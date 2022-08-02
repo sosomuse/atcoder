64a856c05f6ed38e4f7ca1d2ff7adee0fcea7d7d
So Nishimura <s.nishimura@gemcook.com>
2022-08-03 00:40:25 +0900

    feat: abc082

--- abc082/src/bin/b.rs
+++ abc082/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut s: Chars,
+        mut t: Chars,
+    };
+
+    s.sort();
+    t.sort_by(|a, b| b.cmp(a));
+
+    let ss = s.iter().collect::<String>();
+    let ts = t.iter().collect::<String>();
+
+    if ss < ts {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

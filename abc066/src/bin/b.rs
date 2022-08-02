5aa81e912a04d83a2eb768aa923ac9a568da8ebb
So Nishimura <s.nishimura@gemcook.com>
2022-08-03 00:58:00 +0900

    feat: abc066 b

--- abc066/src/bin/b.rs
+++ abc066/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    for i in 1..s.len() {
+        if (s.len() - i) % 2 == 1 {
+            continue;
+        };
+
+        let x = &s[0..(s.len() - i) / 2].iter().collect::<String>();
+        let y = &s[(s.len() - i) / 2..(s.len() - i)]
+            .iter()
+            .collect::<String>();
+
+        if x == y {
+            println!("{}", s.len() - i);
+            return;
+        }
+    }
 }

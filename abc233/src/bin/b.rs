5ec26b083cec5e9aafce176124d55646ecbc1e09
So Nishimura <s.nishimura@gemcook.com>
2022-08-03 00:36:10 +0900

    feat: abc233 b

--- abc233/src/bin/b.rs
+++ abc233/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        l: usize,
+        r: usize,
+        s: Chars,
+    };
+
+    for i in 0..l - 1 {
+        print!("{}", s[i]);
+    }
+
+    for i in (l - 1..r).rev() {
+        print!("{}", s[i]);
+    }
+
+    for i in r..s.len() {
+        print!("{}", s[i]);
+    }
 }

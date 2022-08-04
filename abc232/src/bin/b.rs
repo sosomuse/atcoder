7503a861aadf5f5cefe632419b8d7a849d8dc56e
So Nishimura <s.nishimura@gemcook.com>
2022-08-04 22:48:30 +0900

    feat: abc232 b

--- abc232/src/bin/b.rs
+++ abc232/src/bin/b.rs
@@ -2,9 +2,19 @@ use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        s: Chars,
+        mut s: Chars,
         t: Chars,
     };
 
-    for i in 0..s.len() {}
+    let sub = (t[0] as i8 - s[0] as i8 + 26) % 26;
+
+    for i in 0..s.len() {
+        s[i] = (((s[i] as i8) - b'a' as i8 + sub) % 26_i8 + b'a' as i8).abs() as u8 as char;
+    }
+
+    if s.iter().collect::<String>() == t.iter().collect::<String>() {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

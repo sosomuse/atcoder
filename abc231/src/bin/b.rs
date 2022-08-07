c7825ef8a6bfbe3d94b144487ccadb5ff2fdd5f3
So Nishimura <s.nishimura@gemcook.com>
2022-08-07 21:20:12 +0900

    feat: abc231 b

--- abc231/src/bin/b.rs
+++ abc231/src/bin/b.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+
+    let mut map = HashMap::<String, usize>::new();
+
+    for v in s.iter() {
+        *map.entry(v.to_string()).or_insert(0) += 1;
+    }
+
+    let max = map.values().max().unwrap();
+
+    for (k, v) in map.iter() {
+        if *v == *max {
+            println!("{}", k);
+            return;
+        }
+    }
 }

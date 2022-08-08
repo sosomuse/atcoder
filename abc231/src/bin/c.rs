5bd7542ae111ebca7cb1a13c2df8c325c55071a0
So Nishimura <s.nishimura@gemcook.com>
2022-08-08 22:16:02 +0900

    feat: abc231 c

--- abc231/src/bin/c.rs
+++ abc231/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort();
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+        };
+
+        let ans = a.binary_search(&x).unwrap_or_else(|e| e);
+
+        println!("{}", a.len() - ans);
+    }
 }

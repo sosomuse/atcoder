6521d5a1c286aa8e489a412fb60a5d0b8a59408a
So Nishimura <s.nishimura@gemcook.com>
2022-08-09 23:34:38 +0900

    feat: abc230 b

--- abc230/src/bin/b.rs
+++ abc230/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+
+    let x = "oxx";
+    let y = x.repeat(10_usize.pow(5));
+
+    if y.contains(&s) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

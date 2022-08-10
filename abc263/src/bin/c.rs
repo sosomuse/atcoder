55e86a01ada44aaf6092744b4325ebc50e0c0b7f
So Nishimura <s.nishimura@gemcook.com>
2022-08-10 20:52:36 +0900

    feat abc263 c

--- abc263/src/bin/c.rs
+++ abc263/src/bin/c.rs
@@ -1,5 +1,25 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize
+    }
+
+    let vec = {
+        let mut comb = (1..=m).combinations(n).collect::<Vec<Vec<usize>>>();
+        for row in &mut comb {
+            row.sort();
+        }
+        comb
+    };
+
+    for v in vec.iter() {
+        for t in v {
+            print!("{} ", t);
+        }
+
+        println!();
+    }
 }

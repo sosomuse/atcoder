f407e376bc681b6e17ef5ac21f0848e007f0b03c
So Nishimura <s.nishimura@gemcook.com>
2022-08-12 15:45:53 +0900

    feat: abc195 b

--- abc195/src/bin/b.rs
+++ abc195/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: f64,
+        b: f64,
+        w: f64,
+    }
+
+    let max = (w / a * 1000_f64).floor();
+    let min = (w / b * 1000_f64).ceil();
+
+    if min > max {
+        println!("UNSATISFIABLE");
+    } else {
+        println!("{} {}", min, max);
+    }
 }

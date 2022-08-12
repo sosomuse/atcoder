266a6cc3df2132929556da088d7d38e799278d03
So Nishimura <s.nishimura@gemcook.com>
2022-08-12 14:34:08 +0900

    feat: abc113 c

--- abc113/src/bin/c.rs
+++ abc113/src/bin/c.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _:usize,
+        m:usize,
+        py: [(usize, usize); m],
+    };
+
+    let mut map = HashMap::<usize, Vec<usize>>::new();
+
+    for &(p, y) in &py {
+        let v = map.entry(p).or_insert(Vec::new());
+        v.push(y);
+    }
+
+    for v in map.values_mut() {
+        v.sort();
+    }
+
+    for &(p, y) in &py {
+        let v = map.get(&p).unwrap();
+        let ans = v.binary_search(&y).unwrap_or_else(|x| x);
+
+        println!("{:06}{:06}", p, ans + 1);
+    }
 }

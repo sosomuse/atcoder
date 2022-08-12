20461b95180de17542eb345c0ef6f0a5f30f9a92
So Nishimura <s.nishimura@gemcook.com>
2022-08-12 15:07:05 +0900

    feat: abc121 c

--- abc121/src/bin/c.rs
+++ abc121/src/bin/c.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n:usize,
+        mut m:usize,
+        mut ab:[(usize,usize);n],
+    };
+
+    ab.sort_by_key(|&(a, _)| a);
+    let mut ans = 0;
+
+    for &(a, b) in &ab {
+        if m > b {
+            m -= b;
+            ans += a * b;
+        } else {
+            ans += a * m.min(b);
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }

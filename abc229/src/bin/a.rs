612fff584dab8e4685dc677a39f33d71b50745c0
So Nishimura <s.nishimura@gemcook.com>
2022-08-20 23:43:57 +0900

    feat: abc229 a

--- abc229/src/bin/a.rs
+++ abc229/src/bin/a.rs
@@ -1,3 +1,27 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s1: Chars,
+        s2: Chars,
+    };
+
+    for i in 0..s1.len() {
+        if s1[i] == '#' && s2[i] == '#' {
+            println!("Yes");
+            return;
+        }
+    }
+
+    if s1.iter().filter(|&c| *c == '#').count() == 2 {
+        println!("Yes");
+        return;
+    };
+
+    if s2.iter().filter(|&c| *c == '#').count() == 2 {
+        println!("Yes");
+        return;
+    };
+
+    println!("No");
 }

578a000bb04215c28b564c658bc41fcab0452113
So Nishimura <s.nishimura@gemcook.com>
2022-08-25 20:39:39 +0900

    feat: typical90 007

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [isize; n],
+        q: usize,
+        b: [isize; q],
+    };
+
+    a.sort();
+
+    for v in b.iter() {
+        let t = a.binary_search(v).unwrap_or_else(|i| i);
+
+        let mut ans = 100000000000000;
+
+        for i in ((t as isize - 1).max(0) as usize)..(t + 1).min(n) {
+            ans = ans.min(a[i] - v).abs();
+        }
+
+        println!("{}", ans);
+    }
 }

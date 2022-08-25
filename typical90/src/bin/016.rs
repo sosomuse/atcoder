2603b1a8a61a2210289557d7ae908b1740bba7fb
So Nishimura <s.nishimura@gemcook.com>
2022-08-25 20:56:17 +0900

    feat: abc016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+
+    let mut ans = 10000000000000;
+
+    for i in 0..=9999 {
+        let x = i * a;
+
+        if x > n {
+            continue;
+        }
+
+        for j in 0..=9999 {
+            let y = j * b;
+
+            if x + y > n {
+                continue;
+            }
+
+            let z = n - x - y;
+            let k = z / c;
+
+            if x + y + z == n && z % c == 0 {
+                ans = ans.min(i + j + k);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }

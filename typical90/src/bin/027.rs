4c6bfb3d4bb195a23e9024e43999632c0e3c8768
So Nishimura <s.nishimura@gemcook.com>
2022-08-26 22:57:22 +0900

    feat: typical027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,3 +1,21 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+
+    let mut set = HashSet::<String>::new();
+
+    for i in 0..n {
+        if set.contains(&s[i]) {
+            continue;
+        }
+
+        set.insert(s[i].to_string());
+        println!("{}", i + 1);
+    }
 }

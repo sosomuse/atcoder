bb75cfd9e59dc4e4679afa45be3eb8b309824b4a
So Nishimura <s.nishimura@gemcook.com>
2022-08-26 22:50:31 +0900

    feat: typical90 024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: isize,
+        a: [isize; n],
+        b: [isize; n],
+    };
+
+    let mut count = 0;
+
+    for i in 0..n {
+        count += (a[i] - b[i]).abs();
+    }
+
+    if k >= count && (k - count) % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

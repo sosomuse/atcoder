cfde52b9f44ae68cf7e4d745bed03c28a524ed9d
So Nishimura <s.nishimura@gemcook.com>
2022-08-26 22:44:55 +0900

    feat: typical

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,3 +1,5 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {}
 }
--- typical90/src/bin/019.rs
+++ typical90/src/bin/019.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [isize; n*2],
+    };
+
+    let mut cost = 0;
+
+    while a.len() != 0 {
+        let mut min = 100000000000000;
+        let mut min_i = 0;
+
+        for i in 0..a.len() {
+            if i + 1 < a.len() && (a[i] - a[i + 1]).abs() < min {
+                min = (a[i] - a[i + 1]).abs();
+                min_i = i;
+            }
+        }
+
+        let x = a[min_i];
+        let y = a[min_i + 1];
+
+        cost += (x - y).abs();
+
+        a.remove(min_i);
+        a.remove(min_i);
+    }
+
+    println!("{}", cost);
 }

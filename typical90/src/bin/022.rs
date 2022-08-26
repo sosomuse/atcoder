8e168b8e3f5dcc6cfcc4ca075a96fd8ba5722dbb
So Nishimura <s.nishimura@gemcook.com>
2022-08-26 22:47:03 +0900

    feat: typical90 022

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+
+    let r = gcd(a, gcd(b, c));
+
+    println!("{}", a / r + b / r + c / r - 3);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }

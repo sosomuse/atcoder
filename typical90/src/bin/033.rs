8c20851c412234de2d2ae52fc07bc1b35dce3f72
So Nishimura <s.nishimura@gemcook.com>
2022-08-27 13:47:18 +0900

    feat: typical90 033

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+    };
+
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+        return;
+    }
+
+    let x = h / 2 + h % 2;
+    let y = w / 2 + w % 2;
+
+    println!("{}", x * y);
 }

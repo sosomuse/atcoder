71ac8a7cfe4b0d193367a0a8b83d95133b5030ae
So Nishimura <s.nishimura@gemcook.com>
2022-08-27 20:13:58 +0900

    feat: typical044

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+    };
+
+    let mut queue = std::collections::VecDeque::from(a.clone());
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: usize,
+            y: usize,
+        };
+
+        if t == 1 {
+            queue.swap(x - 1, y - 1);
+        } else if t == 2 {
+            let v = queue.pop_back().unwrap();
+            queue.push_front(v);
+        } else {
+            println!("{}", queue[x - 1]);
+        }
+    }
 }

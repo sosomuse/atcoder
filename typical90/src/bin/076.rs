0527ab7f83eec6b7a9f5d6b61e144f671ebb7875
So Nishimura <s.nishimura@gemcook.com>
2022-09-03 20:34:36 +0900

    feat: typical076

--- typical90/src/bin/076.rs
+++ typical90/src/bin/076.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let sum = a.iter().sum::<usize>();
+
+    if sum % 10 != 0 {
+        println!("No");
+        return;
+    }
+
+    let mut vec = vec![0; n * 2 + 1];
+
+    for i in 1..=n {
+        let x = a[i - 1];
+        vec[i] = x + vec[i - 1];
+    }
+
+    for i in n..n * 2 {
+        let x = a[i - n];
+        vec[i + 1] = x + vec[i];
+    }
+
+    for i in 0..=n {
+        let x = vec[i];
+        if let Ok(_) = vec.binary_search(&(x + sum / 10)) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }

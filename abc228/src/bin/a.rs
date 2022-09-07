71d49fd741a3adc98468008b48d096d5c4f70719
So Nishimura <s.nishimura@gemcook.com>
2022-09-07 20:26:38 +0900

    feat: abc 228 a

--- abc228/src/bin/a.rs
+++ abc228/src/bin/a.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: usize,
+        mut t: usize,
+        x: usize,
+    };
+
+    let mut ans = false;
+
+    if s > t {
+        if x < t {
+            ans = true;
+        }
+
+        t += 24;
+    }
+    if s <= x && x < t {
+        ans = true;
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }

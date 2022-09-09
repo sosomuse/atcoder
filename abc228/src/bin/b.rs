6e50f8bcaa5208fcb0643813baa7772b27778e84
So Nishimura <s.nishimura@gemcook.com>
2022-09-09 20:09:53 +0900

    feat: abc228 b

--- abc228/src/bin/b.rs
+++ abc228/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut x: Usize1,
+        a: [usize; n],
+    };
+
+    let mut ans = 0;
+    let mut visible = vec![false; n];
+
+    while !visible[x] {
+        ans += 1;
+        visible[x] = true;
+        let next = a[x] - 1;
+        x = next;
+    }
+
+    println!("{}", ans);
 }

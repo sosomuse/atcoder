7d7141373cfaef52bb9bb3cd83e43f4b175c4fab
So Nishimura <s.nishimura@gemcook.com>
2022-09-13 22:54:28 +0900

    feat: abc228 c

--- abc228/src/bin/c.rs
+++ abc228/src/bin/c.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        p: [[usize; 3]; n],
+    };
+
+    let mut sums = p
+        .iter()
+        .map(|v| v.iter().sum::<usize>())
+        .collect::<Vec<_>>();
+    sums.sort_by(|a, b| b.cmp(a));
+    let x = sums[k - 1];
+
+    for i in 0..n {
+        let v = p[i].iter().sum::<usize>() + 300;
+
+        if v >= x {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }

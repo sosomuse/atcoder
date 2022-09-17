3d84818d88db5126cca2eb06bb6d318be5cf5b06
So Nishimura <s.nishimura@gemcook.com>
2022-09-17 17:54:37 +0900

    feat: tessoku 08

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        x: [[usize; w]; h],
+        q: usize,
+    };
+
+    let mut s = vec![vec![0; w + 1]; h + 1];
+
+    for i in 0..h {
+        for j in 0..w {
+            s[i + 1][j + 1] = s[i + 1][j] + x[i][j];
+        }
+    }
+
+    for _ in 0..q {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        };
+
+        let mut sum = 0;
+
+        for i in a..=c {
+            sum += s[i][d] - s[i][b - 1];
+        }
+
+        println!("{}", sum);
+    }
 }
--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -1,3 +1,41 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        xy: [(usize, usize); n],
+        q: usize,
+    };
+
+    let max_x = *xy.iter().map(|(x, _)| x).max().unwrap();
+    let max_y = *xy.iter().map(|(_, y)| y).max().unwrap();
+
+    let mut s = vec![vec![0; max_y + 1]; max_x + 1];
+
+    for (x, y) in xy {
+        s[x][y] += 1;
+    }
+
+    for i in 0..max_x {
+        for j in 0..max_y {
+            s[i + 1][j + 1] += s[i + 1][j];
+        }
+    }
+
+    for _ in 0..q {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        };
+
+        let mut sum = 0;
+
+        for i in a..=c {
+            sum += s[i][d] - s[i][b - 1];
+        }
+
+        println!("{}", sum);
+    }
 }

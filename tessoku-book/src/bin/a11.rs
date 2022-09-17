d6906ca5fe665b840a3accd36908965794464a23
So Nishimura <s.nishimura@gemcook.com>
2022-09-17 19:18:32 +0900

    feat: tessoku 11

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    };
+
+    println!("{}", binary_search(x, n, a) + 1);
+}
+
+fn binary_search(x: usize, mut r: usize, a: Vec<usize>) -> usize {
+    let mut l = 0;
+
+    while l <= r {
+        let mid = (l + r) / 2;
+        if a[mid] == x {
+            return mid;
+        } else if a[mid] < x {
+            l = mid + 1;
+        } else if a[mid] > x {
+            r = mid - 1;
+        }
+    }
+
+    return 0;
 }
--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,3 +1,44 @@
+use proconio::input;
+use std::cmp::Ordering;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+    };
+
+    a.sort();
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+        };
+
+        println!("{}", a.lower_bound(&x));
+    }
+}
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
 }

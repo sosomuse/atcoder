848e689958b69df006224b4fe8019d4457a1af42
So Nishimura <s.nishimura@gemcook.com>
2022-09-17 17:21:23 +0900

    feat: tessoku 07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        d: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    };
+
+    let mut s: Vec<isize> = vec![0; d + 2];
+
+    for (l, r) in lr {
+        s[l] += 1;
+        s[r + 1] -= 1;
+    }
+
+    for i in 1..=d {
+        s[i] += s[i - 1];
+    }
+
+    for i in 1..=d {
+        println!("{}", s[i]);
+    }
 }
--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    };
+
+    let mut s: Vec<isize> = vec![0; t + 1];
+
+    for (l, r) in lr {
+        s[l] += 1;
+        s[r] -= 1;
+    }
+
+    for i in 1..=t {
+        s[i] += s[i - 1];
+    }
+
+    for i in 0..t {
+        println!("{}", s[i]);
+    }
 }

9e378e0c16f4561b3608535e99ff4e771c92c0ab
So Nishimura <s.nishimura@gemcook.com>
2022-09-17 19:43:04 +0900

    feat: tessoku b12

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let check = |x: usize| -> bool {
+        let mut sum = 0;
+        for i in 0..n {
+            sum += x / a[i];
+        }
+        sum >= k
+    };
+
+    let mut l = 0;
+    let mut r = 1_000_000_000;
+
+    while l < r {
+        let m = (l + r) / 2;
+        if check(m) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    println!("{}", l);
 }
--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: f64,
+    }
+
+    let mut l = 0.0;
+    let mut r = 100.0;
+
+    for _ in 0..10000 {
+        let m = (l + r) / 2.0;
+        if m * m * m + m < n {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    println!("{}", l);
 }

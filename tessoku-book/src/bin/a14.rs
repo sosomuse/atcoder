60e54147e91a06bcb77713abba9f2a9f34dbb43e
So Nishimura <s.nishimura@gemcook.com>
2022-09-18 18:39:18 +0900

    feat: tessoku b14

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -1,3 +1,43 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
+        d: [usize; n],
+    };
+
+    let mut ab = vec![];
+    let mut cd = vec![];
+
+    for i in 0..n {
+        for j in 0..n {
+            ab.push(a[i] + b[j]);
+            cd.push(c[i] + d[j]);
+        }
+    }
+
+    ab.sort();
+    cd.sort();
+
+    for i in 0..ab.len() {
+        let v = ab[i];
+        if let Ok(_) = cd.binary_search(&(k - v)) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    for i in 0..cd.len() {
+        let v = cd[i];
+        if let Ok(_) = ab.binary_search(&(k - v)) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -1,3 +1,66 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let mut x1 = vec![];
+    let mut x2 = vec![];
+
+    let mid = n / 2;
+    let mid2 = n - mid;
+
+    for bit in 0..(1 << mid) {
+        let mut sum = 0;
+        for i in 0..mid {
+            if bit & (1 << i) != 0 {
+                sum += a[i];
+            }
+        }
+        x1.push(sum);
+    }
+
+    for bit in 0..(1 << mid2) {
+        let mut sum = 0;
+        for i in 0..mid2 {
+            if bit & (1 << i) != 0 {
+                sum += a[mid + i];
+            }
+        }
+        x2.push(sum);
+    }
+
+    x1.sort();
+    x2.sort();
+
+    if let Ok(_) = x1.binary_search(&k) {
+        println!("Yes");
+        return;
+    }
+
+    if let Ok(_) = x2.binary_search(&k) {
+        println!("Yes");
+        return;
+    }
+
+    for i in 0..x1.len() {
+        let v = x1[i];
+        if let Ok(_) = x2.binary_search(&(k - v)) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    for i in 0..x2.len() {
+        let v = x2[i];
+        if let Ok(_) = x1.binary_search(&(k - v)) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }

c3a0074e27505bc86a47f4c63082b3dc2c13f9ae
So Nishimura <s.nishimura@gemcook.com>
2022-09-18 19:20:26 +0900

    feat: tessoku b16

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    };
+
+    let mut dp = vec![0; n];
+
+    for i in 1..n {
+        if i == 1 {
+            dp[i] = a[i - 1];
+        } else {
+            dp[i] = (dp[i - 2] + b[i - 2]).min(dp[i - 1] + a[i - 1]);
+        }
+    }
+
+    println!("{}", dp[n - 1]);
 }
--- tessoku-book/src/bin/b16.rs
+++ tessoku-book/src/bin/b16.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        h: [isize; n],
+    };
+
+    let mut dp = vec![0; n + 1];
+
+    for i in 2..=n {
+        if i == 2 {
+            dp[i] = (h[i - 2] - h[i - 1]).abs();
+        } else {
+            dp[i] = std::cmp::min(
+                dp[i - 1] + (h[i - 2] - h[i - 1]).abs(),
+                dp[i - 2] + (h[i - 3] - h[i - 1]).abs(),
+            );
+        }
+    }
+
+    println!("{}", dp[n]);
 }

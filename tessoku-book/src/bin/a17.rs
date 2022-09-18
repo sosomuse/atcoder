3c4d2ab43959527c6df6471a3a5fc360003d3df2
So Nishimura <s.nishimura@gemcook.com>
2022-09-18 20:32:25 +0900

    feat: tessoku 17

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -1,3 +1,52 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    };
+
+    let mut dp = vec![0; n];
+
+    for i in 1..n {
+        if i == 1 {
+            dp[i] = a[i - 1];
+        } else {
+            dp[i] = (dp[i - 2] + b[i - 2]).min(dp[i - 1] + a[i - 1]);
+        }
+    }
+
+    let mut ans = vec![];
+    let mut p = n;
+
+    ans.push(p);
+
+    loop {
+        if p == 1 {
+            break;
+        }
+
+        if p == 2 {
+            p -= 1;
+        } else {
+            let x = dp[p - 2] + a[p - 2];
+            let y = dp[p - 3] + b[p - 3];
+
+            if x > y {
+                p -= 2;
+            } else {
+                p -= 1;
+            }
+        }
+
+        ans.push(p);
+    }
+
+    ans.reverse();
+
+    println!("{}", ans.len());
+    for i in ans {
+        print!("{} ", i);
+    }
 }
--- tessoku-book/src/bin/b17.rs
+++ tessoku-book/src/bin/b17.rs
@@ -1,3 +1,54 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        h: [isize; n],
+    };
+
+    let mut dp = vec![0; n + 1];
+
+    for i in 2..=n {
+        if i == 2 {
+            dp[i] = (h[i - 2] - h[i - 1]).abs();
+        } else {
+            dp[i] = std::cmp::min(
+                dp[i - 1] + (h[i - 2] - h[i - 1]).abs(),
+                dp[i - 2] + (h[i - 3] - h[i - 1]).abs(),
+            );
+        }
+    }
+
+    let mut ans = vec![];
+    let mut p = n;
+
+    ans.push(p);
+
+    loop {
+        if p == 1 {
+            break;
+        }
+
+        if p == 2 {
+            p -= 1;
+        } else {
+            let x = dp[p - 1] + (h[p - 2] - h[p - 1]).abs();
+            let y = dp[p - 2] + (h[p - 3] - h[p - 1]).abs();
+
+            if x > y {
+                p -= 2;
+            } else {
+                p -= 1;
+            }
+        }
+
+        ans.push(p);
+    }
+
+    ans.reverse();
+
+    println!("{}", ans.len());
+    for i in ans {
+        print!("{} ", i);
+    }
 }

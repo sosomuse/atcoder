19c78f869cf8b9dc9d7b21d1bbe8d3e33b2127f0
So Nishimura <s.nishimura@gemcook.com>
2022-09-19 22:08:50 +0900

    feat: tessoku a18

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: usize,
+        a: [usize; n],
+    };
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for i in 1..=n {
+        for j in 0..=s {
+            if dp[i - 1][j] {
+                dp[i][j] = true;
+            }
+
+            if dp[i - 1][j] && s >= j + a[i - 1] {
+                dp[i][j + a[i - 1]] = true;
+            }
+        }
+    }
+
+    println!("{}", if dp[n][s] { "Yes" } else { "No" });
 }

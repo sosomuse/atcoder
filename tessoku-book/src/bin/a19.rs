22aff56b4bd7fad8fa0f266334d5f9bf337c668c
So Nishimura <s.nishimura@gemcook.com>
2022-09-19 22:38:37 +0900

    feat: tessoku a19

--- tessoku-book/src/bin/a19.rs
+++ tessoku-book/src/bin/a19.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        wu: [(usize, usize); n],
+    };
+
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+
+    for i in 1..=n {
+        for j in 0..=w {
+            let (w2, u2) = wu[i - 1];
+
+            if j >= w2 {
+                dp[i][j] = (dp[i - 1][j - w2] + u2).max(dp[i - 1][j]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    println!("{}", dp[n][w]);
 }

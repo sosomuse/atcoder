98264676ebddc9dad3a1e46c79cc6c0cd9404939
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:21:48 +0900

    feat: tessoku a28

--- tessoku-book/src/bin/a28.rs
+++ tessoku-book/src/bin/a28.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        ta: [(char, isize); n],
+    };
+
+    let mut ans: isize = 0;
+
+    for i in 0..n {
+        let (t, a) = ta[i];
+
+        match t {
+            '+' => ans += a,
+            '-' => ans -= a,
+            '*' => ans *= a,
+            _ => panic!(),
+        }
+
+        if ans < 0 {
+            ans += 10000;
+        }
+
+        ans %= 10000;
+
+        println!("{}", ans);
+    }
 }

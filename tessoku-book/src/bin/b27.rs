6fdf68fe928012333c748c761510ccb7fc55f7df
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:13:23 +0900

    feat: tessoku b27

--- tessoku-book/src/bin/b27.rs
+++ tessoku-book/src/bin/b27.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+    };
+
+    println!("{}", lcm(a, b));
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    let mut r = a % b;
+    while r != 0 {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+
+    return b;
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    return a * b / gcd(a, b);
 }

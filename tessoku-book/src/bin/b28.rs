a48e020476722b40d0873e72659b4618a433efc8
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:25:35 +0900

    feat: tessoku b28

--- tessoku-book/src/bin/b28.rs
+++ tessoku-book/src/bin/b28.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut fib = vec![];
+
+    fib.push(1);
+    fib.push(1);
+
+    for _ in 2..n {
+        let v = fib[fib.len() - 1] + fib[fib.len() - 2];
+        fib.push(v % 1000000007);
+    }
+
+    println!("{}", fib[n - 1]);
 }

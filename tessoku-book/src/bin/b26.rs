1a8576e29870cb9ed9f0e3f2ed8dec42437d4918
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:10:21 +0900

    feat: tessoku b26

--- tessoku-book/src/bin/b26.rs
+++ tessoku-book/src/bin/b26.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    solve(n);
+}
+
+fn solve(n: usize) -> () {
+    let mut flags = vec![true; n + 1];
+    flags[0] = false;
+    flags[1] = false;
+
+    let x = (n as f64).sqrt() as usize;
+    for p in 2..=x {
+        if !flags[p] {
+            continue;
+        }
+
+        for m in ((p * p)..=n).step_by(p) {
+            flags[m] = false;
+        }
+    }
+
+    for (i, v) in flags.iter().enumerate() {
+        if *v {
+            println!("{}", i);
+        }
+    }
 }

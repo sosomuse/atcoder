b03d9aa3e7c0e487dda88f7b43ace3b5bba6e193
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:01:07 +0900

    feat: tessoku a25

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -1,3 +1,8 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        pa: [(usize, usize); n],
+    };
 }
--- tessoku-book/src/bin/a25.rs
+++ tessoku-book/src/bin/a25.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        cw: [Chars; h],
+    };
+
+    let mut dp: Vec<Vec<usize>> = vec![vec![0; w + 1]; h + 1];
+
+    for i in 1..=h {
+        for j in 1..=w {
+            let v = cw[i - 1][j - 1];
+            if v == '#' {
+                continue;
+            }
+
+            if i == 1 && j == 1 {
+                dp[1][1] = 1;
+                continue;
+            }
+
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    println!("{}", dp[h][w]);
 }

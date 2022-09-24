b06885b2d2b03341d2d4f566f86f41bad3f4efc9
So Nishimura <s.nishimura@gemcook.com>
2022-09-24 20:08:18 +0900

    feat: tessoku a26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+        };
+
+        println!("{}", if is_prime(x) { "Yes" } else { "No" });
+    }
+}
+
+fn is_prime(n: usize) -> bool {
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+
+        i += 1;
+    }
+
+    return true;
 }

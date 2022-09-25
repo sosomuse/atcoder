8045dbcf7cff19f9472459d3f4321927c6bf3cfc
So Nishimura <s.nishimura@gemcook.com>
2022-09-25 21:15:15 +0900

    feat: tessoku a29

--- tessoku-book/src/bin/a29.rs
+++ tessoku-book/src/bin/a29.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+    };
+
+    println!("{}", power(a, b, 1000000007));
+}
+
+fn power(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut ans = 1;
+
+    for i in 0..30 {
+        let w = 1 << i;
+        if (b & w) != 0 {
+            ans = ans * p % m;
+        }
+        p = p * p % m;
+    }
+
+    ans
 }

cd12977933b23c8c31767c98efa022f9d7e43637
So Nishimura <s.nishimura@gemcook.com>
2022-09-27 13:50:43 +0900

    feat: tessoku a32

--- tessoku-book/src/bin/a32.rs
+++ tessoku-book/src/bin/a32.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    };
+
+    if n % (a + b) == 0 {
+        println!("Second");
+    } else {
+        println!("First");
+    }
 }

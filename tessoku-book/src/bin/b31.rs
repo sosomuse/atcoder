01772f8f3c58b42c8c7985cb1ee8031e506a2c79
So Nishimura <s.nishimura@gemcook.com>
2022-09-27 13:38:37 +0900

    feat: tessoku b31

--- tessoku-book/src/bin/b31.rs
+++ tessoku-book/src/bin/b31.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let x = n / 3;
+    let y = n / 5;
+    let z = n / 7;
+    let u = n / (3 * 5);
+    let q = n / (3 * 7);
+    let p = n / (5 * 7);
+    let l = n / (3 * 5 * 7);
+
+    println!("{}", x + y + z - u - q - p + l);
 }

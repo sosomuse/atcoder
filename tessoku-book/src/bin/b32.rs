166ab4625adbbcf214fdcb5f6640a5eb8f44562c
So Nishimura <s.nishimura@gemcook.com>
2022-09-30 23:35:06 +0900

    feat: tessoku b32

--- tessoku-book/src/bin/b32.rs
+++ tessoku-book/src/bin/b32.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; k],
+    };
+
+    let mut dp = vec![false; n + 1];
+
+    for i in 0..n {
+        if dp[i] {
+            continue;
+        }
+        for &a_i in a.iter() {
+            if i + a_i <= n {
+                dp[i + a_i] = true;
+            }
+        }
+    }
+
+    if dp[n] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }

45a081143db3e3b7070ff5fb1d3b0668bd9c6fba
So Nishimura <s.nishimura@gemcook.com>
2022-10-01 19:08:29 +0900

    feat: tessoku a36

--- tessoku-book/src/bin/a36.rs
+++ tessoku-book/src/bin/a36.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+    };
+
+    if (n - 1) * 2 <= k && k % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

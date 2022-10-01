2696523f2073582c75ef22b425b3d5ef94211d25
So Nishimura <s.nishimura@gemcook.com>
2022-10-01 19:19:26 +0900

    feat: tessoku a37

--- tessoku-book/src/bin/a37.rs
+++ tessoku-book/src/bin/a37.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        b: usize,
+        a: [usize; n],
+        c: [usize; m],
+    };
+
+    let mut ans: usize = 0;
+
+    for i in 0..n {
+        ans += a[i] * m;
+    }
+
+    ans += b * n * m;
+
+    for i in 0..m {
+        ans += c[i] * n;
+    }
+
+    println!("{}", ans);
 }

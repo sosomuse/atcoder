22ed71daf982afc66ca491f9c88f4c0389771c02
So Nishimura <s.nishimura@gemcook.com>
2022-10-01 20:09:54 +0900

    feat: abc a38

--- tessoku-book/src/bin/a38.rs
+++ tessoku-book/src/bin/a38.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        d: usize,
+        n: usize,
+        lrh: [(usize, usize, usize); n],
+    };
+
+    let mut s = vec![24; d];
+
+    for i in 0..n {
+        let (l, r, h) = lrh[i];
+        for j in l - 1..r {
+            s[j] = std::cmp::min(s[j], h);
+        }
+    }
+
+    println!("{}", s.iter().sum::<usize>());
 }

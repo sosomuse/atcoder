9ceca8fd1718d6995e1a2be85155452f10e43b70
So Nishimura <s.nishimura@gemcook.com>
2022-10-01 19:14:43 +0900

    feat: tessoku b36

--- tessoku-book/src/bin/b36.rs
+++ tessoku-book/src/bin/b36.rs
@@ -1,3 +1,18 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        k: usize,
+        s: Chars,
+    };
+
+    let es = s.iter().filter(|&c| *c == '1').collect::<Vec<_>>();
+    let x = es.len();
+
+    if k % 2 == x % 2 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

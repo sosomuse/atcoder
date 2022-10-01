abf955a277823d5c66bdbf96108e85fe22dd2264
So Nishimura <s.nishimura@gemcook.com>
2022-10-01 11:02:17 +0900

    feat: tessoku b33

--- tessoku-book/src/bin/b33.rs
+++ tessoku-book/src/bin/b33.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        _: usize,
+        _: usize,
+        ab: [(usize, usize); n],
+    };
+
+    let mut sum_a = ab[0].0;
+    let mut sum_b = ab[0].0;
+
+    for i in 1..n {
+        sum_a = sum_a ^ (ab[i].0 - 1);
+        sum_b = sum_b ^ (ab[i].1 - 1);
+    }
+
+    if sum_a == 0 && sum_b == 0 {
+        println!("Second");
+    } else {
+        println!("First");
+    }
 }

b1a5b66cb9efe76075e881e488d4ba66610ee573
So Nishimura <s.nishimura@gemcook.com>
2022-10-06 20:41:46 +0900

    feat: tessoku b40

--- tessoku-book/src/bin/b40.rs
+++ tessoku-book/src/bin/b40.rs
@@ -1,3 +1,42 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut cnt = std::collections::HashMap::<usize, usize>::new();
+
+    for i in a {
+        let v = i % 100;
+        *cnt.entry(v).or_insert(0) += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 1..=49 {
+        let v = cnt.get(&i).unwrap_or(&0);
+        let w = cnt.get(&(100 - i)).unwrap_or(&0);
+        ans += w * v;
+    }
+
+    ans += binomial(*cnt.get(&0).unwrap_or(&0), 2);
+    ans += binomial(*cnt.get(&50).unwrap_or(&0), 2);
+
+    println!("{}", ans);
+}
+
+fn binomial(n: usize, k: usize) -> usize {
+    if n < k {
+        return 0;
+    }
+
+    let mut ret: usize = 1;
+    for i in 0..k {
+        ret *= n - i;
+        ret /= i + 1;
+    }
+
+    return ret;
 }

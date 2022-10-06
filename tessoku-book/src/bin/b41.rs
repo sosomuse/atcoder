60eb01335b55cb602c01d4e016173cb226c981c5
So Nishimura <s.nishimura@gemcook.com>
2022-10-06 21:02:35 +0900

    feat: tessoku b41

--- tessoku-book/src/bin/b41.rs
+++ tessoku-book/src/bin/b41.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut x: usize,
+        mut y: usize,
+    };
+
+    let mut ans: Vec<(usize, usize)> = vec![];
+
+    if x == y {
+        println!("{}", 0);
+        return;
+    }
+
+    while x != 1 || y != 1 {
+        ans.push((x, y));
+
+        if x > y {
+            x -= y;
+        } else {
+            y -= x;
+        }
+    }
+
+    ans.reverse();
+
+    println!("{}", ans.len());
+
+    for (x, y) in ans {
+        println!("{} {}", x, y);
+    }
 }

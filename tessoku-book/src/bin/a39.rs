97c76e1de14483b49ae5e3e54c95cc8698a158f7
So Nishimura <s.nishimura@gemcook.com>
2022-10-06 19:09:20 +0900

    feat: tessoku a39

--- tessoku-book/src/bin/a39.rs
+++ tessoku-book/src/bin/a39.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut lr: [(usize, usize); n],
+    };
+
+    lr.sort_by_key(|&(_, r)| r);
+
+    let mut start = 0;
+    let mut count = 0;
+
+    for (l, r) in lr {
+        if start > l {
+            continue;
+        }
+
+        start = r;
+        count += 1;
+    }
+
+    println!("{}", count);
 }

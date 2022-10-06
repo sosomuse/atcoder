375158d34e280842bd515990cc30caa14cc6a546
So Nishimura <s.nishimura@gemcook.com>
2022-10-06 20:48:24 +0900

    feat: tessoku a41

--- tessoku-book/src/bin/a41.rs
+++ tessoku-book/src/bin/a41.rs
@@ -1,3 +1,22 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+
+    let mut ans = false;
+
+    for i in 0..n - 2 {
+        if s[i] == 'R' && s[i + 1] == 'R' && s[i + 2] == 'R' {
+            ans = true;
+        }
+
+        if s[i] == 'B' && s[i + 1] == 'B' && s[i + 2] == 'B' {
+            ans = true;
+        }
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }

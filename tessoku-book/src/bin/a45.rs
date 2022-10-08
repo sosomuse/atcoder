acefbe4d933b60594a02601582b689e4bfb4b0ed
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 18:48:23 +0900

    feat: tessoku a45

--- tessoku-book/src/bin/a45.rs
+++ tessoku-book/src/bin/a45.rs
@@ -1,3 +1,34 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        c: char,
+        a: Chars,
+    };
+
+    let mut score = 0;
+
+    for i in 0..n {
+        let v = a[i];
+
+        match v {
+            'W' => score += 0,
+            'B' => score += 1,
+            'R' => score += 2,
+            _ => unreachable!(),
+        }
+    }
+
+    let m = score % 3;
+
+    if m == 0 && c == 'W' {
+        println!("Yes");
+    } else if m == 1 && c == 'B' {
+        println!("Yes");
+    } else if m == 2 && c == 'R' {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

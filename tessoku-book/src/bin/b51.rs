56026aef878419d68e2af9a6d78b957e3e807ad5
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 19:05:20 +0900

    feat: tessoku b51

--- tessoku-book/src/bin/b51.rs
+++ tessoku-book/src/bin/b51.rs
@@ -1,3 +1,24 @@
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut pairs: Vec<(usize, usize)> = vec![];
+    let mut queue: VecDeque<usize> = VecDeque::new();
+
+    for i in 0..s.len() {
+        if s[i] == '(' {
+            queue.push_back(i + 1)
+        } else {
+            pairs.push((queue.pop_back().unwrap(), i + 1));
+        }
+    }
+
+    for i in 0..pairs.len() {
+        println!("{} {}", pairs[i].0, pairs[i].1);
+    }
 }

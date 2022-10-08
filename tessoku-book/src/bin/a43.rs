6adadee80a1414e94bce4fa6818cd86ccf535fcf
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 18:20:11 +0900

    feat: tessoku a43

--- tessoku-book/src/bin/a43.rs
+++ tessoku-book/src/bin/a43.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        l: usize,
+        ab: [(usize, char); n],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        let (a, b) = ab[i];
+
+        if b == 'E' {
+            ans = ans.max(l - a);
+        } else {
+            ans = ans.max(a);
+        }
+    }
+
+    println!("{}", ans);
 }

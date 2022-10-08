d145d41e160bd28fdba78bc7e82e5a3527eaf0a8
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 20:12:00 +0900

    feat: tessoku b52

--- tessoku-book/src/bin/b52.rs
+++ tessoku-book/src/bin/b52.rs
@@ -1,3 +1,36 @@
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x: usize,
+        mut a: Chars,
+    };
+
+    // #: Black, .: White
+
+    let mut deque: VecDeque<usize> = VecDeque::with_capacity(n);
+
+    deque.push_front(x - 1);
+    a[x - 1] = '@';
+
+    while !deque.is_empty() {
+        if let Some(pos) = deque.pop_front() {
+            if pos > 0 && a[pos - 1] == '.' {
+                a[pos - 1] = '@';
+                deque.push_back(pos - 1);
+            }
+
+            if pos < a.len() - 1 && a[pos + 1] == '.' {
+                a[pos + 1] = '@';
+                deque.push_back(pos + 1);
+            }
+        }
+    }
+
+    for i in 0..n {
+        print!("{}", a[i]);
+    }
 }

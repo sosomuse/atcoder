938cf186ec2cf14c8dd8eef8e4ce01f60e184bae
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 18:54:24 +0900

    feat: tessoku a51

--- tessoku-book/src/bin/a51.rs
+++ tessoku-book/src/bin/a51.rs
@@ -1,3 +1,28 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    let mut queue = VecDeque::new();
+
+    for _ in 0..q {
+        input! {
+            t: usize
+        };
+
+        if t == 1 {
+            input! {
+                x: String,
+            };
+            queue.push_front(x);
+        } else if t == 2 {
+            println!("{}", queue.front().unwrap());
+        } else {
+            queue.pop_front().unwrap();
+        }
+    }
 }

73e29e84d64077c20dd41d4af33cf8ccf247d73b
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 18:38:18 +0900

    feat: tessoku b44

--- tessoku-book/src/bin/b44.rs
+++ tessoku-book/src/bin/b44.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [[usize; n]; n],
+        q: usize,
+    };
+
+    let mut vec = vec![0; n];
+
+    for i in 0..n {
+        vec[i] = i;
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: usize,
+            y: usize,
+        };
+
+        match t {
+            1 => {
+                let nx = vec[x - 1];
+                let ny = vec[y - 1];
+                vec[x - 1] = ny;
+                vec[y - 1] = nx;
+            }
+            2 => {
+                println!("{}", a[vec[x - 1]][y - 1]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }

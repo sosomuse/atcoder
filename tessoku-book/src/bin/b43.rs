abee484892f089617e38187ea2f15118acc6ddd8
So Nishimura <s.nishimura@gemcook.com>
2022-10-08 18:23:05 +0900

    feat: tessoku b43

--- tessoku-book/src/bin/b43.rs
+++ tessoku-book/src/bin/b43.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    };
+
+    let mut ans = vec![m; n];
+
+    for i in 0..m {
+        let v = a[i] - 1;
+        ans[v] -= 1;
+    }
+
+    for a in ans {
+        println!("{}", a);
+    }
 }

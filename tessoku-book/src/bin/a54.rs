19301840128f09322d8ae77737a72f5c841c207f
So Nishimura <s.nishimura@gemcook.com>
2022-10-09 13:39:41 +0900

    feat: tessoku a54

--- tessoku-book/src/bin/a54.rs
+++ tessoku-book/src/bin/a54.rs
@@ -1,3 +1,34 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    let mut map = HashMap::new();
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: String,
+        };
+
+        match t {
+            1 => {
+                input! {
+                    y: usize,
+                };
+
+                map.insert(x, y);
+            }
+            2 => {
+                if let Some(&y) = map.get(&x) {
+                    println!("{}", y);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
 }

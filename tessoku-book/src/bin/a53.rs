816d37caa485f4b8e4398448b216b9dcc2cd38ea
So Nishimura <s.nishimura@gemcook.com>
2022-10-09 13:22:07 +0900

    feat: tessoku a53

--- tessoku-book/src/bin/a53.rs
+++ tessoku-book/src/bin/a53.rs
@@ -1,3 +1,34 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    let mut binary_heap = BinaryHeap::new();
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+        };
+
+        match t {
+            1 => {
+                input! {
+                    x: usize,
+                };
+
+                binary_heap.push(Reverse(x));
+            }
+            2 => {
+                println!("{}", binary_heap.peek().unwrap().0);
+            }
+            3 => {
+                binary_heap.pop().unwrap();
+            }
+            _ => unreachable!(),
+        }
+    }
 }

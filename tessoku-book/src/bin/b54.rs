797236b1dc2cbe5941235c10e712121166f08fa0
So Nishimura <s.nishimura@gemcook.com>
2022-10-09 13:47:13 +0900

    feat: b54

--- tessoku-book/src/bin/b54.rs
+++ tessoku-book/src/bin/b54.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut map = HashMap::new();
+    let mut ans: usize = 0;
+
+    for v in a {
+        if let Some(x) = map.get(&v) {
+            ans += x;
+        }
+
+        *map.entry(v).or_insert(0) += 1;
+    }
+
+    println!("{}", ans);
 }

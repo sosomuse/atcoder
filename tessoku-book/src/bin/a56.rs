3bf2ac56f67368618b2f653b37e7520d64dc4193
So Nishimura <s.nishimura@gemcook.com>
2022-10-09 20:15:32 +0900

    feat: tessoku a56

--- tessoku-book/src/bin/a56.rs
+++ tessoku-book/src/bin/a56.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        q: usize,
+        s: String,
+    };
+
+    for _ in 0..q {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        };
+
+        let x = &s[(a - 1)..b];
+        let y = &s[(c - 1)..d];
+
+        if x == y {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }

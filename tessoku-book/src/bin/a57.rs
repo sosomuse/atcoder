ae083fb704ba06843cb2b20a87aa3483879065bb
So Nishimura <s.nishimura@gemcook.com>
2022-10-13 12:31:04 +0900

    fea:t: tessoku a57

--- tessoku-book/src/bin/a57.rs
+++ tessoku-book/src/bin/a57.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+    };
+
+    let mut dp = vec![vec![0; n + 1]; 32];
+
+    for i in 0..n {
+        dp[0][i + 1] = a[i];
+    }
+
+    for d in 1..30 {
+        for i in 1..=n {
+            dp[d][i] = dp[d - 1][dp[d - 1][i]];
+        }
+    }
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+            y: usize,
+        };
+
+        let mut cp = x;
+
+        for d in (0..30).rev() {
+            if (y / (1 << d)) % 2 != 0 {
+                cp = dp[d][cp];
+            }
+        }
+
+        println!("{}", cp);
+    }
 }

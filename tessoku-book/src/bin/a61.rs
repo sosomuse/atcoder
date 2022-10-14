b2504ffb1c7e9f347c8613b10f2af7de015e2908
So Nishimura <s.nishimura@gemcook.com>
2022-10-14 13:37:47 +0900

    feat: tessoku a61

--- tessoku-book/src/bin/a61.rs
+++ tessoku-book/src/bin/a61.rs
@@ -1,3 +1,27 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    };
+
+    let mut graph = vec![vec![]; n + 1];
+
+    for i in 0..m {
+        let (a, b) = ab[i];
+
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    for i in 1..=n {
+        let nodes = &mut graph[i];
+        nodes.sort();
+
+        let node = nodes.iter().join(", ");
+        println!("{}: {{{}}}", i, node)
+    }
 }

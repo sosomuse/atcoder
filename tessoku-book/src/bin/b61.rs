d5d646f0b0f62f90d19c8f7a0e30c38a2c39807f
So Nishimura <s.nishimura@gemcook.com>
2022-10-14 13:43:47 +0900

    feat: tessoku b61

--- tessoku-book/src/bin/b61.rs
+++ tessoku-book/src/bin/b61.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    };
+
+    let mut graph = vec![vec![]; n + 1];
+
+    for (a, b) in ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut max = (0, 0);
+
+    for i in 1..=n {
+        let nodes = &graph[i];
+        if max.0 < nodes.len() {
+            max = (nodes.len(), i);
+        }
+    }
+
+    println!("{}", max.1)
 }

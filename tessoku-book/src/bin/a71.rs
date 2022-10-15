04ff9112cc499b8ab5427344340dc8a1cba27ec9
So Nishimura <s.nishimura@gemcook.com>
2022-10-15 19:02:47 +0900

    feat: a71

--- tessoku-book/src/bin/a71.rs
+++ tessoku-book/src/bin/a71.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        mut b: [usize; n],
+    };
+
+    a.sort();
+    b.sort_by(|a, b| b.cmp(&a));
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        ans += a[i] * b[i];
+    }
+
+    println!("{}", ans);
 }

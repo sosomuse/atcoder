3f4d54b4a73174d6aec886e164aaa151417d37f8
So Nishimura <s.nishimura@gemcook.com>
2022-10-15 18:09:15 +0900

    feat: tessoku a64

--- tessoku-book/src/bin/a64.rs
+++ tessoku-book/src/bin/a64.rs
@@ -1,3 +1,131 @@
+use std::{collections::BinaryHeap, vec};
+
+use proconio::input;
+use std::cmp::Ordering;
+
+// ダイクストラ法
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+
+struct State {
+    cost: usize,
+    position: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Clone)]
+struct Edge {
+    node: usize,
+    cost: usize,
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        abc: [(usize, usize, usize); m],
+    };
+
+    let mut graph: Vec<Vec<Edge>> = vec![vec![]; n + 1];
+
+    for (a, b, c) in abc {
+        graph[a].push(Edge { node: b, cost: c });
+        graph[b].push(Edge { node: a, cost: c });
+    }
+
+    let ans = dijkstra(&graph, 1);
+
+    for i in 1..ans.len() {
+        let cost = ans[i];
+
+        if cost == std::usize::MAX {
+            println!("-1");
+        } else {
+            println!("{}", cost);
+        }
+    }
+}
+
+fn dijkstra(graph: &Vec<Vec<Edge>>, start: usize) -> Vec<usize> {
+    let mut dist: Vec<_> = (0..graph.len()).map(|_| std::usize::MAX).collect();
+    let mut heap = BinaryHeap::new();
+
+    dist[start] = 0;
+    heap.push(State {
+        cost: 0,
+        position: start,
+    });
+
+    while let Some(State { cost, position }) = heap.pop() {
+        if cost > dist[position] {
+            continue;
+        }
+
+        for edge in &graph[position] {
+            let next = State {
+                cost: cost + edge.cost,
+                position: edge.node,
+            };
+
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                dist[next.position] = next.cost;
+            }
+        }
+    }
+
+    dist
+}
+
+fn shortest_path(graph: &Vec<Vec<Edge>>, start: usize, end: usize) -> Option<usize> {
+    let mut dist: Vec<_> = (0..graph.len()).map(|_| std::usize::MAX).collect();
+    let mut heap = BinaryHeap::new();
+
+    dist[start] = 0;
+    heap.push(State {
+        cost: 0,
+        position: start,
+    });
+
+    while let Some(State { cost, position }) = heap.pop() {
+        if position == end {
+            return Some(cost);
+        }
+
+        if cost > dist[position] {
+            continue;
+        }
+
+        for edge in &graph[position] {
+            let next = State {
+                cost: cost + edge.cost,
+                position: edge.node,
+            };
+
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                dist[next.position] = next.cost;
+            }
+        }
+    }
+
+    None
 }

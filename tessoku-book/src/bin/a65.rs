2e9e8c3b7400a0523d76db9d50aaf7541420b64f
So Nishimura <s.nishimura@gemcook.com>
2022-10-15 18:27:06 +0900

    feat: tessoku a65

--- tessoku-book/src/bin/a65.rs
+++ tessoku-book/src/bin/a65.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+    };
+
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; n + 1];
+    let mut dp = vec![0; n + 1];
+
+    for (i, v) in a.iter().enumerate() {
+        graph[*v].push(i + 2);
+    }
+
+    for i in (1..graph.len()).rev() {
+        for v in graph[i].iter() {
+            dp[i] += dp[*v] + 1;
+        }
+    }
+
+    for i in 1..=n {
+        print!("{} ", dp[i]);
+    }
 }

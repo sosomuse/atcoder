f04324c220a3cb42425a90d749b7cc9aa905fa89
So Nishimura <s.nishimura@gemcook.com>
2022-10-16 13:43:55 +0900

    feat: update binary_search

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -18,7 +18,7 @@ fn main() {
 
     for i in 0..n {
         let v = a[i];
-        let x = c.lower_bound(&v);
+        let x = c.lower_bound(&v).unwrap();
         ans[i] = x + 1;
     }
 
@@ -28,11 +28,12 @@ fn main() {
 }
 
 pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> usize;
+    fn lower_bound(&self, x: &T) -> Option<usize>;
+    fn upper_bound(&self, x: &T) -> Option<usize>;
 }
 
 impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: &T) -> usize {
+    fn lower_bound(&self, x: &T) -> Option<usize> {
         let mut low = 0;
         let mut high = self.len();
 
@@ -47,6 +48,33 @@ impl<T: Ord> BinarySearch<T> for [T] {
                 }
             }
         }
-        low
+
+        if self.len() > low && self[low] >= *x {
+            Some(low)
+        } else {
+            None
+        }
+    }
+
+    fn upper_bound(&self, x: &T) -> Option<usize> {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        if self.len() > low && self[low] > *x {
+            Some(low)
+        } else {
+            None
+        }
     }
 }
--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -15,16 +15,17 @@ fn main() {
             x: usize,
         };
 
-        println!("{}", a.lower_bound(&x));
+        println!("{}", a.lower_bound(&x).unwrap_or(a.len()));
     }
 }
 
 pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> usize;
+    fn lower_bound(&self, x: &T) -> Option<usize>;
+    fn upper_bound(&self, x: &T) -> Option<usize>;
 }
 
 impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: &T) -> usize {
+    fn lower_bound(&self, x: &T) -> Option<usize> {
         let mut low = 0;
         let mut high = self.len();
 
@@ -39,6 +40,33 @@ impl<T: Ord> BinarySearch<T> for [T] {
                 }
             }
         }
-        low
+
+        if self.len() > low && self[low] >= *x {
+            Some(low)
+        } else {
+            None
+        }
+    }
+
+    fn upper_bound(&self, x: &T) -> Option<usize> {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        if self.len() > low && self[low] > *x {
+            Some(low)
+        } else {
+            None
+        }
     }
 }

e66c333e6a5520275482c24843d7229a5bbae136
So Nishimura <s.nishimura@gemcook.com>
2022-10-16 14:11:47 +0900

    feat: abc 273

--- abc273/src/bin/d.rs
+++ abc273/src/bin/d.rs
@@ -30,6 +30,30 @@ fn main() {
         v.sort();
     }
 
+    let get_limit = |d: char, l: usize, c: usize, r: usize| {
+        let limit = match d {
+            'U' => {
+                if r > l {
+                    r - l
+                } else {
+                    1
+                }
+            }
+            'D' => h.min(r + l),
+            'L' => {
+                if c > l {
+                    c - l
+                } else {
+                    1
+                }
+            }
+            'R' => w.min(c + l),
+            _ => todo!(),
+        };
+
+        limit
+    };
+
     for _ in 0..q {
         input! {
             d: char,
@@ -38,132 +62,83 @@ fn main() {
 
         match d {
             'L' => {
-                if c == 1 {
-                    println!("{} {}", r, c);
-                    continue;
-                }
-
                 if let Some(y) = h_block.get(&(r - 1)) {
-                    let t = y.lower_bound(&(c));
+                    dbg!(d, c, &y);
 
-                    if t == 0 {
-                        if c > l {
-                            c -= l;
-                        } else {
-                            c = 1;
+                    if let Some(t) = y.lower_bound(&(c)) {
+                        let mut index = t;
+                        if index == 0 {
+                            index += 1;
                         }
 
-                        println!("{} {}", r, c);
-                        continue;
-                    }
-
-                    if let Some(z) = y.get(t - 1) {
-                        if z + l >= c {
-                            c = z + 1;
-                        } else {
-                            c -= l
+                        if let Some(z) = y.get(index - 1) {
+                            if z <= &c && z + l >= c {
+                                c = z + 1;
+                            } else {
+                                c -= l
+                            }
                         }
+                    } else {
+                        c = get_limit(d, l, c, w);
                     }
                 } else {
-                    if c > l {
-                        c -= l;
-                    } else {
-                        c = 1;
-                    }
+                    c = get_limit(d, l, c, r);
                 }
             }
             'R' => {
-                if c == w {
-                    println!("{} {}", r, c);
-
-                    continue;
-                }
-
                 if let Some(y) = h_block.get(&(r - 1)) {
-                    let t = y.lower_bound(&(c));
-
-                    if let Some(z) = y.get(t) {
+                    if let Some(t) = y.lower_bound(&(c)) {
+                        let z = &y[t];
                         if c + l >= *z {
                             c = z - 1;
                         } else {
                             c += l;
                         }
                     } else {
-                        if c + l > w {
-                            c = w;
-                        } else {
-                            c += l;
-                        }
+                        c = get_limit(d, l, c, r);
                     }
                 } else {
-                    if c <= w - l {
-                        c += l;
-                    }
+                    c = get_limit(d, l, c, r);
                 }
             }
             'U' => {
-                if r == 1 {
-                    println!("{} {}", r, c);
-                    continue;
-                }
-
                 if let Some(y) = w_block.get(&(c - 1)) {
-                    let t = y.lower_bound(&(r));
-
-                    if t == 0 {
-                        if r > l {
-                            r -= l;
-                        } else {
-                            r = 1;
+                    if let Some(t) = y.lower_bound(&(c)) {
+                        let mut index = t;
+                        if index == 0 {
+                            index += 1;
                         }
 
-                        println!("{} {}", r, c);
-                        continue;
-                    }
-
-                    if let Some(z) = y.get(t - 1) {
-                        if z + l >= r {
-                            r = z + 1;
-                        } else {
-                            r -= l
+                        if let Some(z) = y.get(index - 1) {
+                            if z <= &r && z + l >= r {
+                                r = z + 1;
+                            } else {
+                                r -= l
+                            }
                         }
+                    } else {
+                        r = get_limit(d, l, c, w);
                     }
                 } else {
-                    if r > l {
-                        r -= l;
-                    } else {
-                        r = 1;
-                    }
+                    r = get_limit(d, l, c, r);
                 }
             }
             'D' => {
-                if r == h {
-                    println!("{} {}", r, c);
-                    continue;
-                }
-
                 if let Some(y) = w_block.get(&(c - 1)) {
-                    let t = y.lower_bound(&(r));
+                    // dbg!(d, r, &y);
+                    if let Some(t) = y.upper_bound(&(r)) {
+                        let z = &y[t];
 
-                    if let Some(z) = y.get(t) {
                         if r + l >= *z {
                             r = z - 1;
                         } else {
                             r += l;
                         }
                     } else {
-                        if r <= h - l {
-                            r += l;
-                        } else {
-                            r = h;
-                        }
+                        r = get_limit(d, l, c, r);
                     }
                 } else {
-                    if r <= h - l {
-                        r += l;
-                    } else {
-                        r = h;
-                    }
+                    r = get_limit(d, l, c, r);
                 }
             }
             _ => unreachable!(),
@@ -174,11 +149,12 @@ fn main() {
 }
 
 pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> usize;
+    fn lower_bound(&self, x: &T) -> Option<usize>;
+    fn upper_bound(&self, x: &T) -> Option<usize>;
 }
 
 impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: &T) -> usize {
+    fn lower_bound(&self, x: &T) -> Option<usize> {
         let mut low = 0;
         let mut high = self.len();
 
@@ -193,6 +169,33 @@ impl<T: Ord> BinarySearch<T> for [T] {
                 }
             }
         }
-        low
+
+        if self.len() > low && self[low] >= *x {
+            Some(low)
+        } else {
+            None
+        }
+    }
+
+    fn upper_bound(&self, x: &T) -> Option<usize> {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        if self.len() > low && self[low] > *x {
+            Some(low)
+        } else {
+            None
+        }
     }
 }

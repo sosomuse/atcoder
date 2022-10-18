e723bdcd6ac5de94d953d169fe23154745ca27ce
So Nishimura <s.nishimura@gemcook.com>
2022-10-18 13:58:55 +0900

    feat: abc232 c

--- abc232/src/bin/c.rs
+++ abc232/src/bin/c.rs
@@ -1,3 +1,5 @@
+use std::{collections::VecDeque, vec};
+
 use proconio::input;
 
 fn main() {
@@ -21,15 +23,58 @@ fn main() {
         graph_b[d].push(c);
     }
 
-    graph_a.sort_by(|a, b| b.len().cmp(&a.len()));
-    graph_b.sort_by(|a, b| b.len().cmp(&a.len()));
+    let mut dist: Vec<String> = vec![];
 
     for i in 0..n {
-        if graph_a[i].len() != graph_b[i].len() {
+        let mut dist_a = bfs(i + 1, &graph_a);
+        dist_a.sort();
+
+        dist.push(
+            dist_a
+                .iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+                .join(","),
+        );
+    }
+
+    for i in 0..n {
+        let mut dist_b = bfs(i + 1, &graph_b);
+        dist_b.sort();
+
+        let str_b = dist_b
+            .iter()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .join(",");
+
+        if !dist.contains(&str_b) {
             println!("No");
             return;
         }
     }
 
     println!("Yes")
+}
+
+fn bfs(v: usize, graph: &Vec<Vec<usize>>) -> Vec<isize> {
+    let mut dist: Vec<isize> = vec![-1; graph.len()];
+    let mut queue: VecDeque<usize> = std::collections::VecDeque::new();
+    queue.push_front(v);
+    dist[v] = 0;
+
+    while !queue.is_empty() {
+        let pos = *queue.front().unwrap();
+        queue.pop_front().unwrap();
+
+        for i in 0..graph[pos].len() {
+            let nex = graph[pos][i];
+            if dist[nex] == -1 {
+                dist[nex] = dist[pos] + 1;
+                queue.push_back(nex);
+            }
+        }
+    }
+
+    dist
 }

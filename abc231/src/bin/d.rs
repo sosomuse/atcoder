c71fedf76a763b1fdb64bccfe94118c12e3a2029
So Nishimura <s.nishimura@gemcook.com>
2022-10-20 01:22:14 +0900

    feat: abc231 d

--- abc231/src/bin/d.rs
+++ abc231/src/bin/d.rs
@@ -1,3 +1,85 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    };
+
+    let mut vec = vec![0; n + 1];
+    let mut uf = UnionFind::new(n + 1);
+
+    for i in 0..m {
+        let (a, b) = ab[i];
+        // 閉路検出
+        if uf.issame(a, b) {
+            println!("No");
+            return;
+        }
+
+        uf.unite(a, b);
+        vec[a] += 1;
+        vec[b] += 1;
+    }
+
+    for i in 1..=n {
+        if vec[i] > 2 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
+}
+
+struct UnionFind {
+    par: Vec<usize>,
+    siz: Vec<usize>,
+}
+
+// ユニオンファインド
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+    fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+        self.par[x] = self.root(self.par[x]);
+        self.par[x]
+    }
+
+    fn issame(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    fn unite(&mut self, mut parent: usize, mut child: usize) -> bool {
+        parent = self.root(parent);
+        child = self.root(child);
+
+        if parent == child {
+            return false;
+        }
+
+        if self.siz[parent] < self.siz[child] {
+            std::mem::swap(&mut parent, &mut child);
+        }
+
+        self.par[child] = parent;
+        self.siz[parent] += self.siz[child];
+        true
+    }
+
+    #[allow(dead_code)]
+    fn size(&mut self, x: usize) -> usize {
+        let root = self.root(x);
+        self.siz[root]
+    }
 }

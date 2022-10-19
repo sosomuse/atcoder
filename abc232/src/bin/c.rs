e27ce56a9b1c0cfea66f870dc224961951d3179d
So Nishimura <s.nishimura@gemcook.com>
2022-10-20 01:42:56 +0900

    feat: abc232 c

--- abc232/src/bin/c.rs
+++ abc232/src/bin/c.rs
@@ -1,5 +1,4 @@
-use std::{collections::VecDeque, vec};
-
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
@@ -10,71 +9,33 @@ fn main() {
         cd: [(usize, usize); m],
     };
 
-    let mut graph_a = vec![vec![]; n + 1];
-    let mut graph_b = vec![vec![]; n + 1];
+    let mut v1 = vec![vec![false; n]; n];
+    let mut v2 = vec![vec![false; n]; n];
 
     for (a, b) in ab {
-        graph_a[a].push(b);
-        graph_a[b].push(a);
+        v1[a - 1][b - 1] = true;
+        v1[b - 1][a - 1] = true;
     }
 
     for (c, d) in cd {
-        graph_b[c].push(d);
-        graph_b[d].push(c);
+        v2[c - 1][d - 1] = true;
+        v2[d - 1][c - 1] = true;
     }
 
-    let mut dist: Vec<String> = vec![];
+    let perm = (0..n).permutations(n).collect::<Vec<Vec<usize>>>();
 
-    for i in 0..n {
-        let mut dist_a = bfs(i + 1, &graph_a);
-        dist_a.sort();
-
-        dist.push(
-            dist_a
-                .iter()
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>()
-                .join(","),
-        );
-    }
-
-    for i in 0..n {
-        let mut dist_b = bfs(i + 1, &graph_b);
-        dist_b.sort();
-
-        let str_b = dist_b
-            .iter()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
-
-        if !dist.contains(&str_b) {
-            println!("No");
-            return;
-        }
-    }
-
-    println!("Yes")
-}
-
-fn bfs(v: usize, graph: &Vec<Vec<usize>>) -> Vec<isize> {
-    let mut dist: Vec<isize> = vec![-1; graph.len()];
-    let mut queue: VecDeque<usize> = std::collections::VecDeque::new();
-    queue.push_front(v);
-    dist[v] = 0;
-
-    while !queue.is_empty() {
-        let pos = *queue.front().unwrap();
-        queue.pop_front().unwrap();
-
-        for i in 0..graph[pos].len() {
-            let nex = graph[pos][i];
-            if dist[nex] == -1 {
-                dist[nex] = dist[pos] + 1;
-                queue.push_back(nex);
+    'a: for p in perm {
+        for i in 0..n {
+            for j in 0..n {
+                if v1[i][j] != v2[p[i]][p[j]] {
+                    continue 'a;
+                }
             }
         }
+
+        println!("Yes");
+        return;
     }
 
-    dist
+    println!("No");
 }

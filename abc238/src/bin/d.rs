67fe01cfd9e850f42c22b67d403ec168011c4d99
So Nishimura <s.nishimura@gemcook.com>
2022-10-22 00:31:13 +0900

    feat: abc238

--- abc238/src/bin/d.rs
+++ abc238/src/bin/d.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+    };
+
+    for _ in 0..t {
+        input! {
+            a: usize,
+            s: usize,
+        };
+
+        if a * 2 <= s {
+            let dif = s - a * 2;
+            if (dif & a) == 0 {
+                println!("Yes");
+                continue;
+            }
+        }
+
+        println!("No");
+    }
 }

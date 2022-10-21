a2a8a49518689327bc874e2f694966e99205bcb6
So Nishimura <s.nishimura@gemcook.com>
2022-10-21 23:51:47 +0900

    feat: abc230 d

--- abc230/src/bin/d.rs
+++ abc230/src/bin/d.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        d: usize,
+        mut lr: [(usize, usize); d],
+    };
+
+    lr.sort_by_key(|x| x.1);
+
+    let mut destroyed = 0;
 }

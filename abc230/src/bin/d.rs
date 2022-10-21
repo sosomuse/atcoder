76311d389c552aa62dd094d09514ee53b5d2b846
So Nishimura <s.nishimura@gemcook.com>
2022-10-21 23:53:55 +0900

    feat: abc230 d

--- abc230/src/bin/d.rs
+++ abc230/src/bin/d.rs
@@ -4,10 +4,20 @@ fn main() {
     input! {
         n: usize,
         d: usize,
-        mut lr: [(usize, usize); d],
+        mut lr: [(usize, usize); n],
     };
 
     lr.sort_by_key(|x| x.1);
 
+    let mut ans = 0;
     let mut destroyed = 0;
+
+    for (l, r) in lr {
+        if l > destroyed {
+            destroyed = r + d - 1;
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }

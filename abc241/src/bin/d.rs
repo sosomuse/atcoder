debc29269f94762e02a9ebd2fb787fd6f64a6670
So Nishimura <s.nishimura@gemcook.com>
2022-10-22 16:36:46 +0900

    feat: abc 241

--- abc241/src/bin/d.rs
+++ abc241/src/bin/d.rs
@@ -1,3 +1,58 @@
+use std::collections::BTreeSet;
+// use std::ops::Bound::Included;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    let mut set: BTreeSet<(isize, usize)> = BTreeSet::new();
+
+    for i in 0..q {
+        input! {
+            t: usize,
+            x: isize,
+        };
+
+        if t == 1 {
+            set.insert((x, i));
+        } else if t == 2 {
+            input! {
+                k: isize,
+            }
+
+            let mut elem = set.range((0, 0)..=(x, t)).rev();
+            for j in 1..=k {
+                if j == k {
+                    if let Some(v) = elem.next() {
+                        println!("{}", v.0);
+                    } else {
+                        println!("-1");
+                    }
+                } else {
+                    elem.next();
+                }
+            }
+        } else {
+            input! {
+                k: usize,
+            }
+
+            let mut elem = set.range((x, 0)..);
+            // dbg!(&elem);
+            for j in 1..=k {
+                if j == k {
+                    if let Some(v) = elem.next() {
+                        println!("{}", v.0);
+                    } else {
+                        println!("-1");
+                    }
+                } else {
+                    elem.next();
+                }
+            }
+        }
+    }
 }

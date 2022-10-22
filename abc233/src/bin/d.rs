251ad5ae824d539fb812e1eb2831123e6f53d1d0
So Nishimura <s.nishimura@gemcook.com>
2022-10-22 15:56:59 +0900

    feat: abc233 d

--- abc233/src/bin/d.rs
+++ abc233/src/bin/d.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: isize,
+        a: [isize; n],
+    };
+
+    let mut s = vec![0; n + 1];
+    let mut m = HashMap::new();
+
+    for i in 0..n {
+        s[i + 1] = a[i] + s[i];
+    }
+
+    let mut ans: isize = 0;
+
+    for i in 1..=n {
+        *m.entry([s[i - 1]]).or_insert(0) += 1;
+        if let Some(v) = m.get(&[s[i] - k]) {
+            ans += v;
+        }
+    }
+
+    println!("{}", ans);
 }

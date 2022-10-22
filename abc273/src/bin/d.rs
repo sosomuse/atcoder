210451bdc05c9375754e7b94c665aa42c7629e99
So Nishimura <s.nishimura@gemcook.com>
2022-10-22 19:30:38 +0900

    feat: abc273 d

--- abc273/src/bin/d.rs
+++ abc273/src/bin/d.rs
@@ -1,16 +1,16 @@
-use std::cmp::Ordering;
-use std::collections::HashMap;
+use std::cmp::*;
+use std::collections::{BTreeSet, HashMap};
 
 use proconio::input;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
-        mut r: usize,
-        mut c: usize,
+        h: isize,
+        w: isize,
+        mut r: isize,
+        mut c: isize,
         n: usize,
-        rc: [(usize, usize); n],
+        rc: [(isize, isize); n],
         q: usize,
     };
 
@@ -18,172 +18,71 @@ fn main() {
     let mut w_block = HashMap::new();
 
     for (r, c) in &rc {
-        h_block.entry(*r - 1).or_insert(vec![]).push(*c);
-        w_block.entry(*c - 1).or_insert(vec![]).push(*r);
+        h_block.entry(*r - 1).or_insert(BTreeSet::new()).insert(*c);
+        w_block.entry(*c - 1).or_insert(BTreeSet::new()).insert(*r);
     }
-
-    for v in h_block.values_mut() {
-        v.sort();
-    }
-
-    for v in w_block.values_mut() {
-        v.sort();
-    }
-
-    let get_limit = |d: char, l: usize, c: usize, r: usize| {
-        let limit = match d {
-            'U' => (r as isize - l as isize).max(1) as usize,
-            'D' => h.min(r + l),
-            'L' => (c as isize - l as isize).max(1) as usize,
-            'R' => w.min(c + l),
-            _ => todo!(),
-        };
-
-        limit
-    };
 
     for _ in 0..q {
         input! {
             d: char,
-            l: usize,
+            l: isize,
         }
+
+        let def = BTreeSet::<isize>::new();
 
         match d {
             'L' => {
-                if let Some(y) = h_block.get(&(r - 1)) {
-                    let mut index = 0;
-                    if let Some(t) = y.lower_bound(&(r)) {
-                        index = t.max(1);
-                    } else {
-                        index = y.len();
-                    }
-                    let z = &y[index - 1];
+                let y = h_block.get(&(r - 1)).unwrap_or(&def);
+                let mut t = y.range(..c);
+                let next = t.next_back();
 
-                    if z >= &c {
-                        c = get_limit(d, l, c, r);
-                    } else {
-                        if z + l >= c {
-                            c = z + 1;
-                        } else {
-                            c = get_limit(d, l, c, r);
-                        }
-                    }
+                c -= l;
+                if let Some(wall) = next {
+                    c = max(c, *wall + 1);
                 } else {
-                    c = get_limit(d, l, c, r);
+                    c = max(c, 1);
                 }
             }
             'R' => {
-                if let Some(y) = h_block.get(&(r - 1)) {
-                    if let Some(t) = y.lower_bound(&(c)) {
-                        let z = &y[t];
-                        if c + l >= *z {
-                            c = z - 1;
-                        } else {
-                            c = get_limit(d, l, c, r);
-                        }
-                    } else {
-                        c = get_limit(d, l, c, r);
-                    }
+                let y = h_block.get(&(r - 1)).unwrap_or(&def);
+                let mut t = y.range(c..);
+                let next = t.next();
+
+                c += l;
+                if let Some(wall) = next {
+                    c = min(c, *wall - 1);
                 } else {
-                    c = get_limit(d, l, c, r);
+                    c = min(c, h);
                 }
             }
             'U' => {
-                if let Some(y) = w_block.get(&(c - 1)) {
-                    let mut index = 0;
-                    if let Some(t) = y.lower_bound(&(r)) {
-                        index = t.max(1);
-                    } else {
-                        index = y.len();
-                    }
+                let y = w_block.get(&(c - 1)).unwrap_or(&def);
+                let mut t = y.range(..r);
+                let next = t.next_back();
 
-                    let z = &y[index - 1];
-
-                    if z >= &r {
-                        r = get_limit(d, l, c, r);
-                    } else {
-                        if z + l >= r {
-                            r = z + 1;
-                        } else {
-                            r = get_limit(d, l, c, r);
-                        }
-                    }
+                r -= l;
+                if let Some(wall) = next {
+                    r = max(r, *wall + 1);
                 } else {
-                    r = get_limit(d, l, c, r);
+                    r = max(r, 1);
                 }
             }
             'D' => {
-                if let Some(y) = w_block.get(&(c - 1)) {
-                    if let Some(t) = y.lower_bound(&(r)) {
-                        let z = &y[t];
+                let y = w_block.get(&(c - 1)).unwrap_or(&def);
+                let mut t = y.range(r..);
+                let next = t.next();
 
-                        if r + l >= *z {
-                            r = z - 1;
-                        } else {
-                            r = get_limit(d, l, c, r);
-                        }
-                    } else {
-                        r = get_limit(d, l, c, r);
-                    }
+                r += l;
+
+                if let Some(wall) = next {
+                    r = min(r, *wall - 1);
                 } else {
-                    r = get_limit(d, l, c, r);
+                    r = min(r, w);
                 }
             }
             _ => unreachable!(),
         }
 
         println!("{} {}", r, c);
-    }
-}
-
-pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> Option<usize>;
-    fn upper_bound(&self, x: &T) -> Option<usize>;
-}
-
-impl<T: Ord> BinarySearch<T> for [T] {
-    fn lower_bound(&self, x: &T) -> Option<usize> {
-        let mut low = 0;
-        let mut high = self.len();
-
-        while low != high {
-            let mid = (low + high) / 2;
-            match self[mid].cmp(x) {
-                Ordering::Less => {
-                    low = mid + 1;
-                }
-                Ordering::Equal | Ordering::Greater => {
-                    high = mid;
-                }
-            }
-        }
-
-        if self.len() > low && self[low] >= *x {
-            Some(low)
-        } else {
-            None
-        }
-    }
-
-    fn upper_bound(&self, x: &T) -> Option<usize> {
-        let mut low = 0;
-        let mut high = self.len();
-
-        while low != high {
-            let mid = (low + high) / 2;
-            match self[mid].cmp(x) {
-                Ordering::Less | Ordering::Equal => {
-                    low = mid + 1;
-                }
-                Ordering::Greater => {
-                    high = mid;
-                }
-            }
-        }
-        if self.len() > low && self[low] > *x {
-            Some(low)
-        } else {
-            None
-        }
     }
 }

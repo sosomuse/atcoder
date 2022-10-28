c0ddc5f4eac388333e7b62cdf4a18b2ca2cef035
So Nishimura <s.nishimura@gemcook.com>
2022-10-28 23:10:18 +0900

    feat: abc274 d

--- abc274/src/bin/d.rs
+++ abc274/src/bin/d.rs
@@ -10,76 +10,55 @@ fn main() {
         a: [usize; n],
     };
 
-    let x_sum = a
-        .iter()
-        .enumerate()
-        .filter(|(i, _)| i % 2 == 0)
-        .map(|(_, v)| v)
-        .sum::<usize>();
-    let y_sum = a
-        .iter()
-        .enumerate()
-        .filter(|(i, _)| i % 2 == 1)
-        .map(|(_, v)| v)
-        .sum::<usize>();
+    let max = 20010;
+    let mid = max / 2;
 
-    if x.abs() as usize > x_sum || y.abs() as usize > y_sum {
-        println!("No");
-        return;
-    }
+    let mut dp_x = vec![vec![false; max]; n + 1];
+    let mut dp_y = vec![vec![false; max]; n + 1];
 
-    let m = 10;
+    dp_x[0][mid] = true;
+    dp_y[0][mid] = true;
+    dp_x[1][mid + a[0]] = true;
+    dp_y[1][mid] = true;
 
-    let mut dp = vec![vec![vec![false; y_sum + m + 1]; x_sum + m + 1]; n + 1];
-    dp[0][m][m] = true;
-
-    for i in 1..=n {
+    for i in 2..=n {
         let v = a[i - 1];
 
-        for j in 0..=x_sum + m {
-            for k in 0..=y_sum + m {
-                if i % 2 == 1 {
-                    if j >= v && dp[i - 1][j - v][k] {
-                        dp[i][j][k] = true;
-                    }
+        for j in 0..max {
+            if i % 2 == 1 {
+                if j >= v && dp_x[i - 1][j - v] {
+                    dp_x[i][j] = true;
+                }
 
-                    if i > 1 && j + v < x_sum + m && dp[i - 1][j + v][k] {
-                        dp[i][j][k] = true;
-                    }
-                } else {
-                    if k >= v && dp[i - 1][j][k - v] {
-                        dp[i][j][k] = true;
-                    }
+                if j + v < max && dp_x[i - 1][j + v] {
+                    dp_x[i][j] = true;
+                }
 
-                    if k + v < y_sum + m && dp[i - 1][j][k + v] {
-                        dp[i][j][k] = true;
-                    }
+                if dp_y[i - 1][j] {
+                    dp_y[i][j] = true;
+                }
+            } else {
+                if dp_x[i - 1][j] {
+                    dp_x[i][j] = true;
+                }
+
+                if j >= v && dp_y[i - 1][j - v] {
+                    dp_y[i][j] = true;
+                }
+
+                if j + v < max && dp_y[i - 1][j + v] {
+                    dp_y[i][j] = true;
                 }
             }
         }
     }
 
-    let ans_x = {
-        if x > 0 {
-            x as usize + m
-        } else {
-            (x + m as isize).abs() as usize
-        }
-    };
+    let ans_x = (x + mid as isize).abs() as usize;
+    let ans_y = (y + mid as isize).abs() as usize;
 
-    let ans_y = {
-        if y > 0 {
-            y as usize + m
-        } else {
-            (y + m as isize).abs() as usize
-        }
-    };
-
-    if dp[n][ans_x][ans_y] {
+    if dp_x[n][ans_x] && dp_y[n][ans_y] {
         println!("Yes");
     } else {
         println!("No");
     }
-
-    // dbg!(ans_x, ans_y, &dp[2][8]);
 }

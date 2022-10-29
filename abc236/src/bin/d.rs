36905adf6784e92deda8c61ec9346d4d7c539267
So Nishimura <s.nishimura@gemcook.com>
2022-10-29 20:16:12 +0900

    feat: abc236

--- abc236/src/bin/d.rs
+++ abc236/src/bin/d.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let n2 = n * 2 - 1;
+
+    let mut s_level = vec![vec![]; n2];
+
+    for i in (1..=n2).rev() {
+        input! {
+            a: [usize; i],
+        }
+
+        for v in a {
+            s_level[n2 - i].push(v);
+        }
+    }
 }
+
+fn dfs(visible: &mut Vec<usize>, x: usize) -> () {}

d155d747aac1b73103c66947f1cf7962fee9f371
So Nishimura <s.nishimura@gemcook.com>
2022-10-29 20:35:36 +0900

    feat: abc246 d

--- abc246/src/bin/d.rs
+++ abc246/src/bin/d.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
+fn f(a: u64, b: u64) -> u64 {
+    a * a * a + a * a * b + a * b * b + b * b * b
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+    };
+
+    let mut k = 1000000;
+    let mut x = std::u64::MAX;
+
+    for i in 0..=1000000 {
+        while f(i, k) >= n {
+            x = x.min(f(i, k));
+
+            if k == 0 {
+                break;
+            };
+
+            k -= 1;
+        }
+    }
+
+    println!("{}", x);
 }

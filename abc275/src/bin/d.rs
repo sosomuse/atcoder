e3c8bed84e8714f69f28cf15c1bc16b86768abb6
So Nishimura <s.nishimura@gemcook.com>
2022-10-30 13:31:48 +0900

    feat: abc275 d

--- abc275/src/bin/d.rs
+++ abc275/src/bin/d.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut memo = HashMap::new();
+    memo.insert(0, 1);
+    memo.insert(1, 2);
+    memo.insert(2, 3);
+
+    println!("{}", f(n, &mut memo));
+}
+
+fn f(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
+    if let Some(v) = memo.get(&n) {
+        return *v;
+    }
+
+    let v = f(n / 2, memo) + f(n / 3, memo);
+
+    memo.insert(n, v);
+
+    return v;
 }

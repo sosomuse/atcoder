de12c1d8155943e54c712717841b601c8372e2e2
So Nishimura <s.nishimura@gemcook.com>
2022-11-03 14:56:31 +0900

    feat: abc228 d

--- abc228/src/bin/d.rs
+++ abc228/src/bin/d.rs
@@ -1,3 +1,40 @@
+use std::collections::BTreeSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        q: usize,
+    };
+
+    let n = 1048576;
+    let mut set = BTreeSet::new();
+    let mut vec: Vec<isize> = vec![-1; n];
+
+    for i in 0..n {
+        set.insert(i);
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: usize,
+        };
+
+        let h = x % n;
+
+        if t == 1 {
+            let mut range = set.range(h..);
+            let i = if let Some(i) = range.next() {
+                *i
+            } else {
+                *set.range(..).next().unwrap()
+            };
+
+            vec[i] = x as isize;
+            set.remove(&i);
+        } else {
+            println!("{}", vec[h]);
+        }
+    }
 }

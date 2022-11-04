aa5aceae965f5eeb5ce9d21942f499406d24fe81
So Nishimura <s.nishimura@gemcook.com>
2022-11-04 13:59:00 +0900

    feat: tessoku a22

--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -1,3 +1,33 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-1],
+    };
+
+    let mut dp = vec![0; n + 1];
+    let mut queue = VecDeque::new();
+    queue.push_back(1);
+
+    while let Some(i) = queue.pop_back() {
+        let x = a[i - 1];
+        let y = b[i - 1];
+
+        dp[x] = dp[x].max(dp[i] + 100);
+        dp[y] = dp[y].max(dp[i] + 150);
+
+        if x != n {
+            queue.push_back(x);
+        }
+
+        if y != n {
+            queue.push_back(y);
+        }
+    }
+
+    println!("{}", dp[n]);
 }

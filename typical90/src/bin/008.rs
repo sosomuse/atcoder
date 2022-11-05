f5cdac256172b44f812d514f10e62998aad3db24
So Nishimura <s.nishimura@gemcook.com>
2022-11-05 20:10:19 +0900

    feat: abc276

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,3 +1,20 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+
+    let mut p = vec![];
+    let mut count = 0;
+
+    for i in 0..n {
+        if s[i] == 'I' {
+            count += 1;
+        } else {
+            count -= 1;
+        }
+        p.push(count);
+    }
 }

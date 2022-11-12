516b67d9671bf6f366ba3c8773080510a817a450
soso-15315 <sonishimura15@gmail.com>
2022-11-12 17:30:31 +0900

    feat: abc212 c

--- abc212/src/bin/c.rs
+++ abc212/src/bin/c.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        mut b: [usize; m],
+    };
+
+    a.sort();
+    b.sort();
+
+    let mut ans = 1000000000;
+
+    for i in 0..n {
+        let v = a[i];
+
+        let j = b.binary_search(&v).unwrap_or_else(|j| j) as isize;
+
+        for k in j - 1..j + 1 {
+            if k < 0 || k >= m as isize {
+                continue;
+            }
+
+            let w = b[k as usize];
+            ans = ans.min((v as isize - w as isize).abs() as usize);
+        }
+    }
+
+    println!("{}", ans);
 }

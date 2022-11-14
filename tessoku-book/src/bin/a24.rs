f2f38f62e52ab783893bc4bcbd724a9d080ed9d3
soso-15315 <sonishimura15@gmail.com>
2022-11-14 22:47:36 +0900

    feat: tessoku a24

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut dp = vec![0; n];
+    let mut c = a[0];
+    dp[0] = 1;
+
+    for i in 1..n {
+        let v = a[i];
+        if v > c {
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            dp[i] = 1;
+        }
+
+        c = v;
+    }
+
+    println!("{}", dp.iter().max().unwrap());
 }

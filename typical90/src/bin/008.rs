81497a8a389b7bd108e7bdf6e09eba2ac2891294
soso-15315 <sonishimura15@gmail.com>
2022-11-18 23:56:22 +0900

    feat: typical 008

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,21 +1,45 @@
 use proconio::{input, marker::Chars};
 
-const A: [char; 7] = ['a', 't', 'c', 'o', 'd', 'e', 'r'];
-
 fn main() {
     input! {
         n: usize,
         s: Chars,
     }
 
-    let mut dp = vec![vec![0; 7]; n + 1];
+    let mut dp = vec![vec![0; 8]; n + 1];
+    dp[0][0] = 1;
 
     for i in 0..n {
-        let v = s[i];
-        for j in 0..7 {
-            if let Some(p) = A.iter().position(|&x| x == v) {};
+        for j in 0..=7 {
+            dp[i + 1][j] += dp[i][j];
 
-            dp[i + 1][j] = dp[i][j].max(dp[i + 1][j]);
+            if s[i] == 'a' && j == 0 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 't' && j == 1 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 'c' && j == 2 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 'o' && j == 3 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 'd' && j == 4 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 'e' && j == 5 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+            if s[i] == 'r' && j == 6 {
+                dp[i + 1][j + 1] += dp[i][j]
+            };
+        }
+
+        for j in 0..=7 {
+            dp[i + 1][j] %= 1_000_000_007;
         }
     }
+
+    println!("{}", dp[n][7]);
 }

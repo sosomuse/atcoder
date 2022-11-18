28ad3288f02ff75eb5da4281e598950f8a6b145a
soso-15315 <sonishimura15@gmail.com>
2022-11-18 23:43:11 +0900

    feat: typecal90

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,20 +1,21 @@
 use proconio::{input, marker::Chars};
 
+const A: [char; 7] = ['a', 't', 'c', 'o', 'd', 'e', 'r'];
+
 fn main() {
     input! {
         n: usize,
         s: Chars,
-    };
+    }
 
-    let mut p = vec![];
-    let mut count = 0;
+    let mut dp = vec![vec![0; 7]; n + 1];
 
     for i in 0..n {
-        if s[i] == 'I' {
-            count += 1;
-        } else {
-            count -= 1;
+        let v = s[i];
+        for j in 0..7 {
+            if let Some(p) = A.iter().position(|&x| x == v) {};
+
+            dp[i + 1][j] = dp[i][j].max(dp[i + 1][j]);
         }
-        p.push(count);
     }
 }

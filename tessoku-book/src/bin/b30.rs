61891a5e9cb1096e14730668bbc0af37146ed0fa
soso-15315 <sonishimura15@gmail.com>
2022-11-19 18:34:40 +0900

    feat: tessoku a43

--- tessoku-book/src/bin/b30.rs
+++ tessoku-book/src/bin/b30.rs
@@ -1,3 +1,8 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+    };
 }
--- tessoku-book/src/bin/b33.rs
+++ tessoku-book/src/bin/b33.rs
@@ -9,7 +9,7 @@ fn main() {
     };
 
     let mut sum_a = ab[0].0;
-    let mut sum_b = ab[0].0;
+    let mut sum_b = ab[0].1;
 
     for i in 1..n {
         sum_a = sum_a ^ (ab[i].0 - 1);

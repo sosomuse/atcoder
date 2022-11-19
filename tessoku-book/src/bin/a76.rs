3e72943d81f63aaa4289dd2c84971794cc916a12
soso-15315 <sonishimura15@gmail.com>
2022-11-19 20:30:28 +0900

    feat: tessoku a76

--- tessoku-book/src/bin/a76.rs
+++ tessoku-book/src/bin/a76.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        _r: usize,
+        _l: usize,
+        x: [usize; n],
+    };
+
+    let _mod = 1_000_000_007;
+    let mut dp = vec![0; n + 1];
+    let mut sum = vec![0; n + 1];
+    let mut _x = vec![0; n + 2];
+
+    for i in 0..n {
+        _x[i + 1] = x[i];
+    }
+    _x[n + 1] = w;
+
+    dp[0] = 1;
+    sum[0] = 1;
 }

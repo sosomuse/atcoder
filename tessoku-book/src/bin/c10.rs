dd9c13d30950caa22c6eaa1edeab4256d7b82297
soso-15315 <sonishimura15@gmail.com>
2022-11-26 14:13:53 +0900

    feat: tessoku c10

--- tessoku-book/src/bin/c10.rs
+++ tessoku-book/src/bin/c10.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        w: usize,
+    };
+
+    let m = 1000000007;
+    println!("{}", 12 * power(7, w - 1, m) % m);
+}
+
+fn power(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut ans = 1;
+
+    for i in 0..60 {
+        let w = 1 << i;
+        if (b & w) != 0 {
+            ans = ans * p % m;
+        }
+        p = p * p % m;
+    }
+
+    ans
 }

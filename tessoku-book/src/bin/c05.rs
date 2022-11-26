b6d30e2b7287c7c2748635b9373418b9c8fedeca
soso-15315 <sonishimura15@gmail.com>
2022-11-26 12:10:49 +0900

    feat: tessoku c05

--- tessoku-book/src/bin/c05.rs
+++ tessoku-book/src/bin/c05.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut s = vec![];
+
+    for bit in 0..(1 << 10) {
+        let mut sum = 0;
+        for i in 0..10 {
+            if bit & (1 << i) == 0 {
+                sum += 10_usize.pow(i) * 4
+            } else {
+                sum += 10_usize.pow(i) * 7
+            }
+        }
+        s.push(sum);
+    }
+
+    s.sort();
+
+    println!("{}", s[n - 1]);
 }

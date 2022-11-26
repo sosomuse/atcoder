d20e019da7e26d401236a2a0c71b842ed882801a
soso-15315 <sonishimura15@gmail.com>
2022-11-26 12:53:09 +0900

    feat: tessoku c07

--- tessoku-book/src/bin/c07.rs
+++ tessoku-book/src/bin/c07.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut c: [usize; n],
+        q: usize,
+    };
+
+    c.sort();
+    let mut vec = vec![0; n + 1];
+
+    for i in 0..n {
+        vec[i + 1] = vec[i] + c[i];
+    }
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+        };
+
+        let ans = vec.binary_search(&x).unwrap_or_else(|i| i - 1);
+
+        println!("{}", ans);
+    }
 }

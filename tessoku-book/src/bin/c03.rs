f8375f91e4ce79f5e51d14fed929217db2ea3bb1
soso-15315 <sonishimura15@gmail.com>
2022-11-26 11:45:30 +0900

    feat: tessoku c03

--- tessoku-book/src/bin/c03.rs
+++ tessoku-book/src/bin/c03.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        d: usize,
+        x: isize,
+        a: [isize; d - 1],
+        q: usize,
+    };
+
+    let mut m: Vec<isize> = vec![0; d];
+    m[0] = x;
+
+    for i in 0..a.len() {
+        m[i + 1] = m[i] + a[i];
+    }
+
+    for _ in 0..q {
+        input! {
+            s: usize,
+            t: usize,
+        }
+
+        if m[s - 1] == m[t - 1] {
+            println!("Same");
+        } else if m[s - 1] > m[t - 1] {
+            println!("{}", s);
+        } else {
+            println!("{}", t);
+        }
+    }
 }

24148b4e1ec30c44aba269a869a16668ecbbac08
soso-15315 <sonishimura15@gmail.com>
2022-11-26 11:39:04 +0900

    feat: tessoku c02

--- tessoku-book/src/bin/c02.rs
+++ tessoku-book/src/bin/c02.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    };
+
+    a.sort_by(|a, b| b.cmp(a));
+
+    println!("{}", a[0] + a[1]);
 }

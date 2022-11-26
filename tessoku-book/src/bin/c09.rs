2665cf389e05f16b7620d3b5ff6432c332233440
soso-15315 <sonishimura15@gmail.com>
2022-11-26 14:00:45 +0900

    feat: tessoku c09

--- tessoku-book/src/bin/c09.rs
+++ tessoku-book/src/bin/c09.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut dp = vec![0; n + 1];
+    dp[1] = a[0];
+
+    for i in 2..=n {
+        let v = a[i - 1];
+        dp[i] = dp[i - 1].max(dp[i - 2] + v);
+    }
+
+    println!("{}", dp[n]);
 }

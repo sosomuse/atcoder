f5fd409b801c2ce6a32641fec4b8792043e6d63d
soso-15315 <sonishimura15@gmail.com>
2022-12-01 23:51:59 +0900

    feat: abc217 d

--- abc217/src/bin/d.rs
+++ abc217/src/bin/d.rs
@@ -1,3 +1,29 @@
+use std::collections::BTreeSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        l: usize,
+        q: usize,
+    };
+
+    let mut s = BTreeSet::new();
+    s.insert(0);
+    s.insert(l);
+
+    for _ in 0..q {
+        input! {
+            c: usize,
+            x: usize,
+        }
+
+        if c == 1 {
+            s.insert(x);
+        } else {
+            let r = s.range(x..).next().unwrap();
+            let l = s.range(..x).rev().next().unwrap();
+            println!("{}", r - l);
+        }
+    }
 }

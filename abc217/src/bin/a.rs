904ddda36ccc0626bd3b8352a7dd9404f38221f3
soso-15315 <sonishimura15@gmail.com>
2022-12-01 23:29:44 +0900

    feat: abc217 a

--- abc217/src/bin/a.rs
+++ abc217/src/bin/a.rs
@@ -1,3 +1,15 @@
+use std::cmp::Ordering;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        t: String,
+    };
+
+    match s.cmp(&t) {
+        Ordering::Less => println!("Yes"),
+        _ => println!("No"),
+    }
 }

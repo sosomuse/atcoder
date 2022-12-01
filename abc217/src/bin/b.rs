62e4810ffc1cabd706ed5642d36b52e0b24a8f32
soso-15315 <sonishimura15@gmail.com>
2022-12-01 23:31:51 +0900

    feat: abc217 b

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: [String; 3],
+    };
+
+    let mut t = vec![
+        "ABC".to_string(),
+        "ARC".to_string(),
+        "AGC".to_string(),
+        "AHC".to_string(),
+    ];
+
+    for s_i in s {
+        t.retain(|t_i| t_i != &s_i);
+    }
+
+    println!("{}", t[0]);
 }

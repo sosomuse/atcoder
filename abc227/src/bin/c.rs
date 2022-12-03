c7deb08da33cbfa4633877a5b240c18a040bf784
soso-15315 <sonishimura15@gmail.com>
2022-12-03 20:27:44 +0900

    feat: abc280

--- abc227/src/bin/c.rs
+++ abc227/src/bin/c.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = 0;
+    ans += n;
+    let mut t = n / 2;
+    let mut wari = 2;
+
+    while t >= n / t {
+        ans += fnc(n).len() / 2;
+        wari += 1;
+        t = n / wari;
+    }
+
+    println!("{}", ans);
+}
+
+fn fnc(n: usize) -> Vec<usize> {
+    let mut lst: Vec<usize> = vec![];
+
+    let mut i = 1;
+
+    while i * i <= n {
+        if n % i == 0 {
+            lst.push(i);
+            if i != n / i {
+                lst.push(n / i);
+            }
+        }
+
+        i += 1;
+    }
+
+    lst
 }

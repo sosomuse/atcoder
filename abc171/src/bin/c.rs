6a54678dda0a5eed86bbb42e466239b338c565e7
soso-15315 <sonishimura15@gmail.com>
2022-12-10 20:31:29 +0900

    feat: abc171 c

--- abc171/src/bin/c.rs
+++ abc171/src/bin/c.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut n: usize,
+    };
+
+    let mut ans = vec![];
+
+    while n > 0 {
+        if n > 26 {
+            let m = n % 26;
+            if m == 0 {
+                ans.push('z');
+                n = n / 26 - 1;
+            } else {
+                ans.push((m + 96) as u8 as char);
+                n = n / 26;
+            }
+        } else {
+            ans.push((n + 96) as u8 as char);
+            n = 0;
+        }
+    }
+
+    println!("{}", ans.iter().rev().collect::<String>());
 }

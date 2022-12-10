e9385a44e640502faefea1ca4805e31a558d638d
soso-15315 <sonishimura15@gmail.com>
2022-12-10 20:24:13 +0900

    feat: abc171 e

--- abc171/src/bin/e.rs
+++ abc171/src/bin/e.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let x = a.iter().fold(0, |acc, x| acc ^ x);
+
+    for i in 0..n {
+        print!("{} ", x ^ a[i]);
+    }
 }

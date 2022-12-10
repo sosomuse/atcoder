4ded10a14002a3af4abe111c043151ba745a6203
soso-15315 <sonishimura15@gmail.com>
2022-12-10 20:13:29 +0900

    feat: abc184

--- abc184/src/bin/b.rs
+++ abc184/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        x: isize,
+        s: Chars,
+    };
+
+    let mut ans: isize = x;
+
+    for v in s {
+        if v == 'o' {
+            ans += 1;
+        } else {
+            ans = 0.max(ans - 1);
+        }
+    }
+
+    println!("{}", ans);
 }

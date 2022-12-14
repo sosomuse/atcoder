10d119ff45add8477957b1b71159cb30b86f4426
soso-15315 <sonishimura15@gmail.com>
2022-12-14 19:37:48 +0900

    feat: abc170

--- abc281/src/bin/e.rs
+++ abc281/src/bin/e.rs
@@ -1,6 +1,8 @@
-use std::collections::{BTreeSet, HashMap};
+use std::collections::BTreeSet;
 
 use proconio::input;
+
+#[allow(unused_variables, unused_mut, dead_code)]
 
 fn main() {
     input! {
@@ -10,18 +12,15 @@ fn main() {
         a: [usize; n],
     };
 
-    let mut b = BTreeSet::new();
-    let mut map = HashMap::new();
+    let mut multi_set = MultiSet::new();
     let mut c = vec![];
 
     for i in 0..m {
-        *map.entry(a[i]).or_insert(0) += 1;
-        b.insert(a[i]);
+        multi_set.insert(a[i]);
         c.push(a[i]);
     }
 
     c.sort();
-    let mut min = *c.iter().take(k).min().unwrap();
     let mut max = *c.iter().take(k).max().unwrap();
     let mut sum = c.iter().take(k).sum::<usize>();
 
@@ -29,30 +28,67 @@ fn main() {
 
     for i in 0..m - 2 {
         let v1 = &a[i];
-        let v2 = &a[i + k];
+        let v2 = &a[i + m];
 
-        *map.entry(*v2).or_insert(0) += 1;
-        b.insert(*v2);
+        multi_set.remove(*v1);
 
-        let mut range1 = b.range(max..);
-        let mut range2 = b.range(..max);
+        // dbg!(i, v1, v2);
+        // dbg!(sum, &multi_set);
 
         if *v1 <= max {
-            sum -= v1;
-            sum += *range1.next().unwrap();
+            sum -= *v1;
+            let range1 = multi_set.set.range(..=(max, 1000000));
+
+            if range1.count() < k {
+                let mut range2 = multi_set.set.range((max + 1, 0)..);
+                let (x, _) = range2.next().unwrap();
+                max = *x;
+                sum += max;
+            }
         }
 
-        if *v2 < max {
-            sum += v2;
+        multi_set.insert(*v2);
+
+        if *v2 <= max {
+            sum += *v2;
             sum -= max;
-        }
 
-        min = *b.iter().next().unwrap();
+            let range1 = multi_set.set.range(..=(max - 1, 1000000));
+
+            if range1.count() >= k {
+                let mut range2 = multi_set.set.range(..(max - 1, 1000000));
+                let (x, _) = range2.rev().next().unwrap();
+                max = *x;
+            }
+        }
 
         print!("{} ", sum);
     }
 }
 
-pub trait BinarySearch<T> {
-    fn lower_bound(&self, x: &T) -> usize;
+#[derive(Clone, Debug)]
+struct MultiSet {
+    set: BTreeSet<(usize, usize)>,
+}
+
+impl MultiSet {
+    fn new() -> Self {
+        Self {
+            set: BTreeSet::new(),
+        }
+    }
+    fn insert(&mut self, x: usize) {
+        let mut e = (x, 1);
+        if let Some((_, c)) = self.set.range((x, 0)..).next() {
+            e = (x, c + 1);
+        }
+        self.set.insert(e);
+    }
+    fn remove(&mut self, x: usize) {
+        if let Some((x2, c)) = self.set.range((x, 0)..).next() {
+            if x == *x2 {
+                self.set.remove(&(x, *c));
+            }
+        }
+    }
 }

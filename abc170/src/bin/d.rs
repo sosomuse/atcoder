99bcee2ad0c99adbbfc659c078f3eaee4853430b
soso-15315 <sonishimura15@gmail.com>
2022-12-14 20:55:06 +0900

    feat: abc170 d

--- abc170/src/bin/d.rs
+++ abc170/src/bin/d.rs
@@ -1,3 +1,44 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    };
+
+    let mut ok: Vec<bool> = vec![true; 1000001];
+    a.sort();
+    let mut map: HashMap<usize, usize> = HashMap::new();
+
+    for i in 0..n {
+        let v = a[i];
+
+        *map.entry(a[i]).or_insert(0) += 1;
+
+        if map[&a[i]] > 1 {
+            ok[a[i]] = false;
+        }
+
+        if ok[v] {
+            for j in 2.. {
+                let k = v * j;
+                if k >= 1000001 {
+                    break;
+                }
+                ok[k] = false;
+            }
+        }
+    }
+
+    let mut ans: usize = 0;
+
+    for i in 0..n {
+        if ok[a[i]] {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }

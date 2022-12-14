567ee62f14cab4da8544cf7f4e27cd0f76a56d87
soso-15315 <sonishimura15@gmail.com>
2022-12-14 20:36:17 +0900

    feat: abc170 c

--- abc170/src/bin/c.rs
+++ abc170/src/bin/c.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: isize,
+        n: usize,
+        mut p: [isize; n],
+    };
+
+    let mut vec = vec![];
+
+    p.sort();
+
+    for i in -100..200 {
+        if let Err(_) = p.binary_search(&i) {
+            vec.push(i);
+        }
+    }
+
+    let mut ans = 0;
+    let mut diff = 1000;
+
+    for v in vec.iter() {
+        if diff > (x - v).abs() {
+            diff = (x - v).abs();
+            ans = *v;
+        }
+    }
+
+    println!("{}", ans);
 }

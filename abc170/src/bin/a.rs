68d037ef959a76a513e837349e8d9fe596b770f2
soso-15315 <sonishimura15@gmail.com>
2022-12-14 19:39:56 +0900

    feat: ac170 a

--- abc170/src/bin/a.rs
+++ abc170/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        x: [usize; 5],
+    }
+
+    let mut ans = 0;
+
+    for i in 0..5 {
+        let v = x[i];
+        if v == 0 {
+            ans = i + 1;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }

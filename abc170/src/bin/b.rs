fe8e3c6297c2b6001451448a3462f1836c07df73
soso-15315 <sonishimura15@gmail.com>
2022-12-14 19:42:01 +0900

    feat: abc170 b

--- abc170/src/bin/b.rs
+++ abc170/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+        y: usize,
+    };
+
+    if x * 4 < y {
+        println!("No");
+        return;
+    }
+
+    if x * 2 > y {
+        println!("No");
+        return;
+    }
+
+    if y % 2 == 1 {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
 }

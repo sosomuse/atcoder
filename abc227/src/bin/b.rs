129e6950f9500b377e74d4c409dbf99ff3001ba3
soso-15315 <sonishimura15@gmail.com>
2022-12-26 23:56:13 +0900

    feat: abc227 b

--- abc227/src/bin/b.rs
+++ abc227/src/bin/b.rs
@@ -9,8 +9,8 @@ fn main() {
     let mut ans = 0;
 
     for v in s {
-        'outer: for a in 1..30 {
-            for b in 1..30 {
+        'outer: for a in 1..300 {
+            for b in 1..300 {
                 if 4 * a * b + 3 * a + 3 * b == v {
                     ans += 1;
                     break 'outer;

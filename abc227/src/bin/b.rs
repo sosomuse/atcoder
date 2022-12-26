5cae874dc3f61f3d4fc5d90bf9744db12659c0e5
soso-15315 <sonishimura15@gmail.com>
2022-12-26 23:55:07 +0900

    feat: abc227 b

--- abc227/src/bin/b.rs
+++ abc227/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [usize; n],
+    };
+
+    let mut ans = 0;
+
+    for v in s {
+        'outer: for a in 1..30 {
+            for b in 1..30 {
+                if 4 * a * b + 3 * a + 3 * b == v {
+                    ans += 1;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    println!("{}", n - ans);
 }

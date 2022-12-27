0980d3c51e67b7b0c5fc723e7a8a96191eb2c8b8
So Nishimura <s.nishimura@gemcook.com>
2022-12-27 09:26:47 +0900

    feat: abc227

--- abc227/src/bin/c.rs
+++ abc227/src/bin/c.rs
@@ -7,33 +7,25 @@ fn main() {
 
     let mut ans = 0;
     ans += n;
-    let mut t = n / 2;
-    let mut wari = 2;
+    let mut b = 2;
+    let mut c = n / 2;
 
-    while t >= n / t {
-        ans += fnc(n).len() / 2;
-        wari += 1;
-        t = n / wari;
+    while b <= c {
+        ans += c - b + 1;
+        b += 1;
+        c = n / b;
+    }
+
+    let mut a = 2;
+    let mut b = 2;
+    let mut c = n / (a * b);
+
+    while (a * b) <= c {
+        ans += c - b + 1;
+        a += 1;
+        b += 1;
+        c = n / (a * b);
     }
 
     println!("{}", ans);
-}
-
-fn fnc(n: usize) -> Vec<usize> {
-    let mut lst: Vec<usize> = vec![];
-
-    let mut i = 1;
-
-    while i * i <= n {
-        if n % i == 0 {
-            lst.push(i);
-            if i != n / i {
-                lst.push(n / i);
-            }
-        }
-
-        i += 1;
-    }
-
-    lst
 }

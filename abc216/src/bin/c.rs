7e00970c49baf250c8b2228d0f11d1532478c930
soso-15315 <sonishimura15@gmail.com>
2022-12-30 13:51:32 +0900

    feat: abc216 c

--- abc216/src/bin/c.rs
+++ abc216/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut n: usize,
+    };
+
+    let mut ans = String::from("");
+
+    while n != 0 {
+        if n == 1 || n % 2 == 1 {
+            ans.push('A');
+            n -= 1;
+        } else {
+            ans.push('B');
+            n /= 2;
+        }
+    }
+
+    println!("{}", ans.chars().rev().collect::<String>());
 }

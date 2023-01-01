d95203beca1d3fbb733c07e5e6b13a0291a3e941
soso-15315 <sonishimura15@gmail.com>
2023-01-01 20:49:08 +0900

    feat: abc204 d

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -11,18 +11,36 @@ fn main() {
     let mut dp = vec![vec![false; sum + 1]; n + 1];
     dp[0][0] = true;
 
-    for v in t {
-        for i in 1..=n {
-            for j in 1..=sum {
-                if dp[i - 1][j] {
-                    dp[i][j] = true;
-                }
-                if j >= v && dp[i - 1][j - v] {
-                    dp[i][j] = true;
-                }
+    for i in 1..=n {
+        let v = t[i - 1];
+
+        for j in 0..=sum {
+            if dp[i - 1][j] {
+                dp[i][j] = true;
+            }
+            if j >= v && dp[i - 1][j - v] {
+                dp[i][j] = true;
             }
         }
     }
 
-    dbg!(dp);
+    let mid = sum / 2;
+    let mut ans = sum;
+    let mut ans_diff = sum;
+
+    for (i, v) in dp[n].iter().enumerate() {
+        if *v {
+            let diff = (i as i64 - mid as i64).abs() as usize;
+            if ans_diff > diff {
+                ans_diff = diff;
+                ans = i;
+            }
+        }
+    }
+
+    if ans <= mid {
+        println!("{}", sum - ans);
+    } else {
+        println!("{}", ans);
+    }
 }

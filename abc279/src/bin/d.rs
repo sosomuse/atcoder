54ba6c2a48b017d03e4198da997d23cf39ca9f2d
soso-15315 <sonishimura15@gmail.com>
2023-01-02 14:42:24 +0900

    feat: abc279 d

--- abc279/src/bin/d.rs
+++ abc279/src/bin/d.rs
@@ -5,48 +5,14 @@ fn main() {
         a: f64,
         b: f64,
     }
-
-    // a / √g
-    let mut g: f64 = 1.;
-    let mut v: f64 = 0.;
-
-    let mut ans = a / g.sqrt();
-    let mut prev = a / g.sqrt();
-
-    loop {
-        g += 10000.;
-        v += b * 10000.;
-
-        let next = a / g.sqrt() + v;
-
-        if next > prev {
-            if next < ans {
-                ans = next;
-            }
-
-            break;
-        }
-
-        prev = next;
-
-        if next < ans {
-            ans = next;
-        }
+    let x = (2. * b / a).powf(-2.0 / 3.0) - 1.0;
+    if x < 0.0 {
+        println!("{}", a);
+    } else {
+        let mut c = (x as i64) as f64;
+        let mut ans = b * c + a * (1.0 + c).powf(-1.0 / 2.0);
+        c += 1.0;
+        ans = ans.min(b * c + a * (1.0 + c).powf(-1.0 / 2.0));
+        println!("{}", ans);
     }
-
-    for _ in 0..1000000 {
-        if g == 1. {
-            break;
-        }
-
-        g -= 1.;
-        v -= b;
-        let time = a / g.sqrt() + v;
-
-        if time < ans {
-            ans = time;
-        }
-    }
-
-    println!("{}", ans);
 }

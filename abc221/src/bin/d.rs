a2937838d9f66ca2406a9bb76f11b4c4761e5d52
soso-15315 <sonishimura15@gmail.com>
2023-01-02 13:30:54 +0900

    feat: abc221

--- abc221/src/bin/d.rs
+++ abc221/src/bin/d.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        ab: [(usize, usize); n],
+    };
+
+    let mut x: Vec<(usize, isize)> = vec![];
+
+    for (a, b) in ab {
+        x.push((a, 1));
+        x.push((a + b, -1));
+    }
+
+    x.sort();
+
+    let mut ans = vec![0; n + 1];
+    let mut cnt = 0;
+
+    for i in 0..x.len() - 1 {
+        cnt += x[i].1;
+        ans[cnt as usize] += x[i + 1].0 - x[i].0;
+    }
+
+    for i in 1..n + 1 {
+        print!("{} ", ans[i]);
+    }
 }

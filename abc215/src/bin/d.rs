96dd9e174fdb9c4bca80b971b11b4ef156095e49
soso-15315 <sonishimura15@gmail.com>
2023-01-02 17:48:51 +0900

    feat: abc215 d

--- abc215/src/bin/d.rs
+++ abc215/src/bin/d.rs
@@ -1,3 +1,71 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+    };
+
+    a.sort();
+
+    // 2以上M以下の整数に対して、Aの約数 or Aの倍数が存在しないものの個数を求める
+    let mut s = vec![true; 100001];
+
+    for i in 0..n {
+        let v = a[i];
+
+        let lst = prime_factorize(v);
+
+        for mut t in lst {
+            if !s[t] {
+                continue;
+            }
+            let c = t;
+
+            while t <= m {
+                s[t] = false;
+                t += c;
+            }
+        }
+    }
+
+    let mut ans = vec![];
+
+    for i in 1..=m {
+        if s[i] {
+            ans.push(i);
+        }
+    }
+
+    println!("{}", ans.len());
+    for i in 0..ans.len() {
+        print!("{} ", ans[i]);
+    }
+}
+
+fn prime_factorize(mut n: usize) -> Vec<usize> {
+    let mut ans = vec![];
+
+    for p in 2..=n {
+        if p * p > n {
+            break;
+        }
+
+        if n % p != 0 {
+            continue;
+        }
+
+        while n % p == 0 {
+            n /= p;
+        }
+
+        ans.push(p);
+    }
+
+    if n != 1 {
+        ans.push(n);
+    }
+
+    ans
 }

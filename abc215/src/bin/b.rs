fcdd2ae0c73bba601a3f3e8fa325b3e7c9a8eeea
soso-15315 <sonishimura15@gmail.com>
2023-01-02 17:05:34 +0900

    feat: abc215 b

--- abc215/src/bin/b.rs
+++ abc215/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut n: usize,
+    };
+
+    let mut ans = 0;
+
+    while n > 1 {
+        n /= 2;
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }

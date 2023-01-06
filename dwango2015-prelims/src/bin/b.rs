bc4f00a8fb6d299f41d5873170868b5d3319b908
So Nishimura <s.nishimura@gemcook.com>
2023-01-06 22:11:16 +0900

    feat: dwango2015

--- dwango2015-prelims/src/bin/b.rs
+++ dwango2015-prelims/src/bin/b.rs
@@ -1,3 +1,41 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut a = vec![0; s.len() / 2 + 1];
+
+    let mut prev = s[0];
+    let mut count = 0;
+
+    for i in 1..s.len() {
+        let v = s[i];
+
+        if prev == '5' && v == '2' {
+            prev = v;
+            continue;
+        }
+
+        if prev == '2' && v == '5' {
+            count += 1;
+        } else {
+            a[count] += 1;
+            count = 0;
+        }
+
+        prev = v;
+    }
+
+    a[count] += 1;
+
+    let mut ans = 0;
+
+    for i in 1..a.len() {
+        let v = a[i];
+        ans += (1 + i) * i / 2 * v;
+    }
+
+    println!("{}", ans);
 }

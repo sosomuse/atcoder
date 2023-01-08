170e63b90aa7db7ce528c2e9ce7be2b9bbd7edc0
soso-15315 <sonishimura15@gmail.com>
2023-01-08 20:26:14 +0900

    feat: abc284 e

--- abc284/src/bin/e.rs
+++ abc284/src/bin/e.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashSet, VecDeque};
-
 use proconio::input;
 
 const MAX: usize = 1000000;
@@ -18,58 +16,28 @@ fn main() {
         graph[v].push(u);
     }
 
-    let mut ans = 1;
+    let mut ans = 0_usize;
+    let mut visited = vec![false; n + 1];
 
-    bfs(1, &graph, &mut ans);
+    dfs(1, &graph, &mut visited, &mut ans);
 
     println!("{}", ans);
 }
 
-struct Node {
-    v: usize,
-    visited: HashSet<usize>,
-}
+// パス数の数え上げ
+fn dfs(v: usize, graph: &Vec<Vec<usize>>, visited: &mut Vec<bool>, ans: &mut usize) {
+    if *ans == MAX {
+        return;
+    }
 
-fn bfs(v: usize, graph: &Vec<Vec<usize>>, ans: &mut usize) -> () {
-    let mut queue: VecDeque<Node> = std::collections::VecDeque::new();
-    let mut set = HashSet::new();
-    set.insert(v);
+    visited[v] = true;
+    *ans += 1;
 
-    queue.push_front(Node { v, visited: set });
-
-    while !queue.is_empty() {
-        let pos = queue.pop_front().unwrap();
-
-        for i in 0..graph[pos.v].len() {
-            if *ans >= MAX {
-                return;
-            }
-            let nex = graph[pos.v][i];
-            if pos.visited.contains(&nex) {
-                continue;
-            }
-            *ans += 1;
-            let mut set = pos.visited.clone();
-            set.insert(nex);
-            queue.push_back(Node {
-                v: nex,
-                visited: set,
-            });
+    for &w in graph[v].iter() {
+        if !visited[w] {
+            dfs(w, graph, visited, ans);
         }
     }
+
+    visited[v] = false;
 }
-
-// fn dfs(v: usize, graph: &Vec<Vec<usize>>, visited: HashSet<usize>, ans: &mut usize) {
-//     if *ans >= MAX {
-//         return;
-//     }
-
-//     for &w in graph[v].iter() {
-//         if !visited.contains(&w) {
-//             let mut set = visited.clone();
-//             set.insert(w);
-//             *ans += 1;
-//             dfs(w, graph, set, ans);
-//         }
-//     }
-// }

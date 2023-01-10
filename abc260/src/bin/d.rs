51358521c5625199667449e99d3df69ef020aea5
soso-15315 <sonishimura15@gmail.com>
2023-01-10 22:18:11 +0900

    feat: abc260 d

--- abc260/src/bin/d.rs
+++ abc260/src/bin/d.rs
@@ -2,6 +2,7 @@ use std::collections::{BTreeSet, HashMap};
 
 use proconio::input;
 
+#[allow(unused_assignments)]
 fn main() {
     input! {
         n: usize,
@@ -10,40 +11,47 @@ fn main() {
     };
 
     // 場に表示されているカード
-    let mut set = BTreeSet::new();
-    // 場に表示されているカードがどこに属しているか
-    let mut map = HashMap::new();
+    let mut set = BTreeSet::<usize>::new();
+    // カードがどの山札にあるか
+    let mut map = HashMap::<usize, usize>::new();
     // 実際のカード群
-    let mut vec: Vec<Vec<usize>> = vec![vec![]; n];
-    // 山札のカウント
-    let mut count = 0;
-    // どのカード群がいつ食べられたか
-    let mut ans = HashMap::new();
+    let mut cards: Vec<Vec<usize>> = vec![];
+    // 答え isizeとusizeの共用体
+    let mut ans: Vec<isize> = vec![-1; n + 1];
 
     for i in 0..n {
-        let v = p[i];
-        let mut z = 0;
+        let x = p[i];
+        let mut min = None;
+        let mut count = 0;
 
-        if let Some(x) = set.range(v..).next() {
-            z = *x;
-            let y = map.get(x).unwrap();
-            map.insert(v, *y);
-            set.insert(v);
-        } else {
-            vec[count].push(v);
-
-            if k != 1 {
-                map.insert(v, count);
-                set.insert(v);
-            } else {
-                ans.insert(count, k);
-            }
-
-            count += 1;
+        let mut range = set.range(x..);
+        if let Some(&y) = range.next() {
+            min = Some(y);
         }
 
-        set.remove(&z);
+        if let Some(min) = min {
+            set.remove(&min);
+            count = map[&min];
+            map.insert(x, count);
+        } else {
+            count = cards.len();
+            map.insert(x, count);
+            cards.push(vec![]);
+        }
+
+        cards[count].push(x);
+        set.insert(x);
+
+        if cards[count].len() == k {
+            set.remove(&x);
+
+            for &y in &cards[count] {
+                ans[y] = (i + 1) as isize;
+            }
+        }
     }
 
-    println!("{}", n);
+    for i in 1..=n {
+        println!("{}", ans[i]);
+    }
 }

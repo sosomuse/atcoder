691679329f20461a1cf4b94dabc01735c7657a72
soso-15315 <sonishimura15@gmail.com>
2023-01-11 23:55:24 +0900

    feat: arc042 a

--- arc042/src/bin/a.rs
+++ arc042/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [Usize1; m],
+    };
+
+    let mut s = vec![true; n];
+    for i in (0..m).rev() {
+        if s[a[i]] {
+            println!("{}", a[i] + 1);
+        }
+        s[a[i]] = false;
+    }
+
+    for i in 0..n {
+        if s[i] {
+            println!("{}", i + 1);
+        }
+    }
 }

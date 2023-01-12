1c17423d2354495b2c3f84136e9f4a6a6b4fdc30
soso-15315 <sonishimura15@gmail.com>
2023-01-12 13:32:49 +0900

    feat: abc048 b

--- abc048/src/bin/b.rs
+++ abc048/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        x: usize,
+    };
+
+    let mut q = b / x;
+    let p = {
+        if a == 0 {
+            0
+        } else {
+            (a - 1) / x
+        }
+    };
+
+    if a == 0 {
+        q += 1;
+    }
+
+    println!("{}", q - p);
 }

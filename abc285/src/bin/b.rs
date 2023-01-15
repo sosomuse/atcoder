00f0c7ab73c4926b93ebbe4965b981470aa652b3
soso-15315 <sonishimura15@gmail.com>
2023-01-16 00:14:38 +0900

    feat: abc285 b

--- abc285/src/bin/b.rs
+++ abc285/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+
+    for i in 1..n {
+        let mut count = 0;
+        let l = n - i;
+        for k in 1..=l {
+            if s[k - 1] != s[k + i - 1] {
+                count += 1;
+            } else {
+                break;
+            }
+        }
+
+        println!("{}", count);
+    }
 }

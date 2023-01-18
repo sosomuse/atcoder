0ca427a5dc0b9488e1ac91e14fbd8f3319ed7c02
soso-15315 <sonishimura15@gmail.com>
2023-01-18 13:36:10 +0900

    feat: abc231

--- abc027/src/bin/b.rs
+++ abc027/src/bin/b.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    };
+
+    let sum = a.iter().sum::<usize>();
+
+    if sum % n != 0 {
+        println!("-1");
+        return;
+    }
+
+    let avg = sum / n;
+
+    let mut ans = vec![false; n - 1];
+    let mut sum = 0;
+    let mut count = 0;
+
+    for i in 0..n - 1 {
+        sum += a[i];
+        count += 1;
+
+        if sum == avg * count {
+            sum = 0;
+            count = 0;
+            continue;
+        }
+
+        ans[i] = true;
+    }
+
+    println!("{}", ans.iter().filter(|&&x| x).count());
 }

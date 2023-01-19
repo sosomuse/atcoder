e7e0429447960777c5e76cbd19dddc0a0f77d827
soso-15315 <sonishimura15@gmail.com>
2023-01-19 13:50:09 +0900

    feat: abc130 d

--- abc130/src/bin/d.rs
+++ abc130/src/bin/d.rs
@@ -1,3 +1,49 @@
+use std::cmp::Ordering;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let mut ans = 0;
+    let mut s = vec![0; n + 1];
+
+    for i in 0..n {
+        s[i + 1] = s[i] + a[i];
+    }
+
+    for i in 0..n {
+        let j = s.lower_bound(&(s[i] + k));
+        ans += n + 1 - j;
+    }
+
+    println!("{}", ans);
+}
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
 }

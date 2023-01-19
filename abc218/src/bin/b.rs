c9a52b57c2a083efe877f1f3606f3e751e39eff7
soso-15315 <sonishimura15@gmail.com>
2023-01-19 20:51:19 +0900

    feat: abc218 b

--- abc218/src/bin/b.rs
+++ abc218/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        p: [usize; 26],
+    };
+
+    for i in 0..p.len() {
+        let v = p[i];
+        let n = b'a' + v as u8 - 1;
+
+        print!("{}", n as char)
+    }
 }

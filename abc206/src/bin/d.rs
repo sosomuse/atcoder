739a2375cd962f2f71f44415ae1ecf618c472e3d
soso-15315 <sonishimura15@gmail.com>
2023-01-22 15:47:45 +0900

    feat: abc206 d

--- abc206/src/bin/d.rs
+++ abc206/src/bin/d.rs
@@ -1,3 +1,77 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut ans = 0;
+    let max = *a.iter().max().unwrap();
+    let mid = a.len() / 2;
+    let las = a.len() - 1;
+    let mut uf = UnionFind::new(max);
+
+    for i in 0..mid {
+        let (x, y) = (a[i], a[las - i]);
+        if x == y {
+            continue;
+        }
+        if uf.issame(x - 1, y - 1) {
+            continue;
+        }
+        ans += 1;
+        uf.unite(x - 1, y - 1);
+    }
+
+    println!("{}", ans);
+}
+
+struct UnionFind {
+    par: Vec<usize>,
+    siz: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+    fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+        self.par[x] = self.root(self.par[x]);
+        self.par[x]
+    }
+
+    fn issame(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    fn unite(&mut self, mut parent: usize, mut child: usize) -> bool {
+        parent = self.root(parent);
+        child = self.root(child);
+
+        if parent == child {
+            return false;
+        }
+
+        if self.siz[parent] < self.siz[child] {
+            std::mem::swap(&mut parent, &mut child);
+        }
+
+        self.par[child] = parent;
+        self.siz[parent] += self.siz[child];
+        true
+    }
+
+    #[allow(dead_code)]
+    fn size(&mut self, x: usize) -> usize {
+        let root = self.root(x);
+        self.siz[root]
+    }
 }

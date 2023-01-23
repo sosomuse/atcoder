ae07406ee5730d5563655b9ea5dc70ef2b9b473d
soso-15315 <sonishimura15@gmail.com>
2023-01-23 22:50:01 +0900

    feat: abc154 b

--- arc154/src/bin/b.rs
+++ arc154/src/bin/b.rs
@@ -1,3 +1,35 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut s: Chars,
+        mut t: Chars,
+    };
+
+    if s == t {
+        println!("0");
+        return;
+    }
+
+    let mut s_c = s.clone();
+    let mut t_c = t.clone();
+    s_c.sort();
+    t_c.sort();
+
+    if s_c != t_c {
+        println!("-1");
+        return;
+    }
+
+    t.reverse();
+    let mut ans = n;
+
+    for i in 0..n {
+        if s[ans - 1] == t[i] {
+            ans -= 1;
+        }
+    }
+
+    println!("{}", ans);
 }

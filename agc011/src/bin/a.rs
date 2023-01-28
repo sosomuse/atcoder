60aa3ef3611506159972a6c92f0f66a819fd1df0
soso-15315 <sonishimura15@gmail.com>
2023-01-28 15:47:31 +0900

    feat: agc001

--- agc011/src/bin/a.rs
+++ agc011/src/bin/a.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        c: usize,
+        k: usize,
+        mut t: [usize; n],
+    };
+
+    t.sort();
+
+    let mut ans: usize = 0;
+    let mut p = 0;
+    let mut count = 0;
+
+    for i in 0..n {
+        let v = t[i];
+
+        if v > p || count >= c {
+            ans += 1;
+            p = v + k;
+            count = 1;
+
+            continue;
+        }
+
+        count += 1;
+    }
+
+    println!("{}", ans);
 }

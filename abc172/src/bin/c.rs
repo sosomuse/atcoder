eafac59957cff1571dd81d8797735c5d5ebf17ef
soso-15315 <sonishimura15@gmail.com>
2023-01-29 20:41:54 +0900

    feat: abc172 c

--- abc172/src/bin/c.rs
+++ abc172/src/bin/c.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; m],
+    };
+
+    let mut at = vec![0; n + 1];
+    let mut bt = vec![0; m + 1];
+
+    for i in 0..n {
+        at[i + 1] = at[i] + a[i];
+    }
+    for i in 0..m {
+        bt[i + 1] = bt[i] + b[i];
+    }
+
+    let mut ans = 0;
+    let mut ok = m;
+
+    for i in 0..=n {
+        if at[i] > k {
+            break;
+        }
+        while bt[ok] > k - at[i] {
+            ok -= 1;
+        }
+        ans = ans.max(i + ok);
+    }
+
+    println!("{}", ans);
 }

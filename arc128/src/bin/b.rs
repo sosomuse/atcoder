aec1b0deede5cc4c851d00c48fe943db6aef42b2
soso-15315 <sonishimura15@gmail.com>
2023-02-02 23:08:52 +0900

    feat: arc128 b

--- arc128/src/bin/b.rs
+++ arc128/src/bin/b.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+use std::cmp::min;
+
+const MAX: isize = 1000000000;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+    }
+
+    for _ in 0..t {
+        input! {
+            r: isize,
+            g: isize,
+            b: isize,
+        };
+
+        let mut ans = MAX;
+
+        if (r - g) % 3 == 0 {
+            ans = min(ans, r.max(g));
+        };
+
+        if (r - b) % 3 == 0 {
+            ans = min(ans, r.max(b));
+        };
+
+        if (g - b) % 3 == 0 {
+            ans = min(ans, g.max(b));
+        };
+
+        if ans == MAX {
+            ans = -1;
+        };
+
+        println!("{}", ans);
+    }
 }

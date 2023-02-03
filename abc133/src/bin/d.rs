173c88f70f887029dd0f43ffa406849e3f49eac7
soso-15315 <sonishimura15@gmail.com>
2023-02-03 22:26:46 +0900

    feat: abc133 d

--- abc133/src/bin/d.rs
+++ abc133/src/bin/d.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [isize; n],
+    };
+
+    let mut s = vec![0; n];
+
+    for i in 0..n {
+        if i % 2 == 0 {
+            s[0] += a[i];
+        } else {
+            s[0] -= a[i];
+        }
+    }
+
+    for i in 1..n {
+        s[i] = 2 * a[i - 1] - s[i - 1];
+    }
+
+    for i in 0..n {
+        print!("{} ", s[i]);
+    }
 }

5bad78c0e1d40e1e46448517f1827dfae139a0c7
soso-15315 <sonishimura15@gmail.com>
2023-02-04 20:53:21 +0900

    feat: abc288

--- tenka1-2019/src/bin/c.rs
+++ tenka1-2019/src/bin/c.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+
+    let mut white_count = 0;
+
+    for i in 0..n {
+        if s[i] == '.' {
+            white_count += 1;
+        }
+    }
+
+    let mut ans = white_count;
+    let mut count = 0;
+
+    for i in 1..=n {
+        if s[i - 1] == '.' {
+            count += 1;
+        }
+
+        ans = ans.min(i + white_count - count * 2)
+    }
+
+    println!("{}", ans);
 }

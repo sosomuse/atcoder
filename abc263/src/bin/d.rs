3722763956f0908cd9874bc8b27408e8199ae863
So Nishimura <s.nishimura@gemcook.com>
2023-02-06 20:39:42 +0900

    feat: abc263 d

--- abc263/src/bin/d.rs
+++ abc263/src/bin/d.rs
@@ -3,11 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        r: usize,
-        l: usize,
-        a: [usize; n],
+        r: isize,
+        l: isize,
+        a: [isize; n],
     };
 
-    // 0,1,2
-    let mut dp = vec![vec![]];
+    let mut s = vec![0; n];
+
+    for i in 0..n {
+        if i == 0 {
+            s[i] = a[i]
+        } else {
+            s[i] = s[i - 1] + a[i]
+        }
+    }
+
+    dbg!(s);
 }

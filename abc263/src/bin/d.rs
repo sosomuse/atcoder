612733d03ecf45d38b412f8fe5f7ad3b28147a65
soso-15315 <sonishimura15@gmail.com>
2023-02-06 23:24:02 +0900

    feat: abc263

--- abc263/src/bin/d.rs
+++ abc263/src/bin/d.rs
@@ -1,22 +1,26 @@
 use proconio::input;
+use std::cmp::{max, min};
 
 fn main() {
     input! {
         n: usize,
-        r: isize,
         l: isize,
+        r: isize,
         a: [isize; n],
-    };
-
-    let mut s = vec![0; n];
-
-    for i in 0..n {
-        if i == 0 {
-            s[i] = a[i]
-        } else {
-            s[i] = s[i - 1] + a[i]
-        }
     }
 
-    dbg!(s);
+    let mut ans = r * n as isize;
+    let mut suma = 0;
+    let mut sumb = 0;
+    let mut maxb = 0;
+
+    for i in 0..n {
+        suma += a[i];
+        sumb += a[i] - l;
+        maxb = max(maxb, sumb);
+        let now = suma + r * (n as isize - i as isize - 1) - maxb;
+        ans = min(ans, now);
+    }
+
+    println!("{}", ans);
 }

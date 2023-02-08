56d71fe350302b542520f62d065e8e087acc973f
soso-15315 <sonishimura15@gmail.com>
2023-02-08 23:57:54 +0900

    feat: abc085 d

--- abc085/src/bin/d.rs
+++ abc085/src/bin/d.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use std::cmp::Reverse;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n : usize,
+        mut h : usize,
+        mut ab : [(usize, usize); n]
+    }
+
+    ab.sort_by_key(|&(_, b)| Reverse(b));
+    let max = ab.iter().map(|&(a, _)| a).max().unwrap();
+
+    let mut ans = std::usize::MAX;
+    for (i, (_, b)) in ab.into_iter().enumerate() {
+        h = h.saturating_sub(b);
+        ans = ans.min(i + 1 + (h + max - 1) / max);
+    }
+
+    println!("{}", ans);
 }

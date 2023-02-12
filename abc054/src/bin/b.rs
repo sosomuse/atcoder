69195b37b1da7997c7aebc242e4bd1c0c04a5d7d
soso-15315 <sonishimura15@gmail.com>
2023-02-12 22:39:32 +0900

    feat: abc054

--- abc054/src/bin/b.rs
+++ abc054/src/bin/b.rs
@@ -1,3 +1,36 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [Chars; n],
+        b: [Chars; m],
+    };
+
+    for i in 0..n - m + 1 {
+        for j in 0..n - m + 1 {
+            let mut ok = true;
+
+            for k in 0..m {
+                for l in 0..m {
+                    if a[i + k][j + l] != b[k][l] {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if !ok {
+                    break;
+                }
+            }
+
+            if ok {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }

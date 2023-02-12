9594d690c1366d3ad04806bf90da9c8658a3d29d
soso-15315 <sonishimura15@gmail.com>
2023-02-12 23:01:49 +0900

    feat: abc211

--- abc211/src/bin/c.rs
+++ abc211/src/bin/c.rs
@@ -1,3 +1,45 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut dp = vec![0; 8];
+
+    for i in 0..s.len() {
+        match s[i] {
+            'c' => {
+                dp[0] += 1;
+            }
+            'h' => {
+                dp[1] += dp[0];
+            }
+            'o' => {
+                dp[2] += dp[1];
+            }
+            'k' => {
+                dp[3] += dp[2];
+            }
+            'u' => {
+                dp[4] += dp[3];
+            }
+            'd' => {
+                dp[5] += dp[4];
+            }
+            'a' => {
+                dp[6] += dp[5];
+            }
+            'i' => {
+                dp[7] += dp[6];
+            }
+            _ => continue,
+        }
+
+        for j in 0..8 {
+            dp[j] %= 1_000_000_007;
+        }
+    }
+
+    println!("{}", dp[7]);
 }

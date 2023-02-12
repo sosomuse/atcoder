a58b6ced3f0122a80c056110ef03bdbd15c072c7
soso-15315 <sonishimura15@gmail.com>
2023-02-12 22:41:54 +0900

    feat: abc054

--- abc054/src/bin/b.rs
+++ abc054/src/bin/b.rs
@@ -10,22 +10,13 @@ fn main() {
 
     for i in 0..n - m + 1 {
         for j in 0..n - m + 1 {
-            let mut ok = true;
-
-            for k in 0..m {
+            'a: for k in 0..m {
                 for l in 0..m {
                     if a[i + k][j + l] != b[k][l] {
-                        ok = false;
-                        break;
+                        break 'a;
                     }
                 }
 
-                if !ok {
-                    break;
-                }
-            }
-
-            if ok {
                 println!("Yes");
                 return;
             }

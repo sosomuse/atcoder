33374c933b793eb89b65e17ae1d26970aba883a5
soso-15315 <sonishimura15@gmail.com>
2023-02-15 13:30:12 +0900

    feat: typical 034

--- typical90/src/bin/028.rs
+++ typical90/src/bin/028.rs
@@ -1,3 +1,41 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        lrxy: [(usize, usize, usize, usize); n],
+    };
+
+    // 二次元累積和（imos法）
+    let mut s = vec![vec![0isize; 1010]; 1010];
+
+    for (lx, ly, rx, ry) in lrxy {
+        s[lx][ly] += 1;
+        s[lx][ry] -= 1;
+        s[rx][ly] -= 1;
+        s[rx][ry] += 1;
+    }
+
+    for i in 0..1001 {
+        for j in 0..1001 {
+            s[i][j + 1] += s[i][j];
+        }
+    }
+
+    for i in 0..1001 {
+        for j in 0..1001 {
+            s[i + 1][j] += s[i][j];
+        }
+    }
+
+    let mut ans = vec![0; n + 1];
+    for i in 0..1001 {
+        for j in 0..1001 {
+            ans[s[i][j] as usize] += 1;
+        }
+    }
+
+    for i in 1..=n {
+        println!("{}", ans[i]);
+    }
 }
--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -1,3 +1,46 @@
+use std::collections::{HashMap, HashSet};
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let mut set = HashSet::new();
+    let mut counts = HashMap::new();
+
+    let mut ans = 0;
+    let mut l = 0;
+    let mut r = 0;
+
+    for _ in 0..n {
+        while r < n && set.len() <= k {
+            if set.len() == k && !set.contains(&a[r]) {
+                break;
+            }
+
+            *counts.entry(a[r]).or_insert(0) += 1;
+            set.insert(a[r]);
+            r += 1;
+        }
+
+        if set.len() <= k {
+            ans = ans.max(r - l);
+        }
+
+        let current = set.len();
+
+        while l < n && set.len() == current {
+            *counts.entry(a[l]).or_insert(0) -= 1;
+            if counts[&a[l]] == 0 {
+                set.remove(&a[l]);
+            }
+            l += 1;
+        }
+    }
+
+    println!("{}", ans);
 }

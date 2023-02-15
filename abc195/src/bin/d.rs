81368eb575ff688f7a1f0088dd668d744c8e20e4
soso-15315 <sonishimura15@gmail.com>
2023-02-15 21:37:19 +0900

    feat: abc195 d

--- abc195/src/bin/d.rs
+++ abc195/src/bin/d.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        q: usize,
+        mut wv: [(usize, usize); n],
+        x: [usize; m],
+    };
+
+    wv.sort_by_key(|&(_, v)| std::cmp::Reverse(v));
+
+    for _ in 0..q {
+        input! {
+            l: usize,
+            r: usize,
+        };
+
+        let mut x = x.clone();
+        x.splice(l - 1..r, vec![]);
+
+        x.sort();
+
+        let mut ans = 0;
+        for (w, v) in wv.iter() {
+            if let Some(i) = x.iter().position(|&x| x >= *w) {
+                ans += v;
+                x.remove(i);
+            }
+        }
+
+        println!("{}", ans);
+    }
 }

6d0a4202e94fd0af274a92111721fafedcdede04
soso-15315 <sonishimura15@gmail.com>
2023-02-17 21:47:34 +0900

    feat: abc052

--- abc052/src/bin/c.rs
+++ abc052/src/bin/c.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans: usize = 1;
+    let mut primes = vec![0; n + 1];
+    for i in 2..=n {
+        let mut j = i;
+        for k in 2..=n {
+            while j % k == 0 {
+                primes[k] += 1;
+                j /= k;
+            }
+        }
+    }
+
+    for i in 2..=n {
+        ans *= primes[i] + 1;
+        ans %= 1_000_000_007;
+    }
+
+    println!("{}", ans);
 }

fa342cb04ce4a2ea927aa05ac2cf6e528d78dca0
soso-15315 <sonishimura15@gmail.com>
2023-02-18 10:05:08 +0900

    feat: abc084 c

--- abc084/src/bin/c.rs
+++ abc084/src/bin/c.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [(usize, usize, usize); n-1],
+    }
+
+    for i in 0..n - 1 {
+        let mut t = 0;
+
+        for j in i..n - 1 {
+            let (c, s, f) = a[j];
+            if t < s {
+                t = s;
+            } else if t % f == 0 {
+                // do nothing
+            } else {
+                t += f - (t % f);
+            }
+            t += c;
+        }
+
+        println!("{}", t);
+    }
+    println!("0");
 }

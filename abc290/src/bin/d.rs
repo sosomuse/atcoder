b186401cece236ee2a8f6d2e58824ac33e075117
soso-15315 <sonishimura15@gmail.com>
2023-02-19 23:39:27 +0900

    feat: abc290 d

--- abc290/src/bin/d.rs
+++ abc290/src/bin/d.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+    }
+
+    for _ in 0..t {
+        input! {
+            n: usize,
+            d: usize,
+            k: usize,
+        }
+
+        println!("{}", solve(n, d, k));
+    }
+}
+
+fn solve(n: usize, d: usize, k: usize) -> usize {
+    let x = d * (k - 1) % n;
+    let y = (k - 1) / (n / gcd(n, d));
+
+    x + y
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }

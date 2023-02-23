bb03e3a8fff6275188923a72866b63e7583b6fd7
soso-15315 <sonishimura15@gmail.com>
2023-02-23 22:01:10 +0900

    feat: abc160 d

--- abc160/src/bin/d.rs
+++ abc160/src/bin/d.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x: isize,
+        y: isize,
+    };
+
+    let mut ans = vec![0; n - 1];
+    for i in 1..=n {
+        for j in i + 1..=n {
+            let d = std::cmp::min(
+                j - i,
+                ((x - i as isize).abs() + 1 + (y - j as isize).abs()) as usize,
+            );
+            ans[d - 1] += 1;
+        }
+    }
+
+    for i in 0..n - 1 {
+        println!("{}", ans[i]);
+    }
 }

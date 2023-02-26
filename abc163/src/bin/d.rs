b33b79ed73ac221a8ec46d95c8246e02abd313a0
soso-15315 <sonishimura15@gmail.com>
2023-02-26 23:01:16 +0900

    feat: abc163

--- abc163/src/bin/d.rs
+++ abc163/src/bin/d.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+    };
+
+    let mut ans = 0;
+
+    for i in k..=n + 1 {
+        let min = i * (i - 1) / 2;
+        let max = i * (2 * n - i + 1) / 2;
+        ans += max - min + 1;
+        ans %= 1_000_000_007;
+    }
+
+    println!("{}", ans);
 }

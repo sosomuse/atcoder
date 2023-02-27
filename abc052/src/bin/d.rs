2e703dd80c82683a0f25f1029e4516ee12f902a1
soso-15315 <sonishimura15@gmail.com>
2023-02-27 23:23:25 +0900

    feat: abc052 d

--- abc052/src/bin/d.rs
+++ abc052/src/bin/d.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        x: [usize; n],
+    };
+
+    let mut ans = 0;
+    let mut current = x[0];
+
+    for i in 1..n {
+        let dist = x[i] - current;
+
+        ans += (a * dist).min(b);
+        current = x[i];
+    }
+
+    println!("{}", ans);
 }

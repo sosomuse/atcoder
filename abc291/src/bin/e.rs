8620095f76ea7ad85efcabd0351eb336e595ff8a
soso-15315 <sonishimura15@gmail.com>
2023-02-28 19:30:14 +0900

    feat abc291 e

--- abc291/src/bin/e.rs
+++ abc291/src/bin/e.rs
@@ -1,3 +1,49 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        xy: [(Usize1, Usize1); m],
+    };
+
+    // 隣接リスト
+    let mut graph = vec![vec![]; n];
+    // 入次数
+    let mut deg = vec![0; n];
+    for (x, y) in xy {
+        graph[y].push(x);
+        deg[x] += 1;
+    }
+
+    let mut queue = std::collections::VecDeque::new();
+    for i in 0..n {
+        if deg[i] == 0 {
+            queue.push_back(i);
+        }
+    }
+
+    let mut na = n;
+    let mut a = vec![na; n];
+    while let Some(v) = queue.pop_front() {
+        if !queue.is_empty() {
+            println!("No");
+            return;
+        }
+
+        a[v] = na;
+        na -= 1;
+
+        for &u in &graph[v] {
+            deg[u] -= 1;
+            if deg[u] == 0 {
+                queue.push_back(u);
+            }
+        }
+    }
+
+    println!("Yes");
+    for i in 0..n {
+        print!("{} ", a[i]);
+    }
 }

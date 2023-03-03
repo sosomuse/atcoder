6ab52e7ce4414cb38ba9f37b02841d6628c52501
So Nishimura <s.nishimura@gemcook.com>
2023-03-03 23:32:13 +0900

    feat: abc107 c

--- abc107/src/bin/c.rs
+++ abc107/src/bin/c.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        x: [isize; n],
+    }
+    let mut ans = 10000000000usize;
+    for i in 0..=n - k {
+        let l = x[i];
+        let r = x[i + k - 1];
+
+        ans = ans.min((l.abs() + (l - r).abs()) as usize);
+        ans = ans.min((r.abs() + (l - r).abs()) as usize);
+    }
+    println!("{}", ans);
 }

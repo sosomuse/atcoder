d7ea96c99db13f4f12cd87869aa1e749b0962521
soso-15315 <sonishimura15@gmail.com>
2023-03-04 20:23:20 +0900

    feat: abc292

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -6,7 +6,27 @@ fn main() {
         pa: [(usize, usize); n],
     };
 
-    let dp = vec![vec![0; n + 1]; n + 1];
+    // dp[l][r] := l番目からr番目までの区間が残っている場合の得点
+    let mut dp = vec![vec![0usize; n + 1]; n + 1];
 
-    dbg!(dp, pa);
+    for l in 1..=n {
+        for s in 1..l {
+            let (p, a) = pa[s - 1];
+
+            if s < p && l < p {
+                dp[l][n] += a;
+            }
+        }
+
+        for r in (l..n).rev() {
+            let (p, a) = pa[r];
+            dp[l][r] = dp[l][r + 1];
+
+            if l < p && r < p {
+                dp[l][r] += a;
+            }
+        }
+    }
+
+    dbg!(dp);
 }

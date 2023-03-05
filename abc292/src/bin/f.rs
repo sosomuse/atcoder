08800937b16bd7932706b665e344f14688790ed7
soso-15315 <sonishimura15@gmail.com>
2023-03-05 23:18:12 +0900

    feat: abc292 f

--- abc292/src/bin/f.rs
+++ abc292/src/bin/f.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+use std::f64::consts::PI;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut a: f64,
+        mut b: f64,
+    }
+
+    if a > b {
+        std::mem::swap(&mut a, &mut b);
+    }
+
+    let mut ok = 0f64;
+    let mut ng = PI / 6.0;
+
+    while ng - ok > 1e-12 {
+        let mid = (ok + ng) / 2.0;
+        let x = a / mid.cos();
+        if x * (PI / 6.0 - mid).cos() <= b {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", a / ok.cos());
 }

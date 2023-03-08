1de941e453ec389554ba1a7225ddef3d64136c8c
soso-15315 <sonishimura15@gmail.com>
2023-03-08 23:33:56 +0900

    feat: abc174 e

--- abc174/src/bin/e.rs
+++ abc174/src/bin/e.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let mut low = 1;
+    let mut high = *a.iter().max().unwrap();
+
+    while low != high {
+        let mid = (low + high) / 2;
+        let mut count = 0;
+
+        for i in 0..n {
+            if a[i] > mid {
+                count += a[i] / mid;
+            }
+        }
+
+        if count <= k {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    println!("{}", low);
 }

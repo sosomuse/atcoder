7cbf1172bdac527c7ec1e71b3a182272d0204806
soso-15315 <sonishimura15@gmail.com>
2023-03-11 17:56:28 +0900

    feat: abc126 e

--- abc126/src/bin/e.rs
+++ abc126/src/bin/e.rs
@@ -1,1 +1,72 @@
-fn main() {}
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        xyz: [(Usize1, Usize1, usize); m],
+    };
+
+    let mut uf = UnionFind::new(n);
+
+    for (x, y, _) in xyz {
+        uf.unite(x, y);
+    }
+
+    let mut set = std::collections::HashSet::new();
+
+    for i in 0..n {
+        set.insert(uf.root(i));
+    }
+
+    println!("{}", set.len());
+}
+
+struct UnionFind {
+    par: Vec<usize>,
+    siz: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+    fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+
+        self.par[x] = self.root(self.par[x]);
+        self.par[x]
+    }
+
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.root(x);
+        let y = self.root(y);
+
+        if x == y {
+            return;
+        }
+
+        if self.siz[x] < self.siz[y] {
+            self.par[x] = y;
+            self.siz[y] += self.siz[x];
+        } else {
+            self.par[y] = x;
+            self.siz[x] += self.siz[y];
+        }
+    }
+
+    fn size(&mut self, x: usize) -> usize {
+        let x = self.root(x);
+        self.siz[x]
+    }
+}

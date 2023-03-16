62a788d3ea46c4824ad98c2662e730c0c0c72c59
soso-15315 <sonishimura15@gmail.com>
2023-03-16 20:31:17 +0900

    feat: abc062 c

--- abc062/src/bin/c.rs
+++ abc062/src/bin/c.rs
@@ -1,3 +1,33 @@
+use std::cmp::min;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+    };
+
+    if h % 3 == 0 || w % 3 == 0 {
+        println!("0");
+        return;
+    }
+
+    fn sub(a: usize, b: usize) -> usize {
+        if a % 2 == 0 {
+            return a / 2;
+        }
+
+        let d = b / 3 + (b + 2) / 3;
+
+        if d >= a {
+            return a;
+        }
+
+        return (a - d) / 2 + d;
+    }
+
+    let ans = min(sub(h, w), sub(w, h));
+
+    println!("{}", ans);
 }

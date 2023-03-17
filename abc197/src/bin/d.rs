87f98e5c79c73149212ebd339fb5a5eb7de09dcd
soso-15315 <sonishimura15@gmail.com>
2023-03-17 21:20:21 +0900

    feat: abc197 d

--- abc197/src/bin/d.rs
+++ abc197/src/bin/d.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use std::f64::consts::PI;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x0: f64,
+        y0: f64,
+        xhalf: f64,
+        yhalf: f64,
+    }
+
+    let (xc, yc) = ((xhalf + x0) / 2.0, (yhalf + y0) / 2.0);
+    let rad = 2.0 * PI / (n as f64);
+    let x = (x0 - xc) * rad.cos() - (y0 - yc) * rad.sin() + xc;
+    let y = (x0 - xc) * rad.sin() + (y0 - yc) * rad.cos() + yc;
+
+    println!("{} {}", x, y);
 }

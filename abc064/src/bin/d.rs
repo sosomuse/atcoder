335984c08ada0ebf80c7f7920ab43ac2f904126d
soso-15315 <sonishimura15@gmail.com>
2023-03-19 23:21:25 +0900

    feat: abc064

--- abc064/src/bin/d.rs
+++ abc064/src/bin/d.rs
@@ -1,3 +1,33 @@
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        s: Chars,
+    };
+
+    let mut queue = VecDeque::new();
+    let mut count = 0usize;
+
+    for c in s {
+        queue.push_back(c);
+
+        if c == '(' {
+            count += 1;
+        } else if c == ')' {
+            if count == 0 {
+                queue.push_front('(');
+            } else {
+                count -= 1;
+            }
+        }
+    }
+
+    for _ in 0..count {
+        queue.push_back(')');
+    }
+
+    println!("{}", queue.iter().collect::<String>());
 }

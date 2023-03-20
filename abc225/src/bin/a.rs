c94c02dd771fd65eeaf650b5888c9bd2c941927e
soso-15315 <sonishimura15@gmail.com>
2023-03-20 22:39:30 +0900

    feat: abc225 a

--- abc225/src/bin/a.rs
+++ abc225/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut ans = 3;
+    if s[0] == s[1] && s[1] == s[2] {
+        ans = 1;
+    } else if s[0] != s[1] && s[1] != s[2] && s[0] != s[2] {
+        ans = 6;
+    }
+
+    println!("{}", ans);
 }

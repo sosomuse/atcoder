e120122c340111f38a0655ffb715526990baef61
soso-15315 <sonishimura15@gmail.com>
2023-03-21 23:32:22 +0900

    feat: abc225 b

--- abc225/src/bin/b.rs
+++ abc225/src/bin/b.rs
@@ -1,3 +1,27 @@
+use std::collections::{HashMap, HashSet};
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        ab: [(usize, usize); n - 1],
+    };
+
+    let mut trees = HashMap::new();
+    for (a, b) in ab {
+        let tree = trees.entry(a).or_insert(HashSet::new());
+        tree.insert(b);
+
+        let tree = trees.entry(b).or_insert(HashSet::new());
+        tree.insert(a);
+    }
+
+    let max_len = trees.values().map(|tree| tree.len()).max().unwrap();
+
+    if max_len == n - 1 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

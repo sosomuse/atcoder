eaa3e25780eaab7fc40e510a6e8c8c806c0997ed
So Nishimura <s.nishimura@gemcook.com>
2023-03-23 23:31:01 +0900

    feat: abc294 b

--- abc294/src/bin/b.rs
+++ abc294/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[u8; w]; h],
+    };
+
+    for i in 0..h {
+        for j in 0..w {
+            let v = a[i][j];
+            if v == 0 {
+                print!(".")
+            } else {
+                print!("{}", (b'A' + v - 1) as char)
+            }
+        }
+
+        println!()
+    }
 }

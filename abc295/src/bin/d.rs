937f55481fcac1db0ea8beaa713efe2c29d5db5c
soso-15315 <sonishimura15@gmail.com>
2023-03-26 22:34:13 +0900

    feat: abc295 d

--- abc295/src/bin/d.rs
+++ abc295/src/bin/d.rs
@@ -1,3 +1,25 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut mp = std::collections::HashMap::new();
+    let mut cnt = vec![0usize; 10];
+
+    mp.insert(cnt.clone(), 1);
+
+    for &nx in &s {
+        cnt[nx as usize - '0' as usize] += 1;
+        cnt[nx as usize - '0' as usize] %= 2;
+        *mp.entry(cnt.clone()).or_insert(0) += 1;
+    }
+
+    let mut res = 0usize;
+    for (_, &x) in &mp {
+        res += x * (x - 1) / 2;
+    }
+
+    println!("{}", res);
 }

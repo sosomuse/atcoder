27d66d70209b78dfc9cdbbcccd5ec22fb94fded6
soso-15315 <sonishimura15@gmail.com>
2023-03-29 20:44:42 +0900

    feat: abc294 e

--- abc294/src/bin/e.rs
+++ abc294/src/bin/e.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        l: usize,
+        n1: usize,
+        n2: usize,
+        ul1: [(usize, usize); n1],
+        ul2: [(usize, usize); n2],
+    };
+
+    let mut ult1 = (0, 0, 0);
+    let mut ult2 = (0, 0, 0);
+    let mut ans = 0;
+
+    while ult1.1 < l || ult2.1 < l {
+        if ult1.1 >= ult2.1 {
+            let diff = ult1.1 - ult2.1;
+            let (u, l) = ul2[ult2.2];
+            ult2 = (u, ult2.1 + l, ult2.2 + 1);
+
+            if ult2.0 == ult1.0 {
+                ans += diff.min(l);
+            }
+        } else {
+            let diff = ult2.1 - ult1.1;
+            let (u, l) = ul1[ult1.2];
+            ult1 = (u, ult1.1 + l, ult1.2 + 1);
+
+            if ult2.0 == ult1.0 {
+                ans += diff.min(l);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }

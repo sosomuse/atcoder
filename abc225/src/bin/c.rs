bb62e23a9a97e79fd78a9e3e4478910affd6a562
soso-15315 <sonishimura15@gmail.com>
2023-03-30 23:38:26 +0900

    feat: abc225 c

--- abc225/src/bin/c.rs
+++ abc225/src/bin/c.rs
@@ -1,3 +1,37 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        b: [[Usize1; m]; n],
+    };
+
+    for i in 0..n {
+        let mut p = b[i][0];
+
+        for j in 1..m {
+            if p + 1 != b[i][j] || p % 7 + 1 != b[i][j] % 7 {
+                println!("No");
+                return;
+            }
+
+            p = b[i][j];
+        }
+    }
+
+    for i in 0..m {
+        let mut p = b[0][i];
+
+        for j in 1..n {
+            if p + 7 != b[j][i] {
+                println!("No");
+                return;
+            }
+
+            p = b[j][i];
+        }
+    }
+
+    println!("Yes");
 }

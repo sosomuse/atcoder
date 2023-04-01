477295f4b4411c0d32c59ac9bd96fbd3c0f95113
soso-15315 <sonishimura15@gmail.com>
2023-04-01 15:11:34 +0900

    feat: abc223

--- abc223/src/bin/d.rs
+++ abc223/src/bin/d.rs
@@ -1,3 +1,68 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    };
+
+    let mut uf = UnionFind::new(n + 1);
+    for (a, b) in ab {
+        if uf.is_same(a, b) {
+            println!("-1");
+            return;
+        }
+
+        uf.unite(a, b);
+    }
+}
+
+struct UnionFind {
+    par: Vec<usize>,
+    siz: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+    fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+
+        self.par[x] = self.root(self.par[x]);
+        self.par[x]
+    }
+
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.root(x);
+        let y = self.root(y);
+
+        if x == y {
+            return;
+        }
+
+        if self.siz[x] < self.siz[y] {
+            self.par[x] = y;
+            self.siz[y] += self.siz[x];
+        } else {
+            self.par[y] = x;
+            self.siz[x] += self.siz[y];
+        }
+    }
+
+    fn size(&mut self, x: usize) -> usize {
+        let x = self.root(x);
+        self.siz[x]
+    }
 }

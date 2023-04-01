b31909b0be1ba268ef7ddf56bbcadd8f5fe1a7aa
soso-15315 <sonishimura15@gmail.com>
2023-04-01 14:57:54 +0900

    feat: abc178 e

--- abc178/src/bin/e.rs
+++ abc178/src/bin/e.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        points: [(isize, isize); n],
+    }
+
+    let (mut min_sum, mut max_sum, mut min_diff, mut max_diff) =
+        (1isize << 60, -(1isize << 60), 1isize << 60, -(1isize << 60));
+
+    for (x, y) in points {
+        let sum = x + y;
+        let diff = x - y;
+        min_sum = min_sum.min(sum);
+        max_sum = max_sum.max(sum);
+        min_diff = min_diff.min(diff);
+        max_diff = max_diff.max(diff);
+    }
+
+    let ans = (max_sum - min_sum).max(max_diff - min_diff);
+    println!("{}", ans);
 }

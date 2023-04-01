85a34980ae5b015017aa811f4212dd8a1fe72583
soso-15315 <sonishimura15@gmail.com>
2023-04-01 16:07:57 +0900

    feat: abc043

--- typical90/src/bin/043.rs
+++ typical90/src/bin/043.rs
@@ -1,95 +1,79 @@
+use proconio::input;
+use proconio::marker::Usize1;
+use std::cmp::min;
 use std::collections::VecDeque;
 
-use proconio::{
-    input,
-    marker::{Chars, Usize1},
-};
+const INF: i64 = 1_012_345_678;
+const DX: [i64; 4] = [1, 0, -1, 0];
+const DY: [i64; 4] = [0, 1, 0, -1];
+
+#[derive(Clone, Copy)]
+struct State {
+    x: usize,
+    y: usize,
+    dir: usize,
+}
 
 fn main() {
     input! {
         h: usize,
         w: usize,
-        (r1, c1): (Usize1, Usize1),
-        (r2, c2): (Usize1, Usize1),
-        s: [Chars; h],
-    };
+        mut sx: Usize1,
+        mut sy: Usize1,
+        mut gx: Usize1,
+        mut gy: Usize1,
+    }
 
-    let graph = create_maze_graph(h, w, s, '#');
-    let dist = bfs(r1 * w + c1, &graph, w);
-
-    println!("{}", dist[r2 * w + c2]);
-}
-
-fn create_maze_graph(h: usize, w: usize, s: Vec<Vec<char>>, wall: char) -> Vec<Vec<usize>> {
-    let mut graph = vec![vec![]; h * w];
-
+    let mut s = vec![String::new(); h];
     for i in 0..h {
-        let v = &s[i];
-        for j in 0..w {
-            let ch = v[j];
-            if ch == wall {
-                continue;
-            }
-
-            let p = w * i + j;
-            // upper
-            if i > 0 && s[i - 1][j] != wall {
-                graph[p].push(w * (i - 1) + j);
-            }
-            // lower
-            if i != (h - 1) && s[i + 1][j] != wall {
-                graph[p].push(w * (i + 1) + j);
-            }
-            // left
-            if j > 0 && s[i][j - 1] != wall {
-                graph[p].push(p - 1);
-            }
-            // right
-            if j != (w - 1) && s[i][j + 1] != wall {
-                graph[p].push(p + 1);
-            }
+        input! {
+            c: String,
         }
+
+        s[i] = c;
     }
 
-    graph
-}
+    let mut dist = vec![vec![vec![INF; 4]; w]; h];
+    let mut deq = VecDeque::new();
 
-fn bfs(v: usize, graph: &Vec<Vec<usize>>, w: usize) -> Vec<isize> {
-    let mut dist: Vec<isize> = vec![-1; graph.len()];
-    let mut queue: VecDeque<(usize, char)> = std::collections::VecDeque::new();
-    queue.push_front((v, '.'));
-    dist[v] = 0;
+    for i in 0..4 {
+        dist[sx][sy][i] = 0;
+        deq.push_back(State {
+            x: sx,
+            y: sy,
+            dir: i,
+        });
+    }
 
-    while !queue.is_empty() {
-        let (pos, direction) = *queue.front().unwrap();
-        queue.pop_front().unwrap();
+    while let Some(u) = deq.pop_front() {
+        for i in 0..4 {
+            let tx = (u.x as i64 + DX[i]) as usize;
+            let ty = (u.y as i64 + DY[i]) as usize;
+            let cost = dist[u.x][u.y][u.dir] + if u.dir != i { 1 } else { 0 };
 
-        for i in 0..graph[pos].len() {
-            let nex = graph[pos][i];
-            let nex_dir = {
-                if pos >= w && nex == pos - w {
-                    'u'
-                } else if nex == pos + w {
-                    'd'
-                } else if pos > 0 && nex == pos - 1 {
-                    'l'
+            if tx < h && ty < w && s[tx].chars().nth(ty).unwrap() == '.' && dist[tx][ty][i] > cost {
+                dist[tx][ty][i] = cost;
+                if u.dir != i {
+                    deq.push_back(State {
+                        x: tx,
+                        y: ty,
+                        dir: i,
+                    });
                 } else {
-                    'r'
+                    deq.push_front(State {
+                        x: tx,
+                        y: ty,
+                        dir: i,
+                    });
                 }
-            };
-
-            let nex_count = if nex_dir == direction || direction == '.' {
-                dist[pos]
-            } else {
-                dist[pos] + 1
-            };
-
-            if dist[nex] == -1 || dist[nex] > nex_count {
-                dist[nex] = nex_count;
-                queue.push_back((nex, nex_dir));
             }
         }
     }
 
-    dist
+    let mut ans = INF;
+    for i in 0..4 {
+        ans = min(ans, dist[gx][gy][i]);
+    }
+
+    println!("{}", ans);
 }

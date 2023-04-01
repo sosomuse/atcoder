09c5ae9229c1701cbba29a6badd5996ef0ec1019
soso-15315 <sonishimura15@gmail.com>
2023-04-01 19:18:23 +0900

    feat: abc079

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -1,3 +1,40 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        mut a: [[isize; w]; h],
+        b: [[isize; w]; h],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..h - 1 {
+        for j in 0..w - 1 {
+            if a[i][j] > b[i][j] {
+                let diff = a[i][j] - b[i][j];
+                ans += diff;
+
+                for (di, dj) in [(0, 0), (0, 1), (1, 0), (1, 1)].iter() {
+                    a[i + di][j + dj] -= diff;
+                }
+            } else {
+                let diff = b[i][j] - a[i][j];
+                ans += diff;
+
+                for (di, dj) in [(0, 0), (0, 1), (1, 0), (1, 1)].iter() {
+                    a[i + di][j + dj] += diff;
+                }
+            }
+        }
+    }
+
+    if a == b {
+        println!("Yes");
+        println!("{}", ans);
+    } else {
+        println!("No");
+    }
 }

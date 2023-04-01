26630a548a9b011121f8f79d08f20010a126bd79
soso-15315 <sonishimura15@gmail.com>
2023-04-01 14:38:52 +0900

    feat: abc114 c

--- abc114/src/bin/c.rs
+++ abc114/src/bin/c.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = 0;
+    for i in 0..3 {
+        let s = ["3", "5", "7"][i].to_string();
+        solve(n, s, &mut ans);
+    }
+
+    println!("{}", ans);
+}
+
+fn solve(n: usize, s: String, ans: &mut usize) {
+    let x: usize = s.parse().unwrap();
+    if x > n {
+        return;
+    }
+
+    if s.contains("3") && s.contains("5") && s.contains("7") {
+        *ans += 1;
+    }
+
+    for i in 0..3 {
+        let s = s.clone() + ["3", "5", "7"][i];
+        solve(n, s, ans);
+    }
 }

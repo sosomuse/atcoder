ff2535411dc8dce073260e7718e41ef5ea4708c1
soso-15315 <sonishimura15@gmail.com>
2023-04-01 14:44:58 +0900

    feat: agc005

--- agc005/src/bin/a.rs
+++ agc005/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: Chars,
+    };
+
+    let mut ans = x.len();
+    let mut s_count = 0;
+
+    for i in 0..x.len() {
+        if x[i] == 'S' {
+            s_count += 1;
+        } else {
+            if s_count > 0 {
+                s_count -= 1;
+                ans -= 2;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }

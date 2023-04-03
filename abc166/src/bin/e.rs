13b5a6f3e12c384014378ea4b6031e399ce85ffb
soso-15315 <sonishimura15@gmail.com>
2023-04-03 21:40:10 +0900

    feat: abc166 e

--- abc166/src/bin/e.rs
+++ abc166/src/bin/e.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut cnt = vec![0usize; n];
+    let mut ans = 0;
+
+    for i in 0..n {
+        if i >= a[i] {
+            ans += cnt[i - a[i]];
+        }
+        if i + a[i] < n {
+            cnt[i + a[i]] += 1;
+        }
+    }
+
+    println!("{}", ans);
 }

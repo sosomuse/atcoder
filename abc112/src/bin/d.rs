7d9d148e44da65c0f0ae994c70484719569d1bf3
soso-15315 <sonishimura15@gmail.com>
2023-04-04 23:08:04 +0900

    feat: abc112 d

--- abc112/src/bin/d.rs
+++ abc112/src/bin/d.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+    };
+
+    let mut ans = 1;
+    let divs = divisors(m);
+
+    for d in divs {
+        if n <= m / d {
+            ans = d;
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn divisors(n: usize) -> Vec<usize> {
+    let mut lst: Vec<usize> = vec![];
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            lst.push(i);
+            if i != n / i {
+                lst.push(n / i);
+            }
+        }
+
+        i += 1;
+    }
+
+    lst
 }

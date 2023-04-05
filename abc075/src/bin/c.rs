6c037ad5f86f1fd410ef0421552d1bc7c3944b92
soso-15315 <sonishimura15@gmail.com>
2023-04-05 13:34:17 +0900

    feat: abc075 c

--- abc075/src/bin/c.rs
+++ abc075/src/bin/c.rs
@@ -1,3 +1,44 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..m {
+        let mut g = vec![vec![]; n];
+        for j in 0..m {
+            if i == j {
+                continue;
+            }
+
+            let (a, b) = ab[j];
+            g[a].push(b);
+            g[b].push(a);
+        }
+
+        let mut visited = vec![false; n];
+        dfs(0, &g, &mut visited);
+
+        if visited.iter().any(|&v| !v) {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn dfs(v: usize, g: &Vec<Vec<usize>>, visited: &mut Vec<bool>) {
+    visited[v] = true;
+
+    for &w in &g[v] {
+        if visited[w] {
+            continue;
+        }
+        dfs(w, g, visited);
+    }
 }

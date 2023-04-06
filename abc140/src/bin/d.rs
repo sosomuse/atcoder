a727656eea5b41ea34b35f873c74ebb4f19dd48a
soso-15315 <sonishimura15@gmail.com>
2023-04-06 21:40:55 +0900

    feat: abc140

--- abc140/src/bin/d.rs
+++ abc140/src/bin/d.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        s: Chars,
+    };
+
+    let mut ans = 0;
+    for i in 0..n {
+        if s[i] == 'L' && i > 0 && s[i - 1] == 'L' {
+            ans += 1;
+        }
+
+        if s[i] == 'R' && i < n - 1 && s[i + 1] == 'R' {
+            ans += 1;
+        }
+    }
+
+    ans += k * 2;
+    println!("{}", ans.min(n - 1));
+}

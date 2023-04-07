448444d10b4005b92bee506afb47832b60e13840
soso-15315 <sonishimura15@gmail.com>
2023-04-07 23:41:14 +0900

    feat: abc224 a

--- abc224/src/bin/a.rs
+++ abc224/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+
+    if s.ends_with("er") {
+        println!("er");
+    } else {
+        println!("ist");
+    }
 }

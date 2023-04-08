f4d6ef7b0ba68731228963398c7ff2cccc511bf6
soso-15315 <sonishimura15@gmail.com>
2023-04-08 10:53:28 +0900

    feat: typical90 042

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        k: usize,
+    };
+
+    if k % 9 != 0 {
+        println!("0");
+        return;
+    }
+
+    let mut dp = vec![0; k + 1];
+    dp[0] = 1;
+
+    for i in 1..=k {
+        let b = std::cmp::min(i, 9);
+        for j in 1..=b {
+            if i < j {
+                continue;
+            }
+
+            dp[i] += dp[i - j];
+            dp[i] %= 1_000_000_007;
+        }
+    }
+
+    println!("{}", dp[k]);
 }

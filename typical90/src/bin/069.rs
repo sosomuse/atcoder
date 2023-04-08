836c20637ef902197d90d3f515f3293605b5b60c
soso-15315 <sonishimura15@gmail.com>
2023-04-08 11:52:04 +0900

    feat: typical90 069

--- typical90/src/bin/069.rs
+++ typical90/src/bin/069.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+    };
+
+    if n == 1 {
+        println!("{}", k);
+        return;
+    }
+
+    let mut ans = mod_pow(k - 2, n - 2, MOD);
+    ans = ans * (k - 1) % MOD;
+    ans = ans * k % MOD;
+
+    println!("{}", ans);
+}
+
+fn mod_pow(a: usize, b: usize, m: usize) -> usize {
+    let mut r = 1;
+    let mut p = a;
+    for i in 0..64 {
+        if (b >> i) & 1 == 1 {
+            r = r * p % m;
+        }
+        p = p * p % m;
+    }
+    r
 }

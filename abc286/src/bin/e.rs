3c64a59a02ded2ee8416efe507797283c9f214fc
soso-15315 <sonishimura15@gmail.com>
2023-04-10 21:48:12 +0900

    feat: abc286 e

--- abc286/src/bin/e.rs
+++ abc286/src/bin/e.rs
@@ -1,95 +1,44 @@
-use std::collections::VecDeque;
-
 use proconio::{input, marker::Chars};
+use std::cmp::min;
 
+const INF: i64 = 1_000_000_000_000_000_000;
+
+// ワーシャルフロイド法
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        a: [i64; n],
         s: [Chars; n],
         q: usize,
         uv: [(usize, usize); q],
-    };
+    }
 
-    let mut graph = vec![vec![]; n + 1];
+    let mut graph = vec![vec![(INF, 0); n]; n];
 
     for i in 0..n {
-        let chars = &s[i];
-
         for j in 0..n {
-            if chars[j] == 'Y' {
-                graph[i + 1].push(j + 1);
+            if s[i][j] == 'Y' {
+                graph[i][j] = (1, -a[j]);
+            }
+        }
+    }
+
+    let add = |a: (i64, i64), b: (i64, i64)| (a.0 + b.0, a.1 + b.1);
+
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                graph[i][j] = min(graph[i][j], add(graph[i][k], graph[k][j]));
             }
         }
     }
 
     for (u, v) in uv {
-        let dist = bfs(u, &graph);
-
-        if dist[v] == -1 {
+        let (d, c) = graph[u - 1][v - 1];
+        if d == INF {
             println!("Impossible");
-            continue;
-        }
-
-        let ans = dist[v];
-        let worth = bfs_with_worth(v, dist, &a);
-
-        println!("{} {}", ans, worth);
-    }
-}
-
-fn bfs(v: usize, graph: &Vec<Vec<usize>>) -> Vec<isize> {
-    let mut dist: Vec<isize> = vec![-1; graph.len()];
-    let mut queue: VecDeque<usize> = std::collections::VecDeque::new();
-    queue.push_front(v);
-    dist[v] = 0;
-
-    while !queue.is_empty() {
-        let pos = *queue.front().unwrap();
-        queue.pop_front().unwrap();
-
-        for i in 0..graph[pos].len() {
-            let nex = graph[pos][i];
-            if dist[nex] == -1 {
-                dist[nex] = dist[pos] + 1;
-                queue.push_back(nex);
-            }
+        } else {
+            println!("{} {}", d, -c + a[u - 1]);
         }
     }
-
-    dist
-}
-
-fn bfs_with_worth(v: usize, dist: Vec<isize>, a: &Vec<usize>) -> usize {
-    let mut queue: VecDeque<(usize, usize)> = std::collections::VecDeque::new();
-    queue.push_front((v, a[v - 1]));
-    let mut routes = vec![vec![]; dist[v] as usize];
-    let mut ans = 0;
-
-    for i in 0..routes.len() {
-        for j in 0..dist.len() {
-            if dist[j] as usize == i {
-                routes[i].push(j)
-            }
-        }
-    }
-
-    while !queue.is_empty() {
-        let (pos, worth) = queue.pop_front().unwrap();
-
-        if dist[pos] == 0 {
-            ans = ans.max(worth);
-            continue;
-        }
-
-        let t = dist[pos] as usize - 1;
-
-        for i in 0..routes[t].len() {
-            let nex = routes[t][i];
-
-            queue.push_back((nex, a[nex - 1] + worth));
-        }
-    }
-
-    ans
 }

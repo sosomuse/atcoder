ebd5d6f3ea4386d5e524f3b7c7d33d2703bea8b2
soso-15315 <sonishimura15@gmail.com>
2023-04-11 20:33:34 +0900

    feat: abc234 e

--- abc234/src/bin/e.rs
+++ abc234/src/bin/e.rs
@@ -1,3 +1,36 @@
+use std::collections::BTreeSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: i64,
+    };
+
+    let set = solve();
+    let mut range = set.range(x..);
+
+    println!("{}", range.next().unwrap());
+}
+
+fn solve() -> BTreeSet<i64> {
+    let mut set = BTreeSet::new();
+
+    for i in 1..=9 {
+        for j in -9..=8 {
+            let mut s = String::new();
+            let mut d = i;
+
+            for _ in 0..18 {
+                s.push_str(&d.to_string());
+                set.insert(s.parse::<i64>().unwrap());
+                d += j;
+                if !(0 <= d && d <= 9) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return set;
 }

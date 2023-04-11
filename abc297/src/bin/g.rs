e28d3ddf33f3096658d3f533001cc3321127851f
soso-15315 <sonishimura15@gmail.com>
2023-04-11 20:15:56 +0900

    feat: abc297 g

--- abc297/src/bin/g.rs
+++ abc297/src/bin/g.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        l: usize,
+        r: usize,
+        mut a: [usize; n],
+    }
+
+    let sum = l + r;
+    let mut count = 0;
+
+    for i in 0..n {
+        if a[i] < l {
+            continue;
+        }
+
+        if a[i] > 0 {
+            count += a[i] / sum * 2;
+            a[i] %= sum;
+        }
+
+        if a[i] > r {
+            count += 2;
+        } else if a[i] <= r {
+            if a[i] < l {
+                continue;
+            }
+
+            count += 1;
+        }
+    }
+
+    if count % 2 == 1 {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }

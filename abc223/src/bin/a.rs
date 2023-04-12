9e5a36ecb3e08519493b14ca90670d2a69f48406
soso-15315 <sonishimura15@gmail.com>
2023-04-12 22:22:14 +0900

    feat: abc223 a

--- abc223/src/bin/a.rs
+++ abc223/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+    };
+
+    if x < 100 {
+        println!("No");
+        return;
+    }
+
+    println!("{}", if x % 100 == 0 { "Yes" } else { "No" })
 }

7a66a03da98294280d962dd53ae19b3f8858e141
soso-15315 <sonishimura15@gmail.com>
2023-04-16 23:04:20 +0900

    feat: abc102 c

--- abc102/src/bin/c.rs
+++ abc102/src/bin/c.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [isize; n],
+    };
+
+    for i in 0..n {
+        a[i] -= (i + 1) as isize;
+    }
+
+    a.sort();
+
+    let b = {
+        if n % 2 == 1 {
+            a[n / 2]
+        } else {
+            (a[n / 2 - 1] + a[n / 2]) / 2
+        }
+    };
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += (a[i] - b).abs();
+    }
+
+    println!("{}", ans);
 }

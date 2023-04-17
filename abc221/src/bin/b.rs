5732c2f1dace33cca38721c75aae1b57e8d23a83
soso-15315 <sonishimura15@gmail.com>
2023-04-17 22:25:49 +0900

    feat: abc221 b

--- abc221/src/bin/b.rs
+++ abc221/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+        t: Chars,
+    };
+
+    if s == t {
+        println!("Yes");
+        return;
+    }
+
+    for i in 0..s.len() - 1 {
+        let mut sc = s.clone();
+        sc.swap(i, i + 1);
+
+        if sc == t {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }

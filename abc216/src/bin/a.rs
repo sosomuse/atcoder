ad6a72dd61d95ae2084f2d53c141f1d60fec74a4
soso-15315 <sonishimura15@gmail.com>
2023-04-23 23:28:06 +0900

    feat: abc261 a

--- abc216/src/bin/a.rs
+++ abc216/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+
+    let a = s.split('.').collect::<Vec<_>>();
+    let x = a[0];
+    let y: usize = a[1].parse().unwrap();
+
+    if y <= 2 {
+        println!("{}-", x);
+    } else if y <= 6 {
+        println!("{}", x);
+    } else {
+        println!("{}+", x);
+    }
 }

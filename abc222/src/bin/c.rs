7a54915ccb3ff2ac8365145fd6ce880386e11b92
soso-15315 <sonishimura15@gmail.com>
2023-04-25 23:07:19 +0900

    feat: abc222 c

--- abc222/src/bin/c.rs
+++ abc222/src/bin/c.rs
@@ -1,3 +1,41 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [Chars; 2 * n],
+    };
+
+    let mut ans = vec![];
+
+    for i in 0..2 * n {
+        ans.push((0, i));
+    }
+
+    for i in 0..m {
+        for j in 0..n {
+            let x = ans[2 * j].1;
+            let y = ans[2 * j + 1].1;
+
+            let xv = a[x][i];
+            let yv = a[y][i];
+
+            if xv == yv {
+                continue;
+            }
+
+            if (xv == 'G' && yv == 'C') || (xv == 'C' && yv == 'P') || (xv == 'P' && yv == 'G') {
+                ans[2 * j].0 -= 1;
+            } else {
+                ans[2 * j + 1].0 -= 1;
+            }
+        }
+
+        ans.sort();
+    }
+
+    for i in 0..2 * n {
+        println!("{}", ans[i].1 + 1);
+    }
 }

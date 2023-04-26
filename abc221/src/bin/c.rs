c33f67ac95147debe672fc8dba60d6681d1b1fa7
soso-15315 <sonishimura15@gmail.com>
2023-04-26 23:24:56 +0900

    feat: abc221 c

--- abc221/src/bin/c.rs
+++ abc221/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::{input, marker::Chars};
+
+use itertools::Itertools;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: Chars,
+    };
+
+    let len = n.len();
+    let perm = n.into_iter().permutations(len).collect::<Vec<_>>();
+
+    let mut ans = 0;
+
+    for p in perm {
+        for v in 1..p.len() {
+            let x = p[..v].iter().join("").parse::<i64>().unwrap();
+            let y = p[v..].iter().join("").parse::<i64>().unwrap();
+
+            ans = ans.max(x * y);
+        }
+    }
+
+    println!("{}", ans);
 }

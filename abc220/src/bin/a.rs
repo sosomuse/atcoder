fd395b3accd62721f5712be139e552800bab2b03
soso-15315 <sonishimura15@gmail.com>
2023-04-27 18:53:15 +0900

    feat: abc220 a

--- abc220/src/bin/a.rs
+++ abc220/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+
+    for x in a..=b {
+        if x % c == 0 {
+            println!("{}", x);
+            return;
+        }
+    }
+
+    println!("-1");
 }

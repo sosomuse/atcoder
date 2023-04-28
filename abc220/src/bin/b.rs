1f0a05cb15c66121991a53f703d02293c94686ab
soso-15315 <sonishimura15@gmail.com>
2023-04-28 23:32:49 +0900

    feat: abc220 b

--- abc220/src/bin/b.rs
+++ abc220/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        k: u32,
+        a: String,
+        b: String,
+    };
+
+    let x = u64::from_str_radix(&a, k).unwrap();
+    let y = u64::from_str_radix(&b, k).unwrap();
+
+    println!("{}", x * y);
 }

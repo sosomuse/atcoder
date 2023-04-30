3a6d4c3f24d8b444526702d509d4b35d58abc16e
soso-15315 <sonishimura15@gmail.com>
2023-04-30 23:39:53 +0900

    feat: abc220 c

--- abc220/src/bin/c.rs
+++ abc220/src/bin/c.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        x: usize,
+    };
+
+    let mut sum = a.iter().sum::<usize>();
+    let mut ans = x / sum * n;
+    let mut res = x % sum;
+
+    for i in 0..n {
+        ans += 1;
+
+        if res < a[i] {
+            println!("{}", ans);
+            return;
+        }
+
+        res -= a[i];
+    }
 }

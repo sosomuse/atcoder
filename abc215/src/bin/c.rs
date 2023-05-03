910939026a2f931de17165b6bce89a49b8b936bd
soso-15315 <sonishimura15@gmail.com>
2023-05-03 23:27:49 +0900

    feat: abc215 c

--- abc215/src/bin/c.rs
+++ abc215/src/bin/c.rs
@@ -1,3 +1,25 @@
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+        k: usize,
+    };
+
+    let perm = (0..s.len()).permutations(s.len()).collect_vec();
+    let mut x = vec![];
+
+    for p in perm {
+        let mut y = vec![];
+        for i in 0..s.len() {
+            y.push(s[p[i]]);
+        }
+        x.push(y);
+    }
+
+    x.sort();
+    x.dedup();
+
+    println!("{}", x[k - 1].iter().join(""));
 }

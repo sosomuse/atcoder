f83aba8fa3d8c79cf6d1b427b5daa3a6a7c83bcd
soso-15315 <sonishimura15@gmail.com>
2023-05-04 15:22:36 +0900

    feat: typical90 070

--- typical90/src/bin/070.rs
+++ typical90/src/bin/070.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        xy: [(isize, isize); n],
+    };
+
+    let mut x = xy.iter().map(|&(x, _)| x).collect::<Vec<_>>();
+    let mut y = xy.iter().map(|&(_, y)| y).collect::<Vec<_>>();
+
+    x.sort();
+    y.sort();
+
+    let ans_x = x[x.len() / 2];
+    let ans_y = y[y.len() / 2];
+
+    let mut ans = 0;
+    for (x, y) in xy {
+        ans += (x - ans_x).abs() + (y - ans_y).abs();
+    }
+
+    println!("{}", ans);
 }

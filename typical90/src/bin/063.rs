37e92e220ca11ec10bb100041abc260ddd9cc16f
soso-15315 <sonishimura15@gmail.com>
2023-05-06 22:09:39 +0900

    feat: typical90 063

--- typical90/src/bin/063.rs
+++ typical90/src/bin/063.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        p: [[usize; w]; h],
+    };
+
+    let mut ans = 0;
+
+    for bit in 0..1 << h {
+        let mut vec = vec![];
+        for i in 0..h {
+            if bit & 1 << i != 0 {
+                vec.push(i);
+            }
+        }
+        let mut counts = HashMap::new();
+
+        if vec.len() == 0 {
+            continue;
+        }
+
+        for j in 0..w {
+            let is_same = vec.iter().all(|&i| p[i][j] == p[vec[0]][j]);
+            if is_same {
+                *counts.entry(p[vec[0]][j]).or_insert(0) += vec.len();
+            }
+        }
+
+        let max_count = counts.values().max().unwrap_or(&0);
+        ans = std::cmp::max(ans, *max_count);
+    }
+
+    println!("{}", ans);
 }

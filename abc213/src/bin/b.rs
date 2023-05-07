05a24f0c7fd44b606afb3fd11500ad39b49484b8
soso-15315 <sonishimura15@gmail.com>
2023-05-07 22:25:44 +0900

    feat: abc213 b

--- abc213/src/bin/b.rs
+++ abc213/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut s = a
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| (i + 1, x))
+        .collect::<Vec<_>>();
+
+    s.sort_by_key(|&(_, x)| x);
+
+    println!("{}", s[n - 2].0)
 }

8c4c23689c2c43e58ac7759cdd836755301844e1
soso-15315 <sonishimura15@gmail.com>
2023-05-10 13:30:31 +0900

    feat: typical90 006

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,3 +1,45 @@
+use std::collections::BTreeSet;
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        s: Chars,
+    };
+
+    let s_with_index = s
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (c as u8, i))
+        .collect::<Vec<_>>();
+
+    let mut set = BTreeSet::new();
+
+    for i in 0..n - k {
+        set.insert(s_with_index[i]);
+    }
+
+    let mut ans = vec![];
+    let mut last_i = 0;
+
+    for i in 0..k {
+        set.insert(s_with_index[i + n - k]);
+
+        let &(c, j) = set.iter().next().unwrap();
+
+        for k in last_i..j {
+            for u in 'a' as u8..='z' as u8 {
+                set.remove(&(u, k));
+            }
+        }
+
+        set.remove(&(c, j));
+
+        ans.push(c as char);
+        last_i = j + 1;
+    }
+
+    println!("{}", ans.into_iter().collect::<String>());
 }

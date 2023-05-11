6900e4596889b339aee7cc9832f1eaaf066163a8
soso-15315 <sonishimura15@gmail.com>
2023-05-11 23:05:06 +0900

    feat: abc211 b

--- abc211/src/bin/b.rs
+++ abc211/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: [String; 4],
+    };
+
+    let mut set = std::collections::HashSet::new();
+    for i in 0..4 {
+        set.insert(s[i].clone());
+    }
+
+    if set.len() == 4 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

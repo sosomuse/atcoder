fdd1e4351e67a0c53f3f8a03e80b517d7e2949c1
soso-15315 <sonishimura15@gmail.com>
2023-05-12 21:20:49 +0900

    feat: abc206 a

--- abc206/src/bin/a.rs
+++ abc206/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: f64,
+    };
+
+    let x = (n * 1.08).floor() as i32;
+
+    if x > 206 {
+        println!(":(");
+    } else if x == 206 {
+        println!("so-so");
+    } else {
+        println!("Yay!");
+    }
 }

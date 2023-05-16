fc6dc919d1b966c721394e1a8382721ab0ce185b
soso-15315 <sonishimura15@gmail.com>
2023-05-16 19:56:41 +0900

    feat: abc080

--- abc080/src/bin/c.rs
+++ abc080/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        f: [[usize; 10]; n],
+        p: [[isize; 11]; n],
+    };
+
+    let mut ans = isize::min_value();
+
+    for bit in 1..1 << 10 {
+        let opens = (0..10).map(|i| (bit >> i) & 1).collect::<Vec<_>>();
+        let mut tmp = 0;
+
+        for i in 0..n {
+            let same_count = (0..10).filter(|j| opens[*j] == 1 && f[i][*j] == 1).count();
+            tmp += p[i][same_count];
+        }
+
+        ans = ans.max(tmp);
+    }
+
+    println!("{}", ans);
 }

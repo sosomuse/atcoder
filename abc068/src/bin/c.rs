a1912efe9a0ce683f35cde8649a4dfec5681361b
soso-15315 <sonishimura15@gmail.com>
2023-05-18 19:42:50 +0900

    feat: abc068

---
+++ abc068/src/bin/c.rs
@@ -0,0 +1,46 @@
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
+    };
+
+    let mut graph = vec![vec![]; n];
+    for &(a, b) in &ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let dist = bfs(0, &graph);
+    if dist[n - 1] == 2 {
+        println!("POSSIBLE");
+    } else {
+        println!("IMPOSSIBLE");
+    }
+}
+
+fn bfs(v: usize, graph: &Vec<Vec<usize>>) -> Vec<isize> {
+    let mut dist: Vec<isize> = vec![-1; graph.len()];
+    let mut queue: VecDeque<usize> = std::collections::VecDeque::new();
+    queue.push_front(v);
+    dist[v] = 0;
+
+    while !queue.is_empty() {
+        let pos = *queue.front().unwrap();
+        queue.pop_front().unwrap();
+
+        for i in 0..graph[pos].len() {
+            let nex = graph[pos][i];
+            if dist[nex] == -1 {
+                dist[nex] = dist[pos] + 1;
+                queue.push_back(nex);
+            }
+        }
+    }
+
+    dist
+}
--- abc142/src/bin/e.rs
+++ abc142/src/bin/e.rs
@@ -1,3 +1,61 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+    };
+
+    let mut a = vec![];
+    let mut b = vec![];
+    let mut c = vec![vec![]; m];
+
+    for i in 0..m {
+        input! {
+            av: usize,
+            bv: usize,
+        };
+
+        a.push(av);
+        b.push(bv);
+
+        input! {
+            cv: [usize; bv],
+        }
+
+        c[i] = cv;
+    }
+
+    let mut ans = usize::max_value();
+
+    for bit in 0..1 << n {
+        let mut keys = vec![];
+
+        for i in 0..n {
+            if bit >> i & 1 != 0 {
+                keys.push(i);
+            }
+        }
+
+        let mut s = HashSet::new();
+        for k in keys.iter() {
+            for v in c[*k].iter() {
+                s.insert(*v);
+            }
+        }
+
+        if s.len() == n {
+            let sum = keys.iter().fold(0, |acc, x| acc + a[*x]);
+
+            ans = std::cmp::min(ans, sum);
+        }
+    }
+
+    if ans == usize::max_value() {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }

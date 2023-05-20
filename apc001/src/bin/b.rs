f7aed7eb05a31d1c31088ce51bda4d3e19dea323
soso-15315 <sonishimura15@gmail.com>
2023-05-20 19:15:06 +0900

    feat: apc001 b

--- apc001/src/bin/b.rs
+++ apc001/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [isize; n],
+        b: [isize; n],
+    };
+
+    let mut count = 0isize;
+
+    for i in 0..n {
+        if a[i] < b[i] {
+            count += (b[i] - a[i]) / 2;
+        } else {
+            count -= a[i] - b[i];
+        }
+    }
+
+    if count >= 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

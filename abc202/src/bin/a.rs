113eb84fca8fe228829a58d1cb17496963194473
So Nishimura <s.nishimura@gemcook.com>
2023-05-22 23:40:36 +0900

    feat: abc202

--- abc202/src/bin/a.rs
+++ abc202/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        abc: [usize; 3],
+    };
+
+    let mut ans = 0;
+    for v in abc {
+        ans += 7 - v;
+    }
+
+    println!("{}", ans);
 }

8e1e6367d2545fb82b693034b9b2504473b402b0
soso-15315 <sonishimura15@gmail.com>
2023-05-23 23:32:46 +0900

    feat: arc023

--- arc023/src/bin/b.rs
+++ arc023/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        r: usize,
+        c: usize,
+        d: usize,
+        a: [[usize; c]; r],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..r {
+        for j in 0..c {
+            if i + j <= d && (i + j) % 2 == d % 2 {
+                ans = ans.max(a[i][j]);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }

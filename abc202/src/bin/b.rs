e58a1e9f5169051d0f4b68846583482ab401afde
soso-15315 <sonishimura15@gmail.com>
2023-05-23 23:17:51 +0900

    feat: abc202 b

--- abc202/src/bin/b.rs
+++ abc202/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    for i in (0..s.len()).rev() {
+        let v = s[i].to_digit(10).unwrap();
+        if v == 6 {
+            print!("9");
+        } else if v == 9 {
+            print!("6");
+        } else {
+            print!("{}", v);
+        }
+    }
 }

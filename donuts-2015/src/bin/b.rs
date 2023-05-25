ad4769f141d1108955415d710713c06ab6115090
soso-15315 <sonishimura15@gmail.com>
2023-05-25 13:22:44 +0900

    feat: donuts-2015 b

--- donuts-2015/src/bin/b.rs
+++ donuts-2015/src/bin/b.rs
@@ -1,3 +1,52 @@
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+    };
+
+    let mut bci = vec![];
+
+    for _ in 0..m {
+        input! {
+            b: usize,
+            c: usize,
+            i: [Usize1; c],
+        };
+
+        bci.push((b, c, i));
+    }
+
+    let perm = (0..n).combinations(9).collect_vec();
+
+    let mut ans = 0;
+
+    for p in perm {
+        let mut tmp = 0;
+
+        for i in 0..p.len() {
+            tmp += a[p[i]];
+        }
+
+        for (b, c, i) in &bci {
+            let mut cnt = 0;
+
+            for j in 0..*c {
+                if p.contains(&i[j]) {
+                    cnt += 1;
+                }
+            }
+
+            if cnt >= 3 {
+                tmp += b;
+            }
+        }
+
+        ans = ans.max(tmp);
+    }
+
+    println!("{}", ans);
 }

150af54956bd9a3ed8a69c9b59e5370383c44b58
soso-15315 <sonishimura15@gmail.com>
2023-05-25 13:37:10 +0900

    feat: abc153 e

--- abc153/src/bin/e.rs
+++ abc153/src/bin/e.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        n: usize,
+        ab: [(usize, usize); n],
+    };
+
+    let mut dp = vec![vec![100000000000; h + 1]; n + 1];
+    dp[0][h] = 0;
+
+    for i in 0..n {
+        for j in (0..=h).rev() {
+            let (a, b) = ab[i];
+            dp[i + 1][j] = std::cmp::min(dp[i + 1][j], dp[i][j]);
+            if j >= a {
+                dp[i][j - a] = std::cmp::min(dp[i][j - a], dp[i][j] + b);
+            } else {
+                dp[i][0] = std::cmp::min(dp[i][0], dp[i][j] + b);
+            }
+        }
+    }
+
+    println!("{}", dp[n][0]);
 }

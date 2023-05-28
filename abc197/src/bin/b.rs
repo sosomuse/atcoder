ca84e783103ea67e8eb8671253ef3b298c1d9d6c
soso-15315 <sonishimura15@gmail.com>
2023-05-28 22:19:43 +0900

    feat: abc197 b

--- abc197/src/bin/b.rs
+++ abc197/src/bin/b.rs
@@ -1,3 +1,50 @@
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        x: Usize1,
+        y: Usize1,
+        s: [Chars; h],
+    };
+
+    let mut ans = 0;
+
+    for i in (0..=x).rev() {
+        if s[i][y] == '.' {
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+
+    for i in x + 1..h {
+        if s[i][y] == '.' {
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+
+    for j in (0..=y).rev() {
+        if s[x][j] == '.' {
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+
+    for j in y + 1..w {
+        if s[x][j] == '.' {
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", ans - 1);
 }

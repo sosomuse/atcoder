9f75c12895f85093ff6093cd59e1d6f6658d3294
soso-15315 <sonishimura15@gmail.com>
2023-05-31 13:38:38 +0900

    feat: agc034

--- agc034/src/bin/a.rs
+++ agc034/src/bin/a.rs
@@ -5,7 +5,7 @@ use proconio::{
 
 fn main() {
     input! {
-        n: usize,
+        _: usize,
         a: Usize1,
         b: Usize1,
         c: Usize1,
@@ -13,62 +13,33 @@ fn main() {
         s: Chars,
     };
 
-    let mut is_change = false;
-    let mut ok_count = 0;
-    let mut ng_count = 0;
-
     for i in a..c {
-        let v = s[i];
-        if v == '#' {
-            ng_count += 1;
-        } else {
-            ng_count = 0;
-        }
-
-        if ng_count == 2 {
+        if s[i] == '#' && s[i + 1] == '#' {
             println!("No");
             return;
         }
     }
-
-    ng_count = 0;
 
     for i in b..d {
-        let v = s[i];
-        if v == '#' {
-            ng_count += 1;
-        } else {
-            ng_count = 0;
-        }
-
-        if ng_count == 2 {
+        if s[i] == '#' && s[i + 1] == '#' {
             println!("No");
             return;
-        }
-    }
-
-    for i in 0..n {
-        let v = s[i];
-        if v == '#' {
-            ok_count = 0;
-        } else {
-            ok_count += 1;
-        }
-
-        if ok_count >= 3 {
-            if i <= d && i > b {
-                is_change = true;
-            }
         }
     }
 
     if c > d {
-        if is_change {
-            println!("Yes");
-        } else {
-            println!("No");
+        let mut is_change = false;
+        for i in b..=d {
+            if s[i - 1] == '.' && s[i] == '.' && s[i + 1] == '.' {
+                is_change = true;
+            }
         }
-    } else {
-        println!("Yes");
+
+        if !is_change {
+            println!("No");
+            return;
+        }
     }
+
+    println!("Yes");
 }

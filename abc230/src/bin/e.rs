2f528b8008ece79595b41b9504c40725f1cd094b
soso-15315 <sonishimura15@gmail.com>
2023-05-31 13:53:20 +0900

    feat: abc230 e

--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = 0;
+    let mut k = 0;
+
+    for i in 1..=n {
+        if i * i <= n {
+            k = i;
+        } else {
+            break;
+        }
+    }
+
+    for i in 1..=k {
+        ans += ((n / i) - (n / (i + 1))) * i;
+    }
+    for i in 1..=n / (k + 1) {
+        ans += n / i;
+    }
+
+    println!("{}", ans);
 }

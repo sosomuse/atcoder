05c142cfa20e09fd8981e428f309ae022795f5eb
soso-15315 <sonishimura15@gmail.com>
2023-06-01 19:28:46 +0900

    feat: abc193

--- abc193/src/bin/b.rs
+++ abc193/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        apx: [(usize, usize, usize); n],
+    };
+
+    let mut ans = std::usize::MAX;
+    for (a, p, x) in apx {
+        if x > a {
+            ans = ans.min(p);
+        }
+    }
+    println!(
+        "{}",
+        if ans == std::usize::MAX {
+            -1
+        } else {
+            ans as isize
+        }
+    )
 }

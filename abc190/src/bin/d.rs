55659bf719b62b789d8738c1608bd3c8b8f19ec3
soso-15315 <sonishimura15@gmail.com>
2023-06-03 20:56:30 +0900

    feat: abc190 d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -4,4 +4,40 @@ fn main() {
     input! {
         n: usize,
     };
+
+    let mut ans = 0;
+    let mut x = 1;
+
+    while x * x <= n {
+        let mut left = x;
+        let mut right = n.min(1_000_000_000);
+
+        while left < right {
+            let mid = (left + right) / 2;
+
+            if sum_of_numbers(x, mid) < n {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if sum_of_numbers(x, left) == n {
+            ans += 1;
+        }
+
+        x += 1;
+    }
+
+    if n != 1 {
+        ans += 1;
+    }
+
+    ans *= 2;
+
+    println!("{}", ans);
+}
+
+fn sum_of_numbers(start: usize, end: usize) -> usize {
+    (end * (end + 1) / 2) - (start * (start - 1) / 2)
 }

a8ec9945025b12c7f71847e0a647d5b332252d08
soso-15315 <sonishimura15@gmail.com>
2023-06-05 21:15:16 +0900

    feat: abc172

--- abc172/src/bin/b.rs
+++ abc172/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+        t: Chars,
+    };
+
+    let mut ans = 0;
+
+    for i in 0..s.len() {
+        if s[i] != t[i] {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }

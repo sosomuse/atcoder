74a29194617fb27727b25692341b04df8cf82c07
soso-15315 <sonishimura15@gmail.com>
2023-06-07 23:39:05 +0900

    feat: abc168

--- abc168/src/bin/a.rs
+++ abc168/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: Chars,
+    };
+
+    let last = n[n.len() - 1];
+    if last == '2' || last == '4' || last == '5' || last == '7' || last == '9' {
+        println!("hon");
+    } else if last == '0' || last == '1' || last == '6' || last == '8' {
+        println!("pon");
+    } else {
+        println!("bon");
+    }
 }

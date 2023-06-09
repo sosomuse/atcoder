6e233af3614871b2335d71926162dd7fc4b72da3
soso-15315 <sonishimura15@gmail.com>
2023-06-09 19:11:00 +0900

    feat: abc190 d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -2,42 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut n: usize,
     };
 
-    let mut ans = 0;
-    let mut x = 1;
-
-    while x * x <= n {
-        let mut left = x;
-        let mut right = n.min(1_000_000_000);
-
-        while left < right {
-            let mid = (left + right) / 2;
-
-            if sum_of_numbers(x, mid) < n {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        if sum_of_numbers(x, left) == n {
-            ans += 1;
-        }
-
-        x += 1;
+    while n % 2 == 0 {
+        n /= 2;
     }
+    let sq = (n as f64).sqrt() as usize;
+    let ans = (1..=sq).filter(|i| n % i == 0).count() * 2 - (sq * sq == n as usize) as usize;
 
-    if n != 1 {
-        ans += 1;
-    }
-
-    ans *= 2;
-
-    println!("{}", ans);
-}
-
-fn sum_of_numbers(start: usize, end: usize) -> usize {
-    (end * (end + 1) / 2) - (start * (start - 1) / 2)
+    println!("{}", ans * 2);
 }

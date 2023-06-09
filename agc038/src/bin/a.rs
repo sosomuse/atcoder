c0ac5fe63bcfddf3785157133fb13cc7965098ea
soso-15315 <sonishimura15@gmail.com>
2023-06-09 19:00:11 +0900

    feat: agc038 a

--- agc038/src/bin/a.rs
+++ agc038/src/bin/a.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: usize,
+        b: usize,
+    };
+
+    for i in 0..h {
+        for j in 0..w {
+            if i < b {
+                if j < a {
+                    print!("0");
+                } else {
+                    print!("1");
+                }
+            } else {
+                if j < a {
+                    print!("1");
+                } else {
+                    print!("0");
+                }
+            }
+        }
+
+        println!();
+    }
 }

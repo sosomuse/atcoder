0819b6099caeb07bac45a064bb8e3fc6021136f7
soso-15315 <sonishimura15@gmail.com>
2023-06-12 00:19:28 +0900

    feat: code2015

--- code-festival-2015-morning-easy/src/bin/c.rs
+++ code-festival-2015-morning-easy/src/bin/c.rs
@@ -1,3 +1,44 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        m: usize,
+        r: usize,
+        mut s: [usize; n-1],
+    };
+
+    s.sort();
+    s.reverse();
+
+    let take_sum = s.iter().take(k - 1).sum::<usize>();
+    let take_sum2 = s.iter().take(k).sum::<usize>();
+    let x = r * k;
+
+    if take_sum2 >= x {
+        println!("0");
+        return;
+    }
+
+    if take_sum + m < x {
+        println!("-1");
+        return;
+    }
+
+    let mut left = 0;
+    let mut right = m;
+
+    while left + 1 < right {
+        let mid = (left + right) / 2;
+        let sum = take_sum + mid;
+
+        if sum >= x {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+
+    println!("{}", right);
 }

f4ce5a675be6c24d5702a1e8bcc8db010b62e066
soso-15315 <sonishimura15@gmail.com>
2023-06-12 23:44:53 +0900

    feat: abc174

--- abc174/src/bin/b.rs
+++ abc174/src/bin/b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        d: f64,
+        xy: [(isize, isize); n],
+    };
+
+    let origin = (0, 0);
+    let mut ans = 0;
+
+    for (x, y) in xy {
+        if dist(origin, (x, y)) <= d {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn dist(x: (isize, isize), y: (isize, isize)) -> f64 {
+    let dx = x.0 - y.0;
+    let dy = x.1 - y.1;
+    ((dx * dx + dy * dy) as f64).sqrt()
 }

18dd9f0c5261faca0713144632e2068ec31558f7
soso-15315 <sonishimura15@gmail.com>
2023-06-15 22:37:53 +0900

    feat: abc178 c

--- abc178/src/bin/c.rs
+++ abc178/src/bin/c.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+    };
+
+    let mut ans = 1;
+    let mut tmp = 1;
+
+    for _ in 0..x {
+        ans *= 10;
+        ans %= MOD;
+    }
+    for _ in 0..x {
+        tmp *= 9;
+        tmp %= MOD;
+    }
+    tmp *= 2;
+    tmp %= MOD;
+
+    ans = (ans + MOD - tmp) % MOD;
+
+    tmp = 1;
+
+    for _ in 0..x {
+        tmp *= 8;
+        tmp %= MOD;
+    }
+
+    ans += tmp;
+    ans %= MOD;
+
+    println!("{}", ans);
 }

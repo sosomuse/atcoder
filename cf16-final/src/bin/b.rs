ad0b53daeff3500a6f63e361546ea0a89ecf18f9
soso-15315 <sonishimura15@gmail.com>
2023-06-16 22:12:22 +0900

    feat: cf16

--- cf16-final/src/bin/b.rs
+++ cf16-final/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut sum = 0;
+    let mut ans = 0;
+    let mut res = 0;
+
+    for i in 1..=n {
+        sum += i;
+
+        if sum >= n {
+            res = sum - n;
+            ans = i;
+            break;
+        }
+    }
+
+    for i in 1..=ans {
+        if i != res {
+            println!("{}", i);
+        }
+    }
 }

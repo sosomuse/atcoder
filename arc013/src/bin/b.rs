346cced7853b9f1ed452891c03ba80922a8af15a
soso-15315 <sonishimura15@gmail.com>
2023-06-18 15:46:42 +0900

    feat: arc013

--- arc013/src/bin/b.rs
+++ arc013/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        c: usize,
+        mut nml: [[usize; 3]; c],
+    };
+
+    for i in 0..c {
+        nml[i].sort();
+    }
+
+    let mut n = 0;
+    let mut m = 0;
+    let mut l = 0;
+
+    for i in 0..c {
+        n = n.max(nml[i][0]);
+        m = m.max(nml[i][1]);
+        l = l.max(nml[i][2]);
+    }
+
+    println!("{}", n * m * l);
 }

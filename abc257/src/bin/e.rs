c6bf1641d47949dba40c1155b1ff43308d3439ce
soso-15315 <sonishimura15@gmail.com>
2023-06-19 20:43:23 +0900

    feat: abc257 e

--- abc257/src/bin/e.rs
+++ abc257/src/bin/e.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut n: usize,
+        c: [usize; 9],
+    };
+
+    let mn = *c.iter().min().unwrap();
+    let length = n / mn;
+
+    let mut ans = vec![];
+
+    for i in 0..length {
+        for j in (1..=9).rev() {
+            if mn * (length - 1 - i) + c[j - 1] <= n {
+                n -= c[j - 1];
+                ans.push(j);
+                break;
+            }
+        }
+    }
+
+    println!("{}", ans.iter().map(|x| x.to_string()).collect::<String>());
 }
--- agc056/src/bin/a.rs
+++ agc056/src/bin/a.rs
@@ -1,3 +1,43 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = vec![vec![0; n]; n];
+    let mut first = 0;
+
+    for i in 0..n {
+        let vec = vec![0, 1, 2];
+
+        for v in vec {
+            if first + v < n {
+                ans[i][first + v] = 1;
+            } else {
+                ans[i][first + v - n] = 1;
+            }
+        }
+
+        first += 3;
+        if first >= n {
+            first -= n;
+        }
+    }
+
+    if n % 3 != 0 {
+        ans.swap(0, n / 3 - 1);
+        ans.swap(n - 1, n - n / 3);
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            if ans[i][j] == 1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }

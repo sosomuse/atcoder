2e56c80358824b6ba186c14bea808e27df8e9360
soso-15315 <sonishimura15@gmail.com>
2023-06-20 23:36:02 +0900

    feat: abc206 c

--- abc206/src/bin/c.rs
+++ abc206/src/bin/c.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut count = HashMap::new();
+
+    for i in 0..n {
+        *count.entry(a[i]).or_insert(0) += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        ans += n - i - count[&a[i]];
+        *count.entry(a[i]).or_insert(0) -= 1;
+    }
+
+    println!("{}", ans);
 }
--- tenka1-2018/src/bin/c.rs
+++ tenka1-2018/src/bin/c.rs
@@ -1,3 +1,47 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+    };
+
+    a.sort();
+    let mut deque = std::collections::VecDeque::new();
+
+    deque.push_back(a[0]);
+    let mut front_count = 1;
+    let mut back_count = 0;
+
+    while front_count + back_count < n {
+        if back_count < front_count {
+            deque.push_front(a[n - 1 - back_count]);
+            back_count += 1;
+
+            if front_count + back_count >= n {
+                break;
+            }
+
+            deque.push_back(a[n - 1 - back_count]);
+            back_count += 1;
+        } else {
+            deque.push_front(a[front_count]);
+            front_count += 1;
+
+            if front_count + back_count >= n {
+                break;
+            }
+
+            deque.push_back(a[front_count]);
+            front_count += 1;
+        }
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n - 1 {
+        ans += (deque[i] as i64 - deque[i + 1] as i64).abs();
+    }
+
+    println!("{}", ans);
 }

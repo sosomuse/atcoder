d42b64a788b7d8d7f206030696acbd23c2cacd3c
soso-15315 <sonishimura15@gmail.com>
2023-06-21 13:34:35 +0900

    feat: abc188

--- abc188/src/bin/a.rs
+++ abc188/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut x: usize,
+        mut y: usize,
+    };
+
+    if x > y {
+        std::mem::swap(&mut x, &mut y);
+    }
+
+    if x + 3 > y {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

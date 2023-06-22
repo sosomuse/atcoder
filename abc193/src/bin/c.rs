949cebb6b6816ea26fc8c799ca3f378ac834baf5
soso-15315 <sonishimura15@gmail.com>
2023-06-22 20:53:00 +0900

    feat: abc193 c

--- abc193/src/bin/c.rs
+++ abc193/src/bin/c.rs
@@ -1,3 +1,30 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = n;
+    let mut i = 2;
+    let mut visited = HashMap::new();
+
+    while i * i <= n {
+        if visited.contains_key(&i) {
+            i += 1;
+            continue;
+        }
+
+        let mut j = 2;
+        while i.pow(j) <= n {
+            visited.insert(i.pow(j), true);
+            ans -= 1;
+            j += 1;
+        }
+        i += 1;
+    }
+
+    println!("{}", ans);
 }

931d131683aa913b542b62d4c177f9cbfb893a15
soso-15315 <sonishimura15@gmail.com>
2023-06-23 20:51:45 +0900

    feat: abc195 c

--- abc195/src/bin/c.rs
+++ abc195/src/bin/c.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    };
+
+    let mut ans = 0;
+
+    if n >= 1000 {
+        ans += n - 999;
+    }
+    if n >= 1000000 {
+        ans += n - 999999;
+    }
+    if n >= 1000000000 {
+        ans += n - 999999999;
+    }
+    if n >= 1000000000000 {
+        ans += n - 999999999999;
+    }
+    if n >= 1000000000000000 {
+        ans += n - 999999999999999;
+    }
+
+    println!("{}", ans);
 }

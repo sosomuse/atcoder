5ed037db5f48d59387061a356f7e314dc6798b0b
soso-15315 <sonishimura15@gmail.com>
2023-06-25 23:04:08 +0900

    feat: abc

--- tenka1-2018/src/bin/c.rs
+++ tenka1-2018/src/bin/c.rs
@@ -10,30 +10,31 @@ fn main() {
     let mut deque = std::collections::VecDeque::new();
 
     deque.push_back(a[0]);
+    deque.push_back(a[n - 1]);
+
     let mut front_count = 1;
-    let mut back_count = 0;
+    let mut back_count = 1;
 
-    while front_count + back_count < n {
-        if back_count < front_count {
-            deque.push_front(a[n - 1 - back_count]);
-            back_count += 1;
-
-            if front_count + back_count >= n {
-                break;
+    while deque.len() < n {
+        let left = deque[0];
+        let right = deque[deque.len() - 1];
+        let current = {
+            if front_count <= back_count {
+                front_count += 1;
+                a[front_count - 1]
+            } else {
+                back_count += 1;
+                a[n - 1 - (back_count - 1)]
             }
+        };
 
-            deque.push_back(a[n - 1 - back_count]);
-            back_count += 1;
+        let l_abs = (left as i64 - current as i64).abs();
+        let r_abs = (right as i64 - current as i64).abs();
+
+        if l_abs > r_abs {
+            deque.push_front(current);
         } else {
-            deque.push_front(a[front_count]);
-            front_count += 1;
-
-            if front_count + back_count >= n {
-                break;
-            }
-
-            deque.push_back(a[front_count]);
-            front_count += 1;
+            deque.push_back(current);
         }
     }
 

888815e1c079b413b66a3a3d9b87c15ddd8b3a80
soso-15315 <sonishimura15@gmail.com>
2023-06-25 23:07:15 +0900

    feat: abc188 b

--- abc188/src/bin/b.rs
+++ abc188/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [isize; n],
+        b: [isize; n],
+    }
+
+    let dot_product: isize = a.iter().zip(b.iter()).map(|(x, y)| x * y).sum();
+
+    if dot_product == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

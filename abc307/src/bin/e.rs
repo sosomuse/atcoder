d128e550d4b9a66fabac4a7b81e6b90e260b7c24
soso-15315 <sonishimura15@gmail.com>
2023-06-26 19:32:52 +0900

    feat: abc307 e

--- abc307/src/bin/e.rs
+++ abc307/src/bin/e.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
+const MOD: usize = 998244353;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+    };
+
+    let mut dp = vec![vec![0; 2]; n + 1];
+    dp[1][1] = m;
+
+    for i in 1..n {
+        dp[i + 1][0] += dp[i][0] * (m - 2);
+        dp[i + 1][1] += dp[i][0];
+        dp[i + 1][0] += dp[i][1] * (m - 1);
+        dp[i + 1][0] %= MOD;
+        dp[i + 1][1] %= MOD;
+    }
+
+    println!("{}", dp[n][0]);
 }

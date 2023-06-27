ac6417a7744be1cafed85feb87d58b083416507c
soso-15315 <sonishimura15@gmail.com>
2023-06-27 20:30:09 +0900

    feat: abc202 c

--- abc202/src/bin/c.rs
+++ abc202/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [Usize1; n],
+    };
+
+    let mut bs = vec![0usize; n + 1];
+    for i in 0..n {
+        bs[b[c[i]]] += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        ans += bs[a[i]];
+    }
+
+    println!("{}", ans);
 }

d82a91d3477a68ec4e2e948e2bbf32163a42fc7b
soso-15315 <sonishimura15@gmail.com>
2023-06-28 23:24:36 +0900

    feat: abc190

--- abc190/src/bin/a.rs
+++ abc190/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+
+    if a > b {
+        println!("Takahashi");
+    } else if b > a {
+        println!("Aoki");
+    } else {
+        if c == 0 {
+            println!("Aoki");
+        } else {
+            println!("Takahashi");
+        }
+    }
 }

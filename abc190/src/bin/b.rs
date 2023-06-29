0015856881a218d1395dc5b3f0a629c127aabd32
soso-15315 <sonishimura15@gmail.com>
2023-06-29 22:35:38 +0900

    feat: abc190

--- abc190/src/bin/b.rs
+++ abc190/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: usize,
+        d: usize,
+        mut xy: [(usize, usize); n],
+    };
+
+    xy.retain(|&(x, y)| x < s && y > d);
+
+    if xy.is_empty() {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }

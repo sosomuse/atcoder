f2a39302d9c5de6f88ff75f6b2d4acab761b162e
soso-15315 <sonishimura15@gmail.com>
2023-06-30 19:27:43 +0900

    feat: abc190 c

--- abc190/src/bin/c.rs
+++ abc190/src/bin/c.rs
@@ -1,3 +1,33 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
+        k: usize,
+        cd: [(Usize1, Usize1); k],
+    };
+
+    let mut ans = 0;
+    for bit in 0..1 << k {
+        let mut exsits = vec![false; n];
+        let mut cnt = 0;
+
+        for i in 0..k {
+            if bit >> i & 1 == 0 {
+                exsits[cd[i].0] = true;
+            } else {
+                exsits[cd[i].1] = true;
+            }
+        }
+        for i in 0..m {
+            if exsits[ab[i].0] && exsits[ab[i].1] {
+                cnt += 1;
+            }
+        }
+        ans = ans.max(cnt);
+    }
+
+    println!("{}", ans);
 }

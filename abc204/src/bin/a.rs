3bce3efa0d8813045e8d98cebcceaa50c8f4d39e
soso-15315 <sonishimura15@gmail.com>
2023-07-04 19:41:42 +0900

    feat: abc204

--- abc204/src/bin/a.rs
+++ abc204/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+        y: usize,
+    };
+
+    let s = vec![0, 1, 2];
+
+    if x == y {
+        println!("{}", x);
+    } else {
+        for i in s {
+            if i != x && i != y {
+                println!("{}", i);
+            }
+        }
+    }
 }

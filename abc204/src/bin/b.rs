f075b0aa9c1963f29e19a81a0f4d51defc82e36a
soso-15315 <sonishimura15@gmail.com>
2023-07-05 23:24:41 +0900

    feat: abc204

--- abc204/src/bin/b.rs
+++ abc204/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        if a[i] > 10 {
+            ans += a[i] - 10;
+        }
+    }
+
+    println!("{}", ans);
 }

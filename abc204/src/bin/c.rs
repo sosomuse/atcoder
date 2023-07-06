3950c39956a088bff7c82e8b5d96d4f5ee1cf90b
soso-15315 <sonishimura15@gmail.com>
2023-07-06 18:54:18 +0900

    feat: abc204

--- abc204/src/bin/c.rs
+++ abc204/src/bin/c.rs
@@ -1,3 +1,37 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
+    };
+
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        let mut visited = vec![false; n];
+        let mut count = 0;
+        dfs(i, &edges, &mut visited, &mut count);
+        ans += count;
+    }
+
+    println!("{}", ans);
+}
+
+fn dfs(v: usize, edges: &Vec<Vec<usize>>, visited: &mut Vec<bool>, count: &mut usize) {
+    visited[v] = true;
+    *count += 1;
+
+    for &next_v in &edges[v] {
+        if visited[next_v] {
+            continue;
+        }
+        dfs(next_v, edges, visited, count);
+    }
 }

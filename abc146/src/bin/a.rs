b934055b721c55f3e81150f5daf0affacb37e293
soso-15315 <sonishimura15@gmail.com>
2023-07-09 23:33:25 +0900

    feat: abc146

--- abc146/src/bin/a.rs
+++ abc146/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+
+    let days = vec!["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let mut ans = 0;
+
+    for i in 0..days.len() {
+        if s == days[i] {
+            ans = 7 - i;
+        }
+    }
+
+    println!("{}", ans);
 }

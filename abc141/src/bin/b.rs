64fc0f008db75519d47898020bd3620b6687f1b7
soso-15315 <sonishimura15@gmail.com>
2023-07-10 23:40:13 +0900

    feat: abc141 b

--- abc141/src/bin/b.rs
+++ abc141/src/bin/b.rs
@@ -1,3 +1,30 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    for i in 0..s.len() {
+        let c = s[i];
+
+        if c == 'U' || c == 'D' {
+            continue;
+        }
+
+        if i % 2 == 0 {
+            if c == 'R' {
+                continue;
+            }
+        } else {
+            if c == 'L' {
+                continue;
+            }
+        }
+
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
 }

c4fe99b4729aed6254d4b4e0f1b96ac2e895eaae
soso-15315 <sonishimura15@gmail.com>
2023-07-11 23:36:46 +0900

    feat: abc146 b

--- abc146/src/bin/b.rs
+++ abc146/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u8,
+        mut s: Chars,
+    };
+
+    for i in 0..s.len() {
+        let mut c = s[i] as u8 + n;
+        if c > b'Z' {
+            c -= 26;
+        }
+        s[i] = c as char;
+    }
+
+    println!("{}", s.iter().collect::<String>());
 }

8d22ac2f786857024cffd1ff01282165387287d2
soso-15315 <sonishimura15@gmail.com>
2023-07-13 23:32:18 +0900

    feat: abc140

--- abc140/src/bin/b.rs
+++ abc140/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [Usize1; n],
+        b: [usize; n],
+        c: [usize; n - 1],
+    };
+
+    let mut ans = 0;
+    let mut prev = 100000;
+
+    for i in 0..n {
+        ans += b[a[i]];
+
+        if prev + 1 == a[i] {
+            ans += c[prev];
+        }
+
+        prev = a[i];
+    }
+
+    println!("{}", ans);
 }

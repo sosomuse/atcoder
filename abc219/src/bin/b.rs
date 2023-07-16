20cba1a07713778fcf994bf6661ffc93dbce017c
soso-15315 <sonishimura15@gmail.com>
2023-07-16 22:49:06 +0900

    feat: abc219 b

--- abc219/src/bin/b.rs
+++ abc219/src/bin/b.rs
@@ -1,3 +1,13 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: [String; 3],
+        t: Chars,
+    };
+
+    for c in t {
+        let i = c.to_digit(10).unwrap() as usize - 1;
+        print!("{}", s[i]);
+    }
 }

9fa56d0c69d9dda308681d5ab3bbb3f6fcbde811
soso-15315 <sonishimura15@gmail.com>
2023-07-17 22:42:37 +0900

    feat: abc219

--- abc219/src/bin/c.rs
+++ abc219/src/bin/c.rs
@@ -1,3 +1,28 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: Chars,
+        n: usize,
+        mut s: [Chars; n],
+    };
+
+    s.sort_by(|a, b| {
+        let mut i = 0;
+        while i < a.len() && i < b.len() {
+            if a[i] != b[i] {
+                return x
+                    .iter()
+                    .position(|&c| c == a[i])
+                    .unwrap()
+                    .cmp(&x.iter().position(|&c| c == b[i]).unwrap());
+            }
+            i += 1;
+        }
+        a.len().cmp(&b.len())
+    });
+
+    for s in s {
+        println!("{}", s.iter().collect::<String>());
+    }
 }

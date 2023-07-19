689dd86eceaff73c492b532e16dd6a2cd282b4ee
soso-15315 <sonishimura15@gmail.com>
2023-07-19 23:29:47 +0900

    feat: abc185 b

--- abc185/src/bin/b.rs
+++ abc185/src/bin/b.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        t: usize,
+        ab: [(usize, usize); m],
+    };
+
+    let mut res = n;
+    let mut prev = 0;
+
+    for (a, b) in ab {
+        res = res.saturating_sub(a - prev);
+        if res == 0 {
+            println!("No");
+            return;
+        }
+        res += b - a;
+        res = res.min(n);
+        prev = b;
+    }
+
+    res = res.saturating_sub(t - prev);
+
+    if res == 0 {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
 }

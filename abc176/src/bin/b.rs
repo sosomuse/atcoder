dddba6768607b05cb034ce95432d47839e858583
soso-15315 <sonishimura15@gmail.com>
2023-07-23 23:28:23 +0900

    feat: abc176 b

--- abc176/src/bin/b.rs
+++ abc176/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: Chars,
+    };
+
+    let mut sum = 0;
+    for c in n {
+        sum += c.to_digit(10).unwrap();
+    }
+
+    if sum % 9 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

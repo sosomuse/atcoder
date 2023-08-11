ec4fab3c790d9eb1d48ffc722ccbc1c9ae037d3c
soso-15315 <sonishimura15@gmail.com>
2023-08-11 23:59:04 +0900

    feat: abc191

--- abc191/src/bin/a.rs
+++ abc191/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        v: usize,
+        t: usize,
+        s: usize,
+        d: usize,
+    };
+
+    if v * t <= d && d <= v * s {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }

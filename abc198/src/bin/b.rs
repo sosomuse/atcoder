5706ae4492b9295c52bb77f2e6611425ed035b00
soso-15315 <sonishimura15@gmail.com>
2023-08-14 23:39:24 +0900

    feat: abc198 b

--- abc198/src/bin/b.rs
+++ abc198/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        mut n: Chars,
+    };
+
+    for i in (0..n.len()).rev() {
+        if n[i] == '0' {
+            n.pop();
+        } else {
+            break;
+        }
+    }
+
+    let mut ans = true;
+    for i in 0..n.len() / 2 {
+        if n[i] != n[n.len() - 1 - i] {
+            ans = false;
+            break;
+        }
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }

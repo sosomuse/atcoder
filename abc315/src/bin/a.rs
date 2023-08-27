1018553a8b8aa33b6c19c63fa7c33588d7019e3a
soso-15315 <sonishimura15@gmail.com>
2023-08-27 20:54:42 +0900

    feat: abc315 a

--- abc315/src/bin/a.rs
+++ abc315/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    };
+
+    s.chars().for_each(|c| {
+        if c != 'a' && c != 'i' && c != 'u' && c != 'e' && c != 'o' {
+            print!("{}", c);
+        }
+    });
 }

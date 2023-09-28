5bd43c23bcd057fcebc8549682d53b54d8dd1c7f
soso-15315 <sonishimura15@gmail.com>
2023-09-28 19:29:15 +0900

    feat: abc321 a

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,3 +1,22 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: Chars,
+    };
+
+    // 10進数変換
+    let mut prev = n[0].to_digit(10).unwrap();
+
+    for i in 1..n.len() {
+        let now = n[i].to_digit(10).unwrap();
+        if prev <= now {
+            println!("No");
+            return;
+        }
+
+        prev = now;
+    }
+
+    println!("Yes");
 }

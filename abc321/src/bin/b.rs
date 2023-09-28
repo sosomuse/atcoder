0d38d9d460d9e50216fee00897956ef94b3269da
soso-15315 <sonishimura15@gmail.com>
2023-09-28 19:56:08 +0900

    feat abc321 b

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        x: usize,
+        mut a: [usize; n-1],
+    };
+
+    a.sort();
+
+    let min = a[0];
+    let max = a[a.len() - 1];
+
+    let mut sum = 0;
+
+    for i in 1..a.len() - 1 {
+        sum += a[i];
+    }
+
+    if x < sum {
+        println!("0");
+        return;
+    }
+
+    let dis = x - sum;
+
+    if dis > max {
+        println!("-1");
+        return;
+    }
+
+    if dis <= min {
+        println!("0");
+        return;
+    }
+
+    println!("{}", dis);
 }

c7495915f0128d948e12d6f6639d6e0de69030fb
soso-15315 <sonishimura15@gmail.com>
2023-11-11 15:55:52 +0900

    feat: abc327 a

--- abc327/src/bin/a.rs
+++ abc327/src/bin/a.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        _: usize,
+        s: String,
+    };
+
+    let is_ok = s.contains("ab") || s.contains("ba");
+    println!("{}", if is_ok { "Yes" } else { "No" });
 }

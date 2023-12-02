ce3178127de5f7a69fb4de2a95fbf9b02f2e62b3
soso-15315 <sonishimura15@gmail.com>
2023-12-02 11:37:41 +0900

    feat abc327 b

--- abc327/src/bin/b.rs
+++ abc327/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        b: usize,
+    };
+
+    for i in 1..20 {
+        let (x, is_over) = (i as usize).overflowing_pow(i);
+        if is_over {
+            println!("-1");
+            return;
+        }
+
+        if x == b {
+            println!("{}", i);
+            return;
+        }
+    }
 }

b82a3cab868b4dd40a204bf2c90de61bf4a26fad
soso-15315 <sonishimura15@gmail.com>
2024-01-04 23:50:10 +0900

    feat abc333 b

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        t: String,
+    };
+
+    let s1 = &s[0..1];
+    let s2 = &s[1..2];
+    let t1 = &t[0..1];
+    let t2 = &t[1..2];
+
+    let s = "ABCDE";
+
+    let mut dist1 = (s.find(s1).unwrap() as i32 - s.find(s2).unwrap() as i32).abs();
+    let mut dist2 = (s.find(t1).unwrap() as i32 - s.find(t2).unwrap() as i32).abs();
+
+    if dist1 == 4 {
+        dist1 = 1;
+    }
+
+    if dist2 == 4 {
+        dist2 = 1;
+    }
+
+    let ans = (dist1 == 1 && dist2 == 1) || (dist1 != 1 && dist2 != 1);
+
+    if ans {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

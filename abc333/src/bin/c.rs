e734c5712f93ea73e179e6907e713d33218f7d09
So Nishimura <s.nishimura@gemcook.com>
2024-02-05 19:19:19 +0900

    feat: abc333 c

--- abc333/src/bin/c.rs
+++ abc333/src/bin/c.rs
@@ -2,10 +2,33 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut n: usize,
     };
 
-    let t = [1, 11, 111];
+    let r: Vec<usize> = vec![
+        1,
+        11,
+        111,
+        1111,
+        11111,
+        111111,
+        1111111,
+        11111111,
+        111111111,
+        1111111111,
+        11111111111,
+        111111111111,
+    ];
 
-    for i in 0..3 >> 5 {}
+    for i in 0..12 {
+        for j in 0..i + 1 {
+            for k in 0..j + 1 {
+                if n - 1 == 0 {
+                    println!("{}", r[i] + r[j] + r[k]);
+                    return;
+                }
+                n -= 1;
+            }
+        }
+    }
 }
